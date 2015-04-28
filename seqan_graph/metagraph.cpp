65abb1faf1d761dd13472958a318427b8caf1db8
Andre Kahles <akahles@cbio.mskcc.org>
2015-04-27 22:53:07 -0400

    can now parse several input files

--- seqan_graph/metagraph.cpp
+++ seqan_graph/metagraph.cpp
@@ -10,15 +10,20 @@
 #include <seqan/map.h>
 #include <seqan/index.h>
 
+#include <vector>
+#include <string>
+
 using namespace seqan;
 
 struct CFG 
 {
-    String<char> fname;
+    //String<char> > fname;
+    std::vector<std::string> fname;
     bool verbose;
+    unsigned int k;
 
     CFG() :
-        verbose(false)
+        verbose(false), k(31)
     {}
 };
 
@@ -40,10 +45,11 @@ parseCommandLine(CFG & config, int argc, char const ** argv)
                   "purposes.");
 
     // add arguments
-    addArgument(parser, ArgParseArgument(ArgParseArgument::STRING, "TEXT"));
+    addArgument(parser, ArgParseArgument(ArgParseArgument::STRING, "TEXT", 1));
 
     // add options
     addOption(parser, ArgParseOption("v", "verbose", "Increase verbosity level of output"));
+    addOption(parser, ArgParseOption("k", "kmer_length", "Length of the k-mer to use", ArgParseArgument::INTEGER, "INT"));
 
     // parse command line
     ArgumentParser::ParseResult res = parse(parser, argc, argv);
@@ -54,7 +60,8 @@ parseCommandLine(CFG & config, int argc, char const ** argv)
     
     // fill config object 
     getOptionValue(config.verbose, parser, "verbose");
-    getArgumentValue(config.fname, parser, 0); 
+    getOptionValue(config.k, parser, "k");
+    config.fname = getArgumentValues(parser, 0);
 
     // do any logic / error checking here (e.g., mutually exclusive options)
 
@@ -73,14 +80,6 @@ int main(int argc, char const ** argv) {
     if (config.verbose)
         std::cout << CharString("Welcome to MetaGraph") << std::endl;
 
-    // open stream to fasta file
-    SequenceStream stream;
-    open(stream, toCString(config.fname), SequenceStream::READ, SequenceStream::FASTA);
-    if (!isGood(stream)) {
-        std::cerr << "ERROR while opening input file " << config.fname << std::endl;
-        return 1;
-    }
-
     // build the graph from the k-mers in the string
     typedef unsigned int TCargo;
     typedef Graph<Directed<TCargo> > TGraph;
@@ -89,56 +88,68 @@ int main(int argc, char const ** argv) {
     // read from fasta stream 
     CharString id;
     String<Dna5> seq;
-    Shape<Dna5, UngappedShape<11> > hash_func;
-    typedef Value<Shape<Dna5, UngappedShape<11> > >::Type THash;
+    Shape<Dna5, SimpleShape> hash_func;
+    resize(hash_func, config.k);
+    typedef Value<Shape<Dna5, SimpleShape> >::Type THash;
     Map<Pair<THash, TVertexDescriptor> > kmer_map;
     TGraph g;
 
     unsigned int cnt_first = 0;
     unsigned int cnt_recurr = 0;
 
-    while (!atEnd(stream)) {
-        if (readRecord(id, seq, stream) != 0) {
-            std::cerr << "ERROR while reading from " << config.fname << std::endl;
+    // iterate over input files
+    for (unsigned int f = 0; f < length(config.fname); ++f) {
+
+        if (config.verbose) {
+            std::cout << "Parsing " << config.fname[f] << std::endl;
+        }
+
+        // open stream to fasta file
+        SequenceStream stream;
+        open(stream, toCString(config.fname.at(f)), SequenceStream::READ, SequenceStream::FASTA);
+        if (!isGood(stream)) {
+            std::cerr << "ERROR while opening input file " << config.fname.at(f) << std::endl;
             return 1;
         }
 
-        // iterate over k-mers of current sequence
-        THash hash;
-        hashInit(hash_func, begin(seq));
-        TVertexDescriptor last_node;
-        TVertexDescriptor current_node;
-        for (unsigned i = 0; i < length(seq) - length(hash_func) + 1; ++i) {
-            //if (i > 0 && i % 1000 == 0)
-            //    return 0;
-            if (i > 0 && i % 100000 == 0) {
-                std::cout << "." << std::flush;
-                //fprintf(stdout, ".");
-                if (i % 1000000 == 0)
-                    fprintf(stdout, "%i\n", i);
+        while (!atEnd(stream)) {
+            if (readRecord(id, seq, stream) != 0) {
+                std::cerr << "ERROR while reading from " << config.fname.at(f) << std::endl;
+                return 1;
             }
-            hash = hashNext(hash_func, begin(seq) + i);
-            //std::cout << hash << " " << infix(seq, i, i+4) << " ";
-            if (!hasKey(kmer_map, hash)) {
-                current_node = addVertex(g);
-                add(kmer_map, hash, current_node);
-                ++cnt_first;
-                //std::cout << "new" << std::endl;
-            } else {
-                current_node = kmer_map[hash];
-                ++cnt_recurr;
-                //std::cout << "recurr" << std::endl;
+
+            // iterate over k-mers of current sequence
+            THash hash;
+            hashInit(hash_func, begin(seq));
+            TVertexDescriptor last_node;
+            TVertexDescriptor current_node;
+            for (unsigned i = 0; i < length(seq) - length(hash_func) + 1; ++i) {
+                if (i > 0 && i % 100000 == 0) {
+                    std::cout << "." << std::flush;
+                    if (i % 1000000 == 0)
+                        fprintf(stdout, "%i\n", i);
+                }
+                hash = hashNext(hash_func, begin(seq) + i);
+                //std::cout << hash << " " << infix(seq, i, i+4) << " ";
+                if (!hasKey(kmer_map, hash)) {
+                    current_node = addVertex(g);
+                    add(kmer_map, hash, current_node);
+                    ++cnt_first;
+                    //std::cout << "new" << std::endl;
+                } else {
+                    current_node = kmer_map[hash];
+                    ++cnt_recurr;
+                    //std::cout << "recurr" << std::endl;
+                }
+                if (i > 1)
+                    addEdge(g, last_node, current_node);
+                last_node = current_node;
             }
-            if (i > 1)
-                addEdge(g, last_node, current_node);
-            last_node = current_node;
         }
-        std::cout << std::endl << "k-mers opening new nodes: " << cnt_first << std::endl;
-        std::cout << "k-mers already present in nodes: " << cnt_recurr << std::endl;
-        //std::cout << id << ":" << seq << std::endl;
     }
-
-
+    std::cout << std::endl << "k-mers opening new nodes: " << cnt_first << std::endl;
+    std::cout << "k-mers already present in nodes: " << cnt_recurr << std::endl;
+    //std::cout << id << ":" << seq << std::endl;
 
     return 0;
 }
