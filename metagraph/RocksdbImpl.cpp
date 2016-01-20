91c429db755dbd86701a2731de2df183ace1d093
Gideon Dresdner <gideond@gmail.com>
2016-01-20 17:29:11 -0500

    integration test for rocksdb.
    beginning of key only model.

--- metagraph/RocksdbImpl.cpp
+++ metagraph/RocksdbImpl.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <set>
 
 #include <seqan/basic.h>
 #include <seqan/index.h>
@@ -23,6 +24,8 @@ private:
     std::string dbpath;
     rocksdb::DB* db;
     rocksdb::Status status;
+    std::string ANNOTATION_COLUMN = "annotation";
+    std::string KMER_COLUMN = "kmer";
 
     template<typename T> std::vector<T> concat(std::vector<T> vec1, std::vector<T> vec2) {
         // http://stackoverflow.com/questions/3177241/what-is-the-best-way-to-concatenate-two-vectors
@@ -34,25 +37,74 @@ private:
         return ret;
     }
 
+    void partition(std::vector<std::string> columns) {
+        rocksdb::Options options;
+        options.create_if_missing = true;
+
+        std::vector<rocksdb::ColumnFamilyDescriptor> column_families;
+        column_families.push_back(rocksdb::ColumnFamilyDescriptor(rocksdb::kDefaultColumnFamilyName,
+                                                                           rocksdb::ColumnFamilyOptions()));
+        // make the partition
+        for (auto it = columns.begin(); it < columns.end(); ++it) {
+            rocksdb::ColumnFamilyHandle* cf;
+            status = db->CreateColumnFamily(rocksdb::ColumnFamilyOptions(), *it, &cf);
+            column_families.push_back(rocksdb::ColumnFamilyDescriptor(*it, rocksdb::ColumnFamilyOptions()));
+            delete cf;
+        }
+
+        delete db;
+
+        // reopen with new column families
+        std::vector<rocksdb::ColumnFamilyHandle*> handles;
+        status = rocksdb::DB::Open(options, dbpath, column_families, &handles, &db);
+        assert(status.ok());
+    }
+
 public:
     RocksdbImpl(std::string the_dbpath) : dbpath(the_dbpath) {
         rocksdb::Options options;
         options.create_if_missing = true;
         status = rocksdb::DB::Open(options, dbpath, &db);
+
+        std::vector<std::string> existing_column_families;
+        rocksdb::DB::ListColumnFamilies(options, dbpath, &existing_column_families);
+        // if (existing_column_families.size() == 1) {
+        //     std::vector<std::string> column_names;
+        //     column_names.push_back(ANNOTATION_COLUMN);
+        //     column_names.push_back(KMER_COLUMN);
+        //     partition(column_names);
+        // }
     };
 
     void annotate_kmer(std::string kmer, std::string tag) {
-        assert(status.ok());
-        db->Put(rocksdb::WriteOptions(), kmer, tag);
+        std::stringstream key;
+        key << kmer << ":" << tag;
+
+        std::string ignored = "";
+
+        status = db->Put(rocksdb::WriteOptions(), key.str(), ignored);
         assert(status.ok());
     }
 
     std::string get_annotation(std::string kmer) {
-        std::string ret;
-        assert(status.ok());
-        db->Get(rocksdb::ReadOptions(), kmer, &ret);
-        assert(status.ok());
-        return ret;
+        std::string ignored;
+        db->Get(rocksdb::ReadOptions(), kmer, &ignored);
+
+        std::cout << "get_annotation: " << kmer << std::endl;
+        std::vector<std::string> ret;
+        ret.push_back("FOOBAR");
+
+        auto it = db->NewIterator(rocksdb::ReadOptions());
+        it->Seek(kmer);
+        std::cout << it->key().ToString() << std::endl;
+        // iter->Next();
+        // for (iter->Seek(kmer); iter->Valid(); iter->Next()) {
+        // std::cout << "within iter" << std::endl;
+        //     // do something
+        //     iter->key();
+        // }
+        
+        return ret[0];
     }
 };
 
--- metagraph/TestDatabase.cpp
+++ metagraph/TestDatabase.cpp
@@ -7,6 +7,7 @@
 
 #include <dbg_succinct_libmaus.hpp>
 #include <MockDatabase.cpp>
+#include <RocksdbImpl.cpp>
 
 /**
  * Tests that the mock database code is working properly.
@@ -25,27 +26,49 @@ void test_MockDatabase() {
 }
 
 /*
- * Runs DBG graph construction on the given database and makes sure
+ * Runs DBG graph construction on the mock database and makes sure
  * that the database is behaving properly.
  */
 void test_DatabaseIntegration() {
     CFG config;
     config.k = 3;
     DBG_succ* graph = new DBG_succ(config.k, config);
-    dbg_database::IDatabase *db = new dbg_database::MockDatabase();
+    dbg_database::IDatabase *mockdb = new dbg_database::MockDatabase();
 
     seqan::String<seqan::Dna5> seq = "TCGA";
     seqan::CharString annotation = "annotationA";
-    graph->add_annotation_for_seq(db, seq, annotation);
+    graph->add_annotation_for_seq(mockdb, seq, annotation);
 
-    auto datastore = ((dbg_database::MockDatabase *) db)->getDatastore();
+    auto datastore = ((dbg_database::MockDatabase *) mockdb)->getDatastore();
 
     assert(2 == datastore.size());
     assert(datastore["TCG"] == "annotationA");
     assert(datastore["CGA"] == "annotationA");
+
+    delete graph;
+    delete mockdb;
+}
+
+/**
+ * Runs the DBG graph construction on the RocksDB implementation
+ */
+void test_RocksDbIntegration() {
+    CFG config;
+    config.k = 3;
+    DBG_succ* graph = new DBG_succ(config.k, config);
+    dbg_database::IDatabase *therocksdb = new dbg_database::RocksdbImpl("/tmp/dbg-test-annotation-db");
+
+    seqan::String<seqan::Dna5> seq = "TCGA";
+    seqan::CharString annotation = "annotationA";
+    graph->add_annotation_for_seq(therocksdb, seq, annotation);
+
+    therocksdb->get_annotation("TCG");
+
+    delete graph;
+    delete therocksdb;
 }
 
 int main() {
     test_MockDatabase();
-    test_DatabaseIntegration();
+    test_RocksDbIntegration();
 }
