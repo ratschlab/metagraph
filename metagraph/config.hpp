38a450bd4ad0d665182f146e92028c7b024257cb
Andre Kahles <andre.kahles@ratschlab.org>
2016-05-26 19:54:07 +0200

    Switched from seqan to kseq
    * removed all seqan dependencies
    * using kseq library instead (part of HTSeq)
    * re-organized config and command line parsing

--- metagraph/config.hpp
+++ metagraph/config.hpp
@@ -1,9 +1,26 @@
 #ifndef __CONFIG_HPP__
 #define __CONFIG_HPP__
 
-    struct CFG 
-    {
-        //String<char> > fname;
+#include <cstring>
+#include <string>
+#include <vector>
+
+class Config {
+    public:
+        Config();
+        Config(int argc, const char *argv[]);
+
+        ~Config();
+       
+        void print_usage(std::string prog_name);
+        //void print_call(string prog_name);
+
+        bool verbose;
+        bool integrate;
+        
+        unsigned int k;
+        unsigned int distance;
+
         std::vector<std::string> fname;
         std::string outfbase;
         std::string infbase;
@@ -11,17 +28,8 @@
         std::string merge;
         std::string compare;
         std::string align;
-        bool verbose;
-        bool integrate;
-        unsigned int k;
-        unsigned int distance;
 
-        CFG() :
-            verbose(false), k(31)
-        {}
-    };
-
-
-
-
+    private:
+        void init();
+};
 #endif
