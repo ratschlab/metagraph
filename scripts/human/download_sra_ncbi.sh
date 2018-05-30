31ede89355e990f1229feb88e7a33f8f1cf5ff17
Mikhail Karasikov <mike-karas@ya.ru>
2018-05-30 13:47:56 +0200

    scripts updated

--- scripts/human/download_sra_ncbi.sh
+++ scripts/human/download_sra_ncbi.sh
@@ -18,19 +18,29 @@ fi
 URL="$1"
 FILE=$(basename $URL)
 
+
+if [ ! -d "$BUFFER/downloaded" ]; then
+  echo "Initialize downloaded mirror:"
+  echo mkdir -p "$BUFFER/downloaded"
+  echo "for x in \$(ssh leomed 'sh -c \"ls $FINAL_DEST\"'); do touch $BUFFER/downloaded/\$x; done"
+  exit 1
+fi
+
+
 # check if the file already exists
 #if $LFTP -c "connect -u mikhaika, sftp://leomed; glob --exist $FINAL_DEST/*$FILE"; then
-if [ -f $BUFFER/$FILE ] || ssh leomed 'sh -c "if [ -f '"'$FINAL_DEST/$FILE'"' ] ; then exit 0; else exit 1; fi"'; then
+#ssh leomed 'sh -c "if [ -f '"'$FINAL_DEST/$FILE'"' ] ; then exit 0; else exit 1; fi"'
+if [ -f $BUFFER/$FILE ] || [ -f $BUFFER/downloaded/$FILE ]; then
   echo "$FILE already exists"
   exit 0
 fi
 
 # download file to buffer zone
-if ! $ASCP -l 1500000 -T -k 1 -q -i $ASCP_KEY anonftp@ftp.ncbi.nlm.nih.gov:$URL $BUFFER; then
+if ! $ASCP -l 3000m -T -k 1 -q -i $ASCP_KEY anonftp@ftp.ncbi.nlm.nih.gov:$URL $BUFFER; then
   echo "Loading failed: $FILE"
   exit 0
 fi
 
 # move downloaded file to final destination
 #$LFTP -c "connect -u mikhaika, sftp://leomed; mput -e -E -O $FINAL_DEST $BUFFER/$FILE"
-scp $BUFFER/$FILE leomed:$FINAL_DEST && rm $BUFFER/$FILE
+scp $BUFFER/$FILE leomed:$FINAL_DEST && touch $BUFFER/downloaded/$FILE && rm $BUFFER/$FILE
