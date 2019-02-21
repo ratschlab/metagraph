92d062b0f11469b76a9dcf936e4aa5e826608709
Mikhail Karasikov <mike-karas@ya.ru>
2019-02-21 12:32:48 +0100

    some cleanups

--- metagraph/lossless_dbg/main.cpp
+++ metagraph/lossless_dbg/main.cpp
@@ -15,17 +15,24 @@
 #include <gtest/gtest.h>
 #endif
 
+// TODO: get rid of this
 using namespace std;
 using namespace std::string_literals;
 
 // debugging functions
-struct d_ {template<typename T> d_ & operator ,(const T & x) { cerr << ' ' <<  x; return *this;}} d_t;
-#define D(args ...) { d_t,"|",__LINE__,"|",#args,":",args,"\n"; }
+struct d_t {
+    template<typename T> d_ & operator,(const T & x) {
+        std::cerr << ' ' <<  x;
+        return *this;
+    }
+};
+
+#define D(args ...) { d_t, "|", __LINE__, "|", #args, ":", args, "\n"; }
 
 using node_index = SequenceGraph::node_index;
 
 
-
+// TODO: remove these constants
 const string HUMAN_REFERENCE_FILENAME = "/Users/janstudeny/Library/Mobile Documents/com~apple~CloudDocs/Active projects/Master Thesis/genomic-data/GCF_000001405.38_GRCh38.p12_genomic.fna";
 const string HUMAN_CHROMOSOME_10_SAMPLE = ""s + dirname(__FILE__) + "/human_chromosome_10_sample.fasta";
 const int CHROMOSOME_NUMBER = 10;
@@ -38,48 +45,54 @@ const int test_seed = 3424;
 #define all(x) begin(x),end(x)
 
 
-
-
-node_index k_mer_to_node(DBGSuccinct& de_Bruijn_Graph, const string &k_mer) {
-    node_index result;
-    de_Bruijn_Graph.map_to_nodes(k_mer,[&](node_index node) {result = node;},[](){return true;});
-    return result;
-}
-
 class CompressedReads {
     using bifurcation_choices_t = vector<char>;
-    using k_mer_t = string;
-    using compressed_read_t = pair<k_mer_t,bifurcation_choices_t>;
+    using kmer_t = string;
+    using compressed_read_t = pair<kmer_t, bifurcation_choices_t>;
 
 public:
-    CompressedReads(vector<string> raw_reads) :  read_length(raw_reads[0].length()), graph(dbg_succ_graph_constructor(raw_reads)) {
-        for(auto & read : raw_reads) {
+    // TODO: read about lvalues, rvalues, etc
+    // http://thbecker.net/articles/rvalue_references/section_01.html
+    // const value
+    // CompressedReads(const vector<string> &raw_reads)
+    // non-const pointer to modify
+    // CompressedReads(vector<string> *raw_reads)
+    // rvalue (move constructor)
+    CompressedReads(vector<string>&& raw_reads)
+          : read_length(raw_reads[0].length()),
+            graph(dbg_succ_graph_constructor(raw_reads)) {
+        // for(auto &read : raw_reads) {
+        for(auto&& read : raw_reads) {
             reads.insert(align_read(read));
         }
     }
+
     int compressed_size_without_reference() {
         // returns size in bits
         int size = 0;
-        for(auto & read : reads) {
+        for(auto &read : reads) {
             // *2 for two bit encoding
-            size += read.x.size()*2;
-            size += read.y.size()*2;
+            size += read.x.size() * 2;
+            size += read.y.size() * 2;
         }
         // to be really fair
         // size += sizeof(read_length);
         return size;
     }
-    double bits_per_symbol(bool include_reference=false) {
+
+    double bits_per_symbol(bool include_reference = false) {
         assert(!include_reference);
-        return static_cast<double>(compressed_size_without_reference())/(reads.size()*read_length);
+        return static_cast<double>(compressed_size_without_reference())
+                    / (reads.size() * read_length);
     }
-private:
+
+  private:
     compressed_read_t align_read(const string &read) {
-        auto k_mer = read.substr(0,k_k_mer);
         auto bifurcation_choices = bifurcation_choices_t();
-        auto node = k_mer_to_node(graph,k_mer);
+        auto node = graph.kmer_to_node(read.data());
         // for all other characters
-        for(auto& character : read.substr(k_k_mer)) {
+        for (auto &character : read.substr(k_kmer)) {
+            // TODO: it's faster to initialize vector once outside the loop
             vector<node_index> outnodes;
             graph.adjacent_incoming_nodes(node, &outnodes);
             if (outnodes.size() > 1) {
@@ -87,22 +100,37 @@ private:
             }
             assert(!outnodes.empty());
         }
-        return {k_mer,bifurcation_choices};
+
+        return { read.substr(0, k_kmer), bifurcation_choices };
     }
+
     static DBG_succ* dbg_succ_graph_constructor(vector<string> raw_reads) {
-        auto graph_constructor = DBGSuccConstructor(k_k_mer);
+        auto graph_constructor = DBGSuccConstructor(k_kmer);
         cerr << "Starting building the graph" << endl;
-        for(auto & read : raw_reads) {
-            assert(read.size() >= k_k_mer);
+        for (auto & read : raw_reads) {
+            assert(read.size() >= k_kmer);
             graph_constructor.add_sequence(read);
         }
         return new DBG_succ(&graph_constructor);
         //graph = DBGSuccinct(stupid_old_representation);
     }
 
+    // TODO: use underscores for private members of class
+    // https://google.github.io/styleguide/cppguide.html
+    //
+    // Class Data Members
+    // Data members of classes, both static and non-static, are named like ordinary nonmember variables, but with a trailing underscore.
+
+    // class TableInfo {
+    //   ...
+    //  private:
+    //   string table_name_;  // OK - underscore at end.
+    //   string tablename_;   // OK.
+    //   static Pool<TableInfo>* pool_;  // OK.
+    // };
     multiset<compressed_read_t> reads;
     int read_length;
-    static const size_t k_k_mer = 21;
+    static const size_t k_kmer = 21;
     DBGSuccinct graph;
 };
 
@@ -219,11 +247,12 @@ void to_be_determined() {
     auto sampler = Sampler(chromosome,test_seed);
     auto reads = sampler.sample_coverage(READ_LENGTH, READ_COVERAGE);
     SamplerConvenient::transform_to_fasta(HUMAN_CHROMOSOME_10_SAMPLE,reads);
-    auto compressed_reads = CompressedReads(reads);
+    auto compressed_reads = CompressedReads(std::move(reads));
 }
+
 void code_to_violate_assertion() {
     auto reads = read_reads_from_fasta(HUMAN_CHROMOSOME_10_SAMPLE);
-    auto compressed_reads = CompressedReads(reads);
+    auto compressed_reads = CompressedReads(std::move(reads));
 }
 
 int main(int argc,char**argv) {
