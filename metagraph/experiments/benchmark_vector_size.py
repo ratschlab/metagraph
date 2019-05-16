6390182274f9a682450e1d1a17ed076e89fed6b7
Mikhail Karasikov <mike-karas@ya.ru>
2019-05-16 16:26:17 +0000

    benchmarking bit vectors

--- metagraph/experiments/benchmark_vector_size.py
+++ metagraph/experiments/benchmark_vector_size.py
@@ -12,7 +12,7 @@ markers = ['^', 'v', '<', '>', 'o', 's', 'P', '*']
 
 
 if len(sys.argv) != 2:
-    print("Usage:\n{} plot <results.txt>".format(sys.argv[0]))
+    print("Usage:\n{} <results.txt>".format(sys.argv[0]))
     print(
 """\n\n#First, run this script to generate vectors and measure their size
 
@@ -21,6 +21,13 @@ for type in small smart stat sd rrr63 rrr127; do
         ./run_experiments vectors $type -d $d -l 100000000 >> results.txt
     done
 done
+
+or, in parallel:
+
+for type in {"stat","sd","rrr63","small","smart"}; do
+    seq 0 0.05 1 | xargs -n 1 -P 50 -I % ./run_experiments vectors $type -d % -l 2000000 >> results.txt;
+done
+
 """)
     exit(1)
 
@@ -34,37 +41,45 @@ vector_size = lines[:, 1].astype(int)
 density = lines[:, 3].astype(float)
 bits_per_entry = lines[:, 4].astype(float)
 RAM_per_entry = lines[:, 5].astype(float) / vector_size * 8
+bits_per_entry_expected = lines[:, 6].astype(float)
 
 plt.rcParams.update({ 'font.size': 22 })
 
 figsize = 8
 
-for vs in np.unique(vector_size):
-    plt.figure(figsize=(figsize * 1.2, figsize))
+def plot_feature(feature=bits_per_entry, name="Bits per entry"):
+    for vs in np.unique(vector_size):
+        plt.figure(figsize=(figsize * 1.2, figsize))
 
-    ms = np.linspace(20, 4, len(np.unique(method)))
+        ms = np.linspace(10, 4, len(np.unique(method)))
 
-    for i, m in enumerate(np.unique(method)):
-        dens = density[(method == m) & (vector_size == vs)]
-        sizes = RAM_per_entry[(method == m) & (vector_size == vs)]
-        idx = dens.argsort()
-        plt.plot(dens[idx], sizes[idx],
-                 label=m,
-                 marker=markers[i],
-                 ms=ms[i])
+        for i, m in enumerate(np.unique(method)):
+            dens = density[(method == m) & (vector_size == vs)]
+            sizes = feature[(method == m) & (vector_size == vs)]
+            idx = dens.argsort()
+            plt.plot(dens[idx], sizes[idx],
+                     label=m,
+                     marker='o',
+                     ms=ms[i])
 
-    sizes = -xlogy(dens, dens) + 2 * dens
-    plt.plot(dens[idx], sizes[idx],
-             label='SD_Theory',
-             marker=markers[i],
-             ms=ms[i])
+        for i, m in enumerate(np.unique(method)):
+            dens = density[(method == m) & (vector_size == vs)]
+            sizes = bits_per_entry_expected[(method == m) & (vector_size == vs)]
+            idx = dens.argsort()
+            plt.plot(dens[idx], sizes[idx],
+                     label=m + ' predicted',
+                     marker='*',
+                     ms=ms[i])
 
-    plt.title('Vector size: {:.1e}'.format(vs))
-    plt.xlabel('Density')
-    plt.ylabel('Bits per entry')
+        plt.title('{}: {:.1e}'.format(name, vs))
+        plt.xlabel('Density')
+        plt.ylabel('Bits per entry')
 
-    plt.grid(True)
-    plt.legend(fontsize=18)
-    plt.tight_layout()
-    plt.savefig('vectors_size_{}.pdf'.format(vs), fmt='pdf')
-    plt.show()
+        plt.grid(True)
+        plt.legend(fontsize=18)
+        plt.tight_layout()
+        plt.savefig('vectors_{}_{}.pdf'.format('_'.join(name.split(' ')).lower(), vs), fmt='pdf')
+        plt.show()
+
+plot_feature(bits_per_entry, 'Serialized Size')
+plot_feature(RAM_per_entry, 'RAM')
