3d1209ad365be38737c08327afc0cab6d9f26c47
janstudeny <jendas1@yahoo.com>
2019-05-28 15:20:31 +0200

    Fixed bug.

--- metagraph/experiments/lossless_dbg/playground/compressor/compressor.hpp
+++ metagraph/experiments/lossless_dbg/playground/compressor/compressor.hpp
@@ -46,111 +46,112 @@ using node_index = SequenceGraph::node_index;
 
 template<class Database>
 Database compressReads(ValueArg<string> &compressedArg,
-                       const vector<string> &filenames,
-                       const vector<string> &reads) {
-    auto db = Database(filenames);
-    db.encode(reads);
+		const vector<string> &filenames,
+		const vector<string> &reads) {
+	auto db = Database(filenames);
+	db.encode(reads);
 
-    if (compressedArg.isSet()) {
-        fs::path compress_folder = compressedArg.getValue();
-        db.serialize(compress_folder);
-    }
-    return db;
+	if (compressedArg.isSet()) {
+		fs::path compress_folder = compressedArg.getValue();
+		db.serialize(compress_folder);
+	}
+	return db;
 }
 
 template<class Database>
 Database compressReadsDeprecated(ValueArg<string> &compressedArg,
-                                 const vector<string> &reads,int kmer_length) {
-    auto db = Database(reads,kmer_length);
-    db.encode(reads);
+		const vector<string> &reads,int kmer_length) {
+	auto db = new Database(reads,kmer_length);
+	db.encode(reads);
 
-    if (compressedArg.isSet()) {
-        fs::path compress_folder = compressedArg.getValue();
-        db.serialize(compress_folder);
-    }
-    return db;
+	if (compressedArg.isSet()) {
+		fs::path compress_folder = compressedArg.getValue();
+		db.serialize(compress_folder);
+	}
+	return db;
 }
 
 int main_compressor(int argc, char *argv[]) {
-    TCLAP::CmdLine cmd("Compress reads",' ', "0.1");
+	TCLAP::CmdLine cmd("Compress reads",' ', "0.1");
 
-    //TODO:
-    TCLAP::ValueArg<std::string> graphArg("g",
-                                          "graph",
-                                          "DeBruijnGraph to use as a reference in compression",
-                                          true,
-                                          "",
-                                          "string",cmd);
+	//TODO:
+	TCLAP::ValueArg<std::string> graphArg("g",
+			"graph",
+			"DeBruijnGraph to use as a reference in compression",
+			true,
+			"",
+			"string",cmd);
 
-    TCLAP::ValueArg<std::string> inputArg("i",
-                                         "input",
-                                         "FASTA/Q file that should be compressed",
-                                         true,
-                                         "",
-                                         "string",cmd);
-    TCLAP::ValueArg<int> kmerLengthArg("k",
-                                       "kmer-length",
-                                       "Length of the kmers for graph construction",
-                                       false,
-                                       21,
-                                       "int",cmd);
+	TCLAP::ValueArg<std::string> inputArg("i",
+			"input",
+			"FASTA/Q file that should be compressed",
+			true,
+			"",
+			"string",cmd);
+	TCLAP::ValueArg<int> kmerLengthArg("k",
+			"kmer-length",
+			"Length of the kmers for graph construction",
+			false,
+			21,
+			"int",cmd);
 
-    TCLAP::ValueArg<std::string> statisticsArg("s",
-                                          "statistics",
-                                          "Filename of json file that will output statistics about compressed file.",
-                                          false,
-                                          "statistics.json",
-                                          "string",cmd);
-    TCLAP::ValueArg<std::string> compressedArg("o",
-                                          "output",
-                                          "Folder where to store the compressed files.",
-                                          false,
-                                          "",
-                                          "string",cmd);
-    TCLAP::ValueArg<int> numThreadsArg("p",
-                                       "threads",
-                                       "Number of threads to use for parallel computation.",
-                                       false,
-                                       1,
-                                       "int",cmd);
+	TCLAP::ValueArg<std::string> statisticsArg("s",
+			"statistics",
+			"Filename of json file that will output statistics about compressed file.",
+			false,
+			"statistics.json",
+			"string",cmd);
+	TCLAP::ValueArg<std::string> compressedArg("o",
+			"output",
+			"Folder where to store the compressed files.",
+			false,
+			"",
+			"string",cmd);
+	TCLAP::ValueArg<int> numThreadsArg("p",
+			"threads",
+			"Number of threads to use for parallel computation.",
+			false,
+			1,
+			"int",cmd);
 
 
-    std::vector<std::string> regimes {
-            "wavelet",
-    };
-    ValuesConstraint<std::string> regime_constraint(regimes);
-    ValueArg<std::string> compressor_type("c",
-                                          "compressor-type",
-                                          "Which compressor to choose",
-                                          false,
-                                          "wavelet",
-                                          &regime_constraint, cmd);
-    cmd.parse(argc, argv);
-    set_num_threads(numThreadsArg.getValue());
-    auto input_filename = inputArg.getValue();
-    auto statistics_filename = statisticsArg.getValue();
-    // TODO: Don't read all reads to memory
-    auto reads = read_reads_from_fasta(input_filename);
-    auto kmer_length = kmerLengthArg.getValue();
-    auto compressor = compressor_type.getValue();
-    if (compressor == "wavelet") {
+	std::vector<std::string> regimes {
+		"wavelet",
+	};
+	ValuesConstraint<std::string> regime_constraint(regimes);
+	ValueArg<std::string> compressor_type("c",
+			"compressor-type",
+			"Which compressor to choose",
+			false,
+			"wavelet",
+			&regime_constraint, cmd);
+	cmd.parse(argc, argv);
+	set_num_threads(numThreadsArg.getValue());
+	auto input_filename = inputArg.getValue();
+	auto statistics_filename = statisticsArg.getValue();
+	// TODO: Don't read all reads to memory
+	auto reads = read_reads_from_fasta(input_filename);
+	auto kmer_length = kmerLengthArg.getValue();
+	auto compressor = compressor_type.getValue();
+	if (compressor == "wavelet") {
 		std::unique_ptr<PathDatabaseWavelet<>> pd;
-        if (graphArg.isSet()) {
-            auto graph = std::make_shared<DBGSuccinct>();
-            graph->load(graphArg.getValue());
-            pd.reset(new PathDatabaseWavelet<>(graph));
-            pd->encode(reads);
-        } else {
-        	pd.reset(new PathDatabaseWavelet<>>(
-						compressReadsDeprecated<PathDatabaseWavelet<>>(
-							compressedArg, reads,kmer_length)
-						));
+		if (graphArg.isSet()) {
+			auto graph = std::make_shared<DBGSuccinct>();
+			graph->load(graphArg.getValue());
+			pd.reset(new PathDatabaseWavelet<>(graph));
+		} else {
+			pd.reset(new PathDatabaseWavelet<>(reads,kmer_length));
 		}
-        if (statisticsArg.isSet()) {
-            auto statistics = pd->get_statistics();
-            save_string(statistics.dump(4),statistics_filename);
-        }
-    }
-    return 0;
+		pd->encode(reads);
+		if (compressedArg.isSet()) {
+			fs::path compress_folder = compressedArg.getValue();
+			pd->serialize(compress_folder);
+		}
+		if (statisticsArg.isSet()) {
+			auto statistics = pd->get_statistics();
+			save_string(statistics.dump(4),statistics_filename);
+		}
+	}
+	return 0;
 }
 
