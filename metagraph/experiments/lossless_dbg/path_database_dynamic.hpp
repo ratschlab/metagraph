0fce61a14bfaa72a2113c1d34b7f7956f8063070
janstudeny <jendas1@yahoo.com>
2019-06-01 18:10:40 +0200

    Changed false assumption.

--- metagraph/experiments/lossless_dbg/path_database_dynamic.hpp
+++ metagraph/experiments/lossless_dbg/path_database_dynamic.hpp
@@ -137,7 +137,7 @@ public:
         incoming_table.incoming_table.rank = &rank_is_join;
         incoming_table.incoming_table.elements = decltype(incoming_table.incoming_table.elements)(
                                                             rank_is_join(rank_is_join.size()),
-                                                            decltype(incoming_table.incoming_table.elements)::value_type{});
+                                                            array<int,6>{});
 
         alloc_routing_table.finished();
         for(auto& table : incoming_table.incoming_table.elements) {
