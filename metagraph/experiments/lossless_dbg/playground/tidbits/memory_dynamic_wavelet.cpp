de664e3699c6447dc09fb7d4dad4703f864d9cbe
Jan Studeny <jendas1@yahoo.com>
2019-06-04 21:00:59 +0200

    Added simple size test.

---
+++ metagraph/experiments/lossless_dbg/playground/tidbits/memory_dynamic_wavelet.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include <vector>
+#include "wavelet_tree.hpp"
+
+int main(int argc,char** argv) {
+    vector<int> initial_content(1000,6);
+    vector<wavelet_tree_dyn> size_test(1000,wavelet_tree_dyn(7,inital_content));
+
+}
