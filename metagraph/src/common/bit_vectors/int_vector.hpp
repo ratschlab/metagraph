16ce45b3128d47ffc9f6d62f9385010c508bfa47
Mikhail Karasikov <mike-karas@ya.ru>
2019-07-07 13:02:58 +0300

    cleanup

--- metagraph/src/common/bit_vectors/int_vector.hpp
+++ metagraph/src/common/bit_vectors/int_vector.hpp
@@ -5,11 +5,14 @@
 
 #include <sdsl/int_vector.hpp>
 
-void call_nonzeros(const sdsl::int_vector<> &vector,
-                   uint64_t begin, uint64_t end,
-                   std::function<void(uint64_t, uint64_t)> callback);
 
 void call_nonzeros(const sdsl::int_vector<> &vector,
-                   std::function<void(uint64_t, uint64_t)> callback);
+                   uint64_t begin, uint64_t end,
+                   std::function<void(uint64_t /* index */,
+                                      uint64_t /* value */)> callback);
+
+void call_nonzeros(const sdsl::int_vector<> &vector,
+                   std::function<void(uint64_t /* index */,
+                                      uint64_t /* value */)> callback);
 
 #endif // __INT_VECTOR_HPP__
