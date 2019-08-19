8193ffb3acb3c7cb302218a5a980f821a3348ebb
Mikhail Karasikov <mike-karas@ya.ru>
2019-08-19 16:27:16 +0000

    fixed bug in bit_vector_dyn::get_int

--- metagraph/src/common/bit_vectors/bit_vector.cpp
+++ metagraph/src/common/bit_vectors/bit_vector.cpp
@@ -267,9 +267,10 @@ bool bit_vector_dyn::operator[](uint64_t id) const {
 
 uint64_t bit_vector_dyn::get_int(uint64_t id, uint32_t width) const {
     assert(id + width <= size());
-    uint64_t pos = id + width - 1;
+    assert(width);
+    int64_t pos = id + width - 1;
     uint64_t word = 0;
-    while (pos >= id) {
+    while (pos >= static_cast<int64_t>(id)) {
         word = (word << 1) + vector_.at(pos--);
     }
     return word;
