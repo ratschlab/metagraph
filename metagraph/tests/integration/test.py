b8ec683d585b830d8ef318fa14d74078fce13b1a
Mikhail Karasikov <mike-karas@ya.ru>
2019-09-27 17:11:47 +0000

    added colors and timings

--- metagraph/tests/integration/test.py
+++ metagraph/tests/integration/test.py
@@ -1,6 +1,7 @@
 import glob
 import unittest
 import os
+import time
 
 
 """Run all integration tests"""
@@ -13,6 +14,27 @@ def create_test_suite():
     return unittest.TestSuite(suites)
 
 
+class TimeLoggingTestResult(unittest.TextTestResult):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stream.write("\033[0;32;40m[----------]\033[0m {}\n".format(""))
+
+    def stopTestRun(self):
+        self.stream.write("\033[0;32;40m[----------]\033[0m {}\n".format(""))
+        super().stopTestRun()
+
+    def startTest(self, test):
+        name = self.getDescription(test)
+        self.stream.write("\033[0;32;40m[ RUN      ]\033[0m {}\n".format(name))
+        self._started_at = time.time()
+        super().startTest(test)
+
+    def addSuccess(self, test):
+        elapsed = time.time() - self._started_at
+        name = self.getDescription(test)
+        self.stream.write("\033[0;32;40m[       OK ]\033[0m {} ({:.03} s)\n".format(name, elapsed))
+        # super().addSuccess(test)
+
 if __name__ == '__main__':
-    text_runner = unittest.TextTestRunner().run(create_test_suite())
+    text_runner = unittest.TextTestRunner(resultclass=TimeLoggingTestResult).run(create_test_suite())
     # unittest.main()
