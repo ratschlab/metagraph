08caf369182b357cfa9d4e33155c83f433141489
Mikhail Karasikov <mike-karas@ya.ru>
2019-09-28 23:58:56 +0000

    cleanup

--- metagraph/tests/integration/test_build.py
+++ metagraph/tests/integration/test_build.py
@@ -12,18 +12,19 @@ TEST_DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../data'
 
 
 class TestBuild(unittest.TestCase):
+    def setUp(self):
+        self.tempdir = TemporaryDirectory()
+
     def test_simple_all_graphs(self):
         """
         Simple build test
         """
 
-        tempdir = TemporaryDirectory()
-
         for representation in ['succinct', 'bitmap', 'hash', 'hashstr']:
 
             command = './metagraph build --graph {repr} -k 20 -o {outfile} {input}'.format(
                 repr=representation,
-                outfile=tempdir.name + '/graph',
+                outfile=self.tempdir.name + '/graph',
                 input=TEST_DATA_DIR + '/transcripts_1000.fa'
             )
 
@@ -35,11 +36,9 @@ class TestBuild(unittest.TestCase):
         Simple build test
         """
 
-        tempdir = TemporaryDirectory()
-
         args = ['./metagraph', 'build',
                     '-k', '2',
-                    '-o', tempdir.name + '/graph',
+                    '-o', self.tempdir.name + '/graph',
                     TEST_DATA_DIR + '/transcripts_1000.fa']
         command = ' '.join(args)
 
