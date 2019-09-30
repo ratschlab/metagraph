1f283f96ba08b69109a613754039a7f4e19018f1
Mikhail Karasikov <mike-karas@ya.ru>
2019-09-30 09:13:19 +0000

    new tests

---
+++ metagraph/integration_tests/test_annotate.py
@@ -0,0 +1,348 @@
+import unittest
+import subprocess
+from tempfile import TemporaryDirectory
+import glob
+import os
+
+
+"""Test graph annotation"""
+
+METAGRAPH = './metagraph'
+TEST_DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../tests/data'
+
+graph_file_extension = {'succinct': '.dbg',
+                        'bitmap': '.bitmapdbg',
+                        'hash': '.orhashdbg',
+                        'hashstr': '.hashstrdbg'}
+
+annotation_file_extension = {'column': '.column.annodbg',
+                             'row': '.row.annodbg'}
+
+
+class TestAnnotate(unittest.TestCase):
+    def setUp(self):
+        self.tempdir = TemporaryDirectory()
+
+    def test_simple_all_graphs(self):
+        """
+        Simple annotation test
+        """
+
+        for graph_repr in ['succinct', 'bitmap', 'hash', 'hashstr']:
+
+            construct_command = '{exe} build -p 4 --graph {repr} -k 20 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=graph_repr,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_100.fa'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 20', params_str[0])
+            self.assertEqual('nodes (k): 46960', params_str[1])
+            self.assertEqual('canonical mode: no', params_str[2])
+
+            for anno_repr in ['row', 'column']:
+                # build annotation
+                annotate_command = '{exe} annotate --anno-header -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation',
+                    input=TEST_DATA_DIR + '/transcripts_100.fa'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  100', params_str[0])
+                self.assertEqual('objects: 46960', params_str[1])
+                self.assertEqual('density: 1.850724e-02', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+    def test_simple_all_graphs_canonical(self):
+        """
+        Simple annotation test
+        """
+
+        for graph_repr in ['succinct', 'bitmap', 'hash']: #, 'hashstr']:
+
+            construct_command = '{exe} build -p 4 --graph {repr} --canonical -k 20 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=graph_repr,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_100.fa'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 20', params_str[0])
+            self.assertEqual('nodes (k): 91584', params_str[1])
+            self.assertEqual('canonical mode: yes', params_str[2])
+
+            for anno_repr in ['row', 'column']:
+                # build annotation
+                annotate_command = '{exe} annotate --anno-header -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation',
+                    input=TEST_DATA_DIR + '/transcripts_100.fa'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  100', params_str[0])
+                self.assertEqual('objects: 91584', params_str[1])
+                self.assertEqual('density: 9.488885e-03', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+    def test_simple_all_graphs_from_kmc(self):
+        """
+        Annotate non-canonical graph constructed from non-canonical KMC database
+        """
+
+        for graph_repr in ['succinct', 'bitmap', 'hash', 'hashstr']:
+
+            construct_command = '{exe} build -p 4 --graph {repr} -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=graph_repr,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 469983', params_str[1])
+            self.assertEqual('canonical mode: no', params_str[2])
+
+            for anno_repr in ['row', 'column']:
+                # build annotation
+                annotate_command = '{exe} annotate --anno-label LabelName -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation',
+                    input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  1', params_str[0])
+                self.assertEqual('objects: 469983', params_str[1])
+                self.assertEqual('density: 1.000000e+00', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+    def test_simple_all_graphs_from_kmc_both(self):
+        """
+        Annotate non-canonical graph constructed from canonical KMC database
+        """
+
+        for graph_repr in ['succinct', 'bitmap', 'hash', 'hashstr']:
+
+            construct_command = '{exe} build -p 4 --graph {repr} -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=graph_repr,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters_both_strands.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 802920', params_str[1])
+            self.assertEqual('canonical mode: no', params_str[2])
+
+            for anno_repr in ['row', 'column']:
+                # build annotation
+                annotate_command = '{exe} annotate --anno-label LabelName -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation_single',
+                    input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation_single' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  1', params_str[0])
+                self.assertEqual('objects: 802920', params_str[1])
+                self.assertEqual('density: 5.853423e-01', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+                # both strands
+                annotate_command = '{exe} annotate --anno-label LabelName -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation_both',
+                    input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters_both_strands.kmc_suf'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation_both' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  1', params_str[0])
+                self.assertEqual('objects: 802920', params_str[1])
+                self.assertEqual('density: 1.000000e+00', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+    def test_simple_all_graphs_from_kmc_both(self):
+        """
+        Annotate canonical graph with k-mers from KMC
+        """
+
+        for graph_repr in ['succinct', 'bitmap', 'hash']: #, 'hashstr']:
+
+            construct_command = '{exe} build -p 4 --graph {repr} --canonical -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=graph_repr,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 802920', params_str[1])
+            self.assertEqual('canonical mode: yes', params_str[2])
+
+            for anno_repr in ['row', 'column']:
+                # build annotation
+                annotate_command = '{exe} annotate --anno-label LabelName -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation_single',
+                    input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation_single' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  1', params_str[0])
+                self.assertEqual('objects: 802920', params_str[1])
+                self.assertEqual('density: 5.000000e-01', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+                # both strands
+                annotate_command = '{exe} annotate --anno-label LabelName -i {graph} \
+                        --anno-type {anno_repr} -o {outfile} {input}'.format(
+                    exe=METAGRAPH,
+                    graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                    anno_repr=anno_repr,
+                    outfile=self.tempdir.name + '/annotation_both',
+                    input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters_both_strands.kmc_suf'
+                )
+                res = subprocess.run([annotate_command], shell=True)
+                self.assertEqual(res.returncode, 0)
+
+                # check annotation
+                anno_stats_command = '{exe} stats -a {annotation}'.format(
+                    exe=METAGRAPH,
+                    annotation=self.tempdir.name + '/annotation_both' + annotation_file_extension[anno_repr],
+                )
+                res = subprocess.run(anno_stats_command.split(), capture_output=True)
+                self.assertEqual(res.returncode, 0)
+                params_str = res.stdout.decode().split('\n')[2:]
+                self.assertEqual('labels:  1', params_str[0])
+                self.assertEqual('objects: 802920', params_str[1])
+                self.assertEqual('density: 5.000000e-01', params_str[2])
+                self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- metagraph/integration_tests/test_build.py
+++ metagraph/integration_tests/test_build.py
@@ -8,7 +8,7 @@ import os
 """Test graph construction"""
 
 METAGRAPH = './metagraph'
-TEST_DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../data'
+TEST_DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../tests/data'
 
 graph_file_extension = {'succinct': '.dbg',
                         'bitmap': '.bitmapdbg',
@@ -50,7 +50,7 @@ class TestBuild(unittest.TestCase):
 
     def test_simple_all_graphs_canonical(self):
         """
-        Simple build test
+        Build simple canonical graphs
         """
 
         for representation in ['succinct', 'bitmap', 'hash']: #, 'hashstr']:
@@ -77,10 +77,6 @@ class TestBuild(unittest.TestCase):
             self.assertEqual('canonical mode: yes', params_str[2])
 
     def test_build_tiny_k(self):
-        """
-        Simple build test
-        """
-
         for representation in ['succinct', 'bitmap', 'hash', 'hashstr']:
             args = [METAGRAPH, 'build', '--graph', representation,
                         '-k', '2',
@@ -102,12 +98,7 @@ class TestBuild(unittest.TestCase):
             self.assertEqual('nodes (k): 16', params_str[1])
             self.assertEqual('canonical mode: no', params_str[2])
 
-
     def test_build_tiny_k_canonical(self):
-        """
-        Simple build test
-        """
-
         for representation in ['succinct', 'bitmap', 'hash']: #, 'hashstr']:
             args = [METAGRAPH, 'build', '--graph', representation, '--canonical',
                         '-k', '2',
@@ -127,6 +118,98 @@ class TestBuild(unittest.TestCase):
             params_str = res.stdout.decode().split('\n')[2:]
             self.assertEqual('k: 2', params_str[0])
             self.assertEqual('nodes (k): 16', params_str[1])
+            self.assertEqual('canonical mode: yes', params_str[2])
+
+    def test_build_from_kmc(self):
+        for representation in ['succinct', 'bitmap', 'hash', 'hashstr']:
+            construct_command = '{exe} build --graph {repr} -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=representation,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[representation],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 469983', params_str[1])
+            self.assertEqual('canonical mode: no', params_str[2])
+
+    def test_build_from_kmc_both(self):
+        for representation in ['succinct', 'bitmap', 'hash', 'hashstr']:
+            construct_command = '{exe} build --graph {repr} -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=representation,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters_both_strands.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[representation],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 802920', params_str[1])
+            self.assertEqual('canonical mode: no', params_str[2])
+
+    def test_build_from_kmc_canonical(self):
+        for representation in ['succinct', 'bitmap', 'hash']: #, 'hashstr']:
+            construct_command = '{exe} build --graph {repr} --canonical -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=representation,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension['succinct'],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 802920', params_str[1])
+            self.assertEqual('canonical mode: yes', params_str[2])
+
+    def test_build_from_kmc_both_canonical(self):
+        for representation in ['succinct', 'bitmap', 'hash']: #, 'hashstr']:
+            construct_command = '{exe} build --graph {repr} --canonical -k 11 -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=representation,
+                outfile=self.tempdir.name + '/graph',
+                input=TEST_DATA_DIR + '/transcripts_1000_kmc_counters_both_strands.kmc_suf'
+            )
+
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            stats_command = '{exe} stats {graph}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension['succinct'],
+            )
+            res = subprocess.run(stats_command.split(), capture_output=True)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 11', params_str[0])
+            self.assertEqual('nodes (k): 802920', params_str[1])
             self.assertEqual('canonical mode: yes', params_str[2])
 
 
