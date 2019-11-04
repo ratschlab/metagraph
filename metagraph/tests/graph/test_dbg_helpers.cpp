c3d193a44e287507b1cb1d943244c46f13b19400
Harun Mustafa <hmusta@users.noreply.github.com>
2019-11-04 11:59:54 +0100

    Update metagraph/tests/graph/test_dbg_helpers.cpp
    Co-Authored-By: Mikhail Karasikov <karasikov@users.noreply.github.com>

--- metagraph/tests/graph/test_dbg_helpers.cpp
+++ metagraph/tests/graph/test_dbg_helpers.cpp
@@ -66,7 +66,7 @@ build_graph<DBGSuccinct>(uint64_t k,
 template <>
 std::shared_ptr<DeBruijnGraph>
 build_graph<DBGSuccinctBloomFPR<1, 1>>(uint64_t k,
-                                         const std::vector<std::string> &sequences,
+                                       const std::vector<std::string> &sequences,
                                        bool canonical) {
     auto graph = build_graph<DBGSuccinct>(k, sequences, canonical);
     dynamic_cast<DBGSuccinct&>(*graph).initialize_bloom_filter(1.0);
