8c3585bef757fe7b125481e979518c34b3de49d3
Mikhail Karasikov <mike-karas@ya.ru>
2019-11-20 20:56:15 +0000

    some optimizations in KmerBloomFilter, added TODOs for further improvements

--- metagraph/src/kmer/kmer_bloom_filter.cpp
+++ metagraph/src/kmer/kmer_bloom_filter.cpp
@@ -12,6 +12,82 @@ typedef KmerExtractorBOSS KmerDef;
 typedef KmerDef::TAlphabet TAlphabet;
 
 
+template <class KmerBF, typename Callback>
+inline void call_kmers(const KmerBF &kmer_bloom,
+                       const char *begin,
+                       const char *end,
+                       Callback callback) {
+    const auto k = kmer_bloom.get_k();
+    if (begin >= end || static_cast<size_t>(end - begin) < k)
+        return;
+
+    const auto max_encoded_val = KmerDef::alphabet.size();
+
+    std::vector<TAlphabet> coded(end - begin);
+    std::transform(begin, end,
+                   coded.begin(),
+                   [](char c) { return KmerDef::encode(c); });
+    auto invalid = utils::drag_and_mark_segments(
+        coded, max_encoded_val, k
+    );
+
+    auto fwd = kmer_bloom.get_hasher();
+    fwd.reset(coded.data());
+
+    if (kmer_bloom.is_canonical_mode()) {
+        std::vector<TAlphabet> rc_coded(end - begin);
+        std::transform(coded.begin(), coded.end(),
+                       rc_coded.rbegin(),
+                       [](TAlphabet c) { return KmerDef::complement(c); });
+
+        auto rev = kmer_bloom.get_hasher();
+        rev.reset(rc_coded.data() + rc_coded.size() - k);
+
+        if (!invalid[k - 1]) {
+            const auto &canonical = std::min(fwd, rev);
+            callback(0,
+                     canonical.template get_hash<0>(),
+                     canonical.template get_hash<1>());
+        }
+
+        for (size_t i = k, j = coded.size() - k - 1; i < coded.size(); ++i, --j) {
+            if (coded.at(i) >= max_encoded_val)
+                continue;
+
+            assert(rc_coded.at(j) < max_encoded_val);
+
+            fwd.next(coded.at(i));
+            rev.prev(rc_coded.at(j));
+
+            if (!invalid[i]) {
+                assert(i + 1 >= k);
+                const auto &canonical = std::min(fwd, rev);
+                callback(i + 1 - k,
+                         canonical.template get_hash<0>(),
+                         canonical.template get_hash<1>());
+            }
+        }
+
+    } else {
+        if (!invalid[k - 1])
+            callback(0, fwd.template get_hash<0>(), fwd.template get_hash<1>());
+
+        for (size_t i = k; i < coded.size(); ++i) {
+            if (coded.at(i) >= max_encoded_val)
+                continue;
+
+            fwd.next(coded.at(i));
+
+            if (!invalid[i]) {
+                assert(i + 1 >= k);
+                callback(i + 1 - k,
+                         fwd.template get_hash<0>(),
+                         fwd.template get_hash<1>());
+            }
+        }
+    }
+}
+
 template <class KmerHasher>
 void KmerBloomFilter<KmerHasher>
 ::add_sequence(const char *begin, const char *end) {
@@ -21,7 +97,8 @@ void KmerBloomFilter<KmerHasher>
     uint64_t counter = 0;
 #endif
 
-    call_kmers(begin, end, [&](auto, auto hash1, auto hash2) {
+    // TODO: insert all hashes in arbitrary order in a single call
+    call_kmers(*this, begin, end, [&](size_t, auto hash1, auto hash2) {
         filter_.insert(hash1, hash2);
         assert(filter_.check(hash1, hash2));
 #ifndef NDEBUG
@@ -45,7 +122,7 @@ sdsl::bit_vector KmerBloomFilter<KmerHasher>
         return sdsl::bit_vector();
 
     sdsl::bit_vector check_vec(end - begin - k_ + 1);
-    call_kmers(begin, end, [&](auto i, auto hash1, auto hash2) {
+    call_kmers(*this, begin, end, [&](size_t i, auto hash1, auto hash2) {
         assert(i < check_vec.size());
 
         if (filter_.check(hash1, hash2))
@@ -75,96 +152,12 @@ bool KmerBloomFilter<KmerHasher>
     try {
         k_ = load_number(in);
         canonical_mode_ = load_number(in);
-        hasher_ = KmerHasherType(k_);
+        const_cast<KmerHasherType&>(hasher_) = KmerHasherType(k_);
 
         return filter_.load(in);
     } catch (...) {
         return false;
     }
-}
-
-template <class KmerHasher>
-void KmerBloomFilter<KmerHasher>
-::call_kmers(const char *begin, const char *end,
-             const std::function<void(size_t /* position */,
-                                      uint64_t /* hash1 */,
-                                      uint64_t /* hash2 */)> &callback) const {
-    if (begin >= end || static_cast<size_t>(end - begin) < k_)
-        return;
-
-    const auto max_encoded_val = KmerDef::alphabet.size();
-
-    std::vector<TAlphabet> coded(end - begin);
-    std::transform(begin, end,
-                   coded.begin(),
-                   [](char c) { return KmerDef::encode(c); });
-    auto invalid = utils::drag_and_mark_segments(
-        coded, max_encoded_val, k_
-    );
-
-    auto fwd = hasher_;
-    fwd.reset(coded.data());
-
-    if (is_canonical_mode()) {
-        std::vector<TAlphabet> rc_coded(end - begin);
-        std::transform(coded.begin(), coded.end(),
-                       rc_coded.rbegin(),
-                       [](TAlphabet c) { return KmerDef::complement(c); });
-
-        auto rev = hasher_;
-        rev.reset(rc_coded.data() + rc_coded.size() - k_);
-
-        if (LIKELY(!invalid[k_ - 1])) {
-            const auto &canonical = std::min(fwd, rev);
-            callback(0,
-                     canonical.template get_hash<0>(),
-                     canonical.template get_hash<1>());
-        }
-
-        for (size_t i = k_, j = coded.size() - k_ - 1; i < coded.size(); ++i, --j) {
-            if (UNLIKELY(coded.at(i) >= max_encoded_val))
-                continue;
-
-            assert(rc_coded.at(j) < max_encoded_val);
-
-            fwd.next(coded.at(i));
-            rev.prev(rc_coded.at(j));
-
-            if (LIKELY(!invalid[i])) {
-                assert(i + 1 >= k_);
-                const auto &canonical = std::min(fwd, rev);
-                callback(i + 1 - k_,
-                         canonical.template get_hash<0>(),
-                         canonical.template get_hash<1>());
-            }
-        }
-
-    } else {
-        if (LIKELY(!invalid[k_ - 1]))
-            callback(0, fwd.template get_hash<0>(), fwd.template get_hash<1>());
-
-        for (size_t i = k_; i < coded.size(); ++i) {
-            if (UNLIKELY(coded.at(i) >= max_encoded_val))
-                continue;
-
-            fwd.next(coded.at(i));
-
-            if (LIKELY(!invalid[i])) {
-                assert(i + 1 >= k_);
-                callback(i + 1 - k_,
-                         fwd.template get_hash<0>(),
-                         fwd.template get_hash<1>());
-            }
-        }
-    }
-}
-
-template <class KmerHasher>
-void KmerBloomFilter<KmerHasher>
-::print_stats() const {
-    std::cout << "Bloom filter parameters" << std::endl
-              << "Size:\t\t\t" << size() << " bits" << std::endl
-              << "Num hash functions:\t" << num_hash_functions() << std::endl;
 }
 
 template class KmerBloomFilter<>;
