4b2f39faf0a88128106c71869bbbed6204702ba8
Daniel Danciu <dd@daedalean.ai>
2019-12-04 22:36:07 +0100

    Small tweaks, added readme

--- metagraph/scripts/cloud/main.py
+++ metagraph/scripts/cloud/main.py
@@ -1,8 +1,41 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
 import time
 
 import googleapiclient.discovery
+
+
+def wait_for_all_operations(compute, project, zone, ids):
+    print('Waiting for *all* operations to finish...', end='', flush=True)
+    sleep_time_sec = 1
+    while True:
+        if count_pending_operations(compute, project, zone, ids) == 0:
+            print("done.")
+            return
+        print('.', end='', flush=True)
+        time.sleep(sleep_time_sec)
+        if sleep_time_sec < 16:
+            sleep_time_sec *= 2
+
+
+def wait_for_operation(compute, project, zone, operation):
+    print('Waiting for operation to finish...', end='', flush=True)
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation
+        ).execute()
+        print('.', end='', flush=True)
+        if result['status'] == 'DONE':
+            print("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
 
 
 def list_instances(compute, project, zone):
@@ -14,8 +47,8 @@ def list_instances(compute, project, zone):
         print(' - ' + instance['name'])
 
 
-def delete_instance_no_wait(compute, project, zone, name):
-    """ Deletes the instance with the given name """
+def send_delete_request(compute, project, zone, name):
+    """ Sends a request to delete the instance with the given name """
     print('Deleting instance %s ...' % name)
     return compute.instances().delete(
         project=project,
@@ -23,42 +56,53 @@ def delete_instance_no_wait(compute, project, zone, name):
         instance=name).execute()
 
 
-def delete_instance(compute, project, zone, name):
-    """ Deletes the instance with the given name """
-    operation = delete_instance_no_wait(compute, project, zone, name)
-    wait_for_operation(compute, project, zone, operation['name'])
-
-
 def delete_instances(compute, project, zone, name, count):
     ids = []
     for i in range(count):
-        operation = delete_instance_no_wait(compute, project, zone, name + '-' + str(i))
+        operation = send_delete_request(compute, project, zone, name + '-' + str(i))
         ids.append(operation['id'])
 
     wait_for_all_operations(compute, project, zone, ids)
 
-def start_instance(compute, project, zone, name):
-    """ Starts the instance with the given name, error if instance doesn't exist """
+
+def send_start_request(compute, project, zone, name):
+    """ Sends a request to start the instance with the given name """
     print('Starting instance %s ...' % name)
-    operation = compute.instances().start(
+    return compute.instances().start(
         project=project,
         zone=zone,
         instance=name).execute()
-    wait_for_operation(compute, project, zone, operation['name'])
 
 
-def stop_instance(compute, project, zone, name):
-    """ Stops the instance with the given name """
+def start_instances(compute, project, zone, name, count):
+    """ Starts count instances with the given name prefix. The names will be name-0, ..., name-count-1 """
+    ids = []
+    for i in range(count):
+        operation = send_start_request(compute, project, zone, name + '-' + str(i))
+        ids.append(operation['id'])
+    wait_for_all_operations(compute, project, zone, ids)
+
+
+def send_stop_request(compute, project, zone, name):
+    """ Sends a request to stop  the instance with the given name """
     print('Stopping instance %s ...' % name)
-    operation = compute.instances().stop(
+    return compute.instances().stop(
         project=project,
         zone=zone,
         instance=name).execute()
-    wait_for_operation(compute, project, zone, operation['name'])
 
 
-def create_instance_no_wait(compute, project, zone, name):
-    """ Creates a new instance with the given parameters """
+def stop_instances(compute, project, zone, name, count):
+    """ Stops count instances with the given name prefix. The names will be name-0, ..., name-count-1 """
+    ids = []
+    for i in range(count):
+        operation = send_stop_request(compute, project, zone, name + '-' + str(i))
+        ids.append(operation['id'])
+    wait_for_all_operations(compute, project, zone, ids)
+
+
+def send_create_request(compute, project, zone, name, startup_script_name):
+    """ Send a request to creates a new instance with the given parameters """
     print('Creating instance %s ...' % name)
     # Get the metagraph Ubuntu 18.04 TLS image
     image_response = compute.images().getFromFamily(
@@ -67,10 +111,16 @@ def create_instance_no_wait(compute, project, zone, name):
 
     # Configure the machine
     machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
-    startup_script = open(
-        os.path.join(
-            os.path.dirname(__file__), 'startup_script.sh'), 'r').read()
-
+    metadata = [{
+        'key': 'instance_id',
+        'value': name.split('-')[-1]}]
+    if startup_script_name != '':
+        startup_script = open(startup_script_name, 'r').read()
+        metadata.append({
+            # Startup script is automatically executed by the instance upon startup.
+            'key': 'startup-script',
+            'value': startup_script
+        })
     config = {
         'name': name,
         'machineType': machine_type,
@@ -104,15 +154,10 @@ def create_instance_no_wait(compute, project, zone, name):
             ]
         }],
 
-        # Metadata is readable from the instance and allows you to
-        # pass configuration from deployment scripts to instances.
+        # Metadata is readable from the instance so we can use it to
+        # pass configuration options to our instances
         'metadata': {
-            'items': [{
-                # Startup script is automatically executed by the
-                # instance upon startup.
-                'key': 'startup-script',
-                'value': startup_script
-            }]
+            'items': metadata
         }
     }
 
@@ -123,49 +168,13 @@ def create_instance_no_wait(compute, project, zone, name):
     return operation
 
 
-def create_instance(compute, project, zone, name):
-    operation = create_instance_no_wait(compute, project, zone, name)
-    wait_for_operation(compute, project, zone, operation['name'])
-
-
-def create_instances(compute, project, zone, name, count):
+def create_instances(compute, project, zone, name, count, startup_script):
     ids = []
     for i in range(count):
-        operation = create_instance_no_wait(compute, project, zone, name + '-' + str(i))
+        operation = send_create_request(compute, project, zone, name + '-' + str(i), startup_script)
         ids.append(operation['id'])
 
     wait_for_all_operations(compute, project, zone, ids)
-
-
-def wait_for_all_operations(compute, project, zone, ids):
-    print('Waiting for *all* operations to finish...', end='', flush=True)
-    sleep_time_sec = 1
-    while True:
-        if count_pending_operations(compute, project, zone, ids) == 0:
-            print("done.")
-            return
-        print('.', end='', flush=True)
-        time.sleep(sleep_time_sec)
-        if sleep_time_sec<16:
-            sleep_time_sec *= 2
-
-
-def wait_for_operation(compute, project, zone, operation):
-    print('Waiting for operation to finish...', end='', flush=True)
-    while True:
-        result = compute.zoneOperations().get(
-            project=project,
-            zone=zone,
-            operation=operation
-        ).execute()
-        print('.', end='', flush=True)
-        if result['status'] == 'DONE':
-            print("done.")
-            if 'error' in result:
-                raise Exception(result['error'])
-            return result
-
-        time.sleep(1)
 
 
 def count_pending_operations(compute, project, zone, operation_ids):
@@ -197,19 +206,21 @@ if __name__ == '__main__':
         '--name', default='metagraph', help='Name (or prefix) of instances to perform the action on')
     parser.add_argument('-n', '--num_instances', default=1, type=int, choices=range(1, 50),
                         help='Number of instances to create/start')
+    parser.add_argument('--script', default='',
+                        help='Optional name of script to run at creation time')
 
     args = parser.parse_args()
 
     compute = googleapiclient.discovery.build('compute', 'v1')
     if args.action == 'create' or args.action == 'c':
-        create_instances(compute, args.project_id, args.zone, args.name, args.num_instances)
+        create_instances(compute, args.project_id, args.zone, args.name, args.num_instances, args.script)
     elif args.action == 'delete' or args.action == 'd':
         delete_instances(compute, args.project_id, args.zone, args.name, args.num_instances)
     elif args.action == 'list' or args.action == 'l':
         list_instances(compute, args.project_id, args.zone)
     elif args.action == 'stop':
-        stop_instance(compute, args.project_id, args.zone, args.name)
+        stop_instances(compute, args.project_id, args.zone, args.name, args.num_instances)
     elif args.action == 'start':
-        start_instance(compute, args.project_id, args.zone, args.name)
+        start_instances(compute, args.project_id, args.zone, args.name, args.num_instances)
     else:
         print('Invalid action %s' % args.action)
--- metagraph/scripts/cloud/startup_script.sh
+++ metagraph/scripts/cloud/startup_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Boo!"
+echo "Boo!" > /tmp/boo
