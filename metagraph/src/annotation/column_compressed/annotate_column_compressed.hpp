d5c42626514194c15668913a0eaac774d511a3f4
Mikhail Karasikov <karasikov@users.noreply.github.com>
2019-12-19 16:04:28 +0100

    construct new ColumnCompressed columns with SortedSet (#105)
    * construct new columns with SortedSet
    
    * improvements
    
    allocate a buffer for 10M in column builder in advance
    
    removed redundant sorting: in sorted set everything will be sorted anyway
    
    * addressed the review comments
    
    * optimized builder choice for small columns
    
    initialize uncompressed columns in the first place for columns smaller than 640M in size

--- metagraph/src/annotation/column_compressed/annotate_column_compressed.hpp
+++ metagraph/src/annotation/column_compressed/annotate_column_compressed.hpp
@@ -106,8 +106,9 @@ class ColumnCompressed : public MultiLabelEncoded<uint64_t, Label> {
                     ProgressBar *progress_bar) const;
     void release();
     void flush() const;
-    void flush(size_t j, const bitmap &annotation_curr);
-    bitmap_dyn& decompress(size_t j);
+    void flush(size_t j, const bitmap_builder &column_builder);
+    bitmap_builder& decompress_builder(size_t j);
+    bitmap_dyn& decompress_bitmap(size_t j);
     const bitmap& get_column(size_t j) const;
 
     SetBitPositions get_label_codes(Index i) const override;
@@ -119,7 +120,7 @@ class ColumnCompressed : public MultiLabelEncoded<uint64_t, Label> {
     std::vector<std::unique_ptr<bit_vector>> bitmatrix_;
 
     caches::fixed_sized_cache<size_t,
-                              bitmap_dyn*,
+                              bitmap_builder*,
                               caches::LRUCachePolicy<size_t>> cached_columns_;
 
     LabelEncoder<Label> &label_encoder_ {
