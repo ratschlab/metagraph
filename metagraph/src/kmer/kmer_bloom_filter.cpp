985c9c92b67162edb13be7cc8f0b0720dd149442
Harun Mustafa <bilgisayaradami@gmail.com>
2019-12-20 15:28:32 +0100

    Added call_valid_kmers. Fixed assert in add_sequences

--- metagraph/src/kmer/kmer_bloom_filter.cpp
+++ metagraph/src/kmer/kmer_bloom_filter.cpp
@@ -83,37 +83,86 @@ inline void call_kmers(const KmerBF &kmer_bloom,
     }
 }
 
+template <class KmerBF>
+inline void call_valid_kmers(const KmerBF &kmer_bloom,
+                             const char *begin,
+                             const char *end,
+                             const std::function<void(uint64_t)> &callback) {
+    const auto k = kmer_bloom.get_k();
+    if (begin >= end || static_cast<size_t>(end - begin) < k)
+        return;
+
+    const auto max_encoded_val = KmerDef::alphabet.size();
+
+    std::vector<TAlphabet> coded(end - begin);
+    std::transform(begin, end,
+                   coded.begin(),
+                   [](char c) { return KmerDef::encode(c); });
+
+    auto invalid = utils::drag_and_mark_segments(coded, max_encoded_val, k);
+
+    auto fwd = kmer_bloom.get_hasher();
+    fwd.reset(coded.data());
+
+    if (kmer_bloom.is_canonical_mode()) {
+        std::vector<TAlphabet> rc_coded(end - begin);
+        std::transform(coded.begin(), coded.end(),
+                       rc_coded.rbegin(),
+                       [](TAlphabet c) { return KmerDef::complement(c); });
+
+        auto rev = kmer_bloom.get_hasher();
+        rev.reset(rc_coded.data() + rc_coded.size() - k);
+
+        if (!invalid[k - 1])
+            callback(std::min(uint64_t(fwd), uint64_t(rev)));
+
+        for (size_t i = k, j = coded.size() - k - 1; i < coded.size(); ++i, --j) {
+            if (coded.at(i) < max_encoded_val) {
+                assert(rc_coded.at(j) < max_encoded_val);
+
+                fwd.next(coded.at(i));
+                rev.prev(rc_coded.at(j));
+
+                assert(invalid[i] || i + 1 >= k);
+                if (!invalid[i])
+                    callback(std::min(uint64_t(fwd), uint64_t(rev)));
+            }
+        }
+
+    } else {
+        if (!invalid[k - 1])
+            callback(fwd);
+
+        for (size_t i = k; i < coded.size(); ++i) {
+            if (coded.at(i) < max_encoded_val) {
+                fwd.next(coded.at(i));
+
+                assert(invalid[i] || i + 1 >= k);
+                if (!invalid[i])
+                    callback(fwd);
+            }
+        }
+    }
+}
+
 template <class KmerHasher>
 void KmerBloomFilter<KmerHasher>
 ::add_sequence(const char *begin, const char *end) {
     assert(end >= begin && static_cast<size_t>(end - begin) >= k_);
 
-#ifndef NDEBUG
-    size_t counter = 0;
-#endif
-
     AlignedVector<uint64_t> hashes;
     hashes.reserve(end - begin - k_ + 1);
-    call_kmers(*this, begin, end, [&](auto hash, bool is_valid) {
-        if (is_valid) {
-            hashes.push_back(hash);
-
-#ifndef NDEBUG
-            ++counter;
-#endif
-        }
-
-    });
-
+    call_valid_kmers(*this, begin, end, [&](uint64_t hash) { hashes.push_back(hash); });
     filter_.insert(hashes.data(), hashes.data() + hashes.size());
 
-    assert(sdsl::util::cnt_one_bits(check_kmer_presence(begin, end)) == counter);
+    // invalid k-mers may be false positives
+    assert(sdsl::util::cnt_one_bits(check_kmer_presence(begin, end)) >= hashes.size());
 
 #ifndef NDEBUG
     std::string rev_comp(begin, end);
     reverse_complement(rev_comp.begin(), rev_comp.end());
     assert(!canonical_mode_
-            || sdsl::util::cnt_one_bits(check_kmer_presence(rev_comp)) == counter);
+            || sdsl::util::cnt_one_bits(check_kmer_presence(rev_comp)) >= hashes.size());
 #endif
 
 }
@@ -133,11 +182,8 @@ void KmerBloomFilter<KmerHasher>
             buffer.clear();
         }
 
-        call_kmers(*this, sequence.c_str(), sequence.c_str() + sequence.size(),
-                   [&](auto hash, bool is_valid) {
-                       if (is_valid)
-                           buffer.push_back(hash);
-                   });
+        call_valid_kmers(*this, sequence.c_str(), sequence.c_str() + sequence.size(),
+                         [&](uint64_t hash) { buffer.push_back(hash); });
     });
 
     filter_.insert(buffer.data(), buffer.data() + buffer.size());
