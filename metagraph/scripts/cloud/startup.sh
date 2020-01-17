491b0ecb805801ee9dd8c50774ed4498f08d2391
Daniel Danciu <dd@daedalean.ai>
2020-01-17 14:26:44 +0100

    Same, but better

--- metagraph/scripts/cloud/startup.sh
+++ metagraph/scripts/cloud/startup.sh
@@ -10,4 +10,39 @@ sudo mount /dev/nvme0n1 /mnt/disks/ssd
 sudo chmod a+w /mnt/disks/ssd
 echo UUID=`sudo blkid -s UUID -o value /dev/disk/by-id/google-local-nvme-ssd-0` /mnt/disks/ssd ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab
 
-sudo -u ddanciu /home/ddanciu/projects2014-metagenome/metagraph/scripts/cloud/start_client.sh
+# starts the cloud metagraph client
+function start_client() {
+  echo "Executing script as: $(whoami)"
+
+  # get the id of the instance from the metadata server and place it into a file
+  response=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance_id -H "Metadata-Flavor: Google" --write-out  %{http_code} --silent --output /dev/null)
+  if ((response == 200)); then
+    curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance_id -H "Metadata-Flavor: Google" > /tmp/instance_id
+    instance_id=$(cat /tmp/instance_id)
+  else
+    instance_id='-1'
+  fi
+  response=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/server_host -H "Metadata-Flavor: Google" --write-out  %{http_code} --silent --output /dev/null)
+  if ((response == 200)); then
+    curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/server_host -H "Metadata-Flavor: Google" > /tmp/server_host
+    server_host=$(cat /tmp/server_host)
+  else
+    server_host='127.0.0.1'
+  fi
+
+  metagraph_path="$HOME/projects2014-metagenome/metagraph"
+  cd "$metagraph_path"
+  git checkout dd/cloud  # TODO: remove this at some point
+  git pull origin dd/cloud
+
+  mkdir -p build
+  cd build
+  cmake ..
+  make -j metagraph
+
+  cd "$metagraph_path/scripts/cloud"
+  python3 ./client.py --server_host="${server_host}" --client_id="${instance_id}" --output_dir="/mnt/disks/ssd/"
+}
+
+export -f start_client
+sudo -u ddanciu "bash -c start_client"
