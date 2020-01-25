10ad491dc23bfa243700528785f0d60f3675dda2
Harun Mustafa <bilgisayaradami@gmail.com>
2020-01-25 22:25:02 +0100

    bench test

--- metagraph/benchmarks/benchmark_matrix.cpp
+++ metagraph/benchmarks/benchmark_matrix.cpp
@@ -19,17 +19,25 @@ std::vector<double> get_densities(uint64_t num_cols,
     return densities;
 }
 
-static void BM_BRWTCompress(benchmark::State& state) {
+template <size_t density_numerator,
+          size_t density_denominator,
+          size_t rows_arg = 1000000,
+          size_t cols_arg = 1000,
+          size_t unique_arg = 100>
+static void BM_BRWTCompressSparse(benchmark::State& state) {
     DataGenerator generator;
     generator.set_seed(42);
 
     for (auto _ : state) {
+        auto density_arg = std::vector<double>(
+            unique_arg, double(density_numerator) / double(density_denominator)
+        );
         std::vector<std::unique_ptr<bit_vector>> generated_columns;
         generated_columns = generator.generate_random_columns(
-            1'000'000,
-            100,
-            get_densities(100, std::vector<double>(100, 0.01)),
-            std::vector<uint32_t>(100, 1'000 / 100)
+            rows_arg,
+            unique_arg,
+            get_densities(unique_arg, density_arg),
+            std::vector<uint32_t>(unique_arg, cols_arg / unique_arg)
         );
 
         std::unique_ptr<BinaryMatrix> matrix;
@@ -43,4 +51,6 @@ static void BM_BRWTCompress(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_BRWTCompress)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_BRWTCompressSparse, 1, 10)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_BRWTCompressSparse, 1, 100)->Unit(benchmark::kMillisecond);
+BENCHMARK_TEMPLATE(BM_BRWTCompressSparse, 1, 1000)->Unit(benchmark::kMillisecond);
