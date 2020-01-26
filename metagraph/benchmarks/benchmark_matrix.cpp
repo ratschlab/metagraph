f34b31c7c234a0eb44cabfbfde7493e23785b974
Harun Mustafa <bilgisayaradami@gmail.com>
2020-01-26 14:32:03 +0100

    generation outside of timed loop

--- metagraph/benchmarks/benchmark_matrix.cpp
+++ metagraph/benchmarks/benchmark_matrix.cpp
@@ -23,30 +23,33 @@ template <size_t density_numerator,
           size_t density_denominator,
           size_t rows_arg = 1000000,
           size_t cols_arg = 1000,
-          size_t unique_arg = 100>
+          size_t unique_arg = 100,
+          size_t arity_arg = 2,
+          bool greedy_arg = true,
+          size_t relax_arg = 2>
 static void BM_BRWTCompressSparse(benchmark::State& state) {
     DataGenerator generator;
     generator.set_seed(42);
 
+    auto density_arg = std::vector<double>(
+        unique_arg, double(density_numerator) / double(density_denominator)
+    );
+    std::vector<std::unique_ptr<bit_vector>> generated_columns;
+    generated_columns = generator.generate_random_columns(
+        rows_arg,
+        unique_arg,
+        get_densities(unique_arg, density_arg),
+        std::vector<uint32_t>(unique_arg, cols_arg / unique_arg)
+    );
+
+    std::unique_ptr<BinaryMatrix> matrix;
+
     for (auto _ : state) {
-        auto density_arg = std::vector<double>(
-            unique_arg, double(density_numerator) / double(density_denominator)
-        );
-        std::vector<std::unique_ptr<bit_vector>> generated_columns;
-        generated_columns = generator.generate_random_columns(
-            rows_arg,
-            unique_arg,
-            get_densities(unique_arg, density_arg),
-            std::vector<uint32_t>(unique_arg, cols_arg / unique_arg)
-        );
-
-        std::unique_ptr<BinaryMatrix> matrix;
-
         matrix = generate_brwt_from_rows(
             std::move(generated_columns),
-            2,
-            true,
-            2
+            arity_arg,
+            greedy_arg,
+            relax_arg
         );
     }
 }
