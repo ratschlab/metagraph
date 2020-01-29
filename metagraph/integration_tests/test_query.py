9fed628ddbc204fe9d6b4d1047345467a5a5b281
Mikhail Karasikov <karasikov@users.noreply.github.com>
2020-01-29 17:10:37 +0100

    Limit query graph size, fasta parser with iterators (#123)
    * limited query graph size
    
    * fast copying of iterator without reopening the file
    
    * minor
    
    * minor
    
    * addressed review comments

--- metagraph/integration_tests/test_query.py
+++ metagraph/integration_tests/test_query.py
@@ -255,6 +255,78 @@ class TestQuery(unittest.TestCase):
             self.assertEqual(res.returncode, 0)
             self.assertEqual(len(res.stdout), 136959)
 
+    @parameterized.expand(GRAPH_TYPES)
+    def test_query_all_graphs_tiny_batch(self, graph_repr):
+
+        construct_command = '{exe} build -p {num_threads} \
+                --graph {repr} -k 20 -o {outfile} {input}'.format(
+            exe=METAGRAPH,
+            num_threads=NUM_THREADS,
+            repr=graph_repr,
+            outfile=self.tempdir.name + '/graph',
+            input=TEST_DATA_DIR + '/transcripts_100.fa'
+        )
+
+        res = subprocess.run([construct_command], shell=True)
+        self.assertEqual(res.returncode, 0)
+
+        stats_command = '{exe} stats {graph}'.format(
+            exe=METAGRAPH,
+            graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+        )
+        res = subprocess.run(stats_command.split(), stdout=PIPE, stderr=PIPE)
+        self.assertEqual(res.returncode, 0)
+        params_str = res.stdout.decode().split('\n')[2:]
+        self.assertEqual('k: 20', params_str[0])
+        self.assertEqual('nodes (k): 46960', params_str[1])
+        self.assertEqual('canonical mode: no', params_str[2])
+
+        for anno_repr in ['row', 'column']:
+            # build annotation
+            annotate_command = '{exe} annotate --anno-header -i {graph} \
+                    --anno-type {anno_repr} -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                anno_repr=anno_repr,
+                outfile=self.tempdir.name + '/annotation',
+                input=TEST_DATA_DIR + '/transcripts_100.fa'
+            )
+            res = subprocess.run([annotate_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            # check annotation
+            anno_stats_command = '{exe} stats -a {annotation}'.format(
+                exe=METAGRAPH,
+                annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
+            )
+            res = subprocess.run(anno_stats_command.split(), stdout=PIPE, stderr=PIPE)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('labels:  100', params_str[0])
+            # self.assertEqual('objects: 46960', params_str[1])
+            self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+            # query graph
+            query_command = '{exe} query --fast --batch-size 100 -i {graph} -a {annotation} {input}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
+                input=TEST_DATA_DIR + '/transcripts_1000.fa'
+            )
+            res = subprocess.run(query_command.split(), stdout=PIPE, stderr=PIPE)
+            self.assertEqual(res.returncode, 0)
+            self.assertEqual(len(res.stdout), 137140)
+
+            query_command = '{exe} query --fast --batch-size 100 --count-labels -i {graph} -a {annotation} {input}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
+                input=TEST_DATA_DIR + '/transcripts_1000.fa'
+            )
+            res = subprocess.run(query_command.split(), stdout=PIPE, stderr=PIPE)
+            self.assertEqual(res.returncode, 0)
+            self.assertEqual(len(res.stdout), 136959)
+
 
 class TestQueryCanonical(unittest.TestCase):
     def setUp(self):
@@ -476,6 +548,78 @@ class TestQueryCanonical(unittest.TestCase):
             self.assertEqual(len(res.stdout), 137269)
 
             query_command = '{exe} query --fast --count-labels -i {graph} -a {annotation} {input}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
+                input=TEST_DATA_DIR + '/transcripts_1000.fa'
+            )
+            res = subprocess.run(query_command.split(), stdout=PIPE, stderr=PIPE)
+            self.assertEqual(res.returncode, 0)
+            self.assertEqual(len(res.stdout), 137093)
+
+    @parameterized.expand(['succinct', 'bitmap', 'hash'])  # 'hashstr'
+    def test_query_all_graphs_batch_tiny_batch(self, graph_repr):
+
+        construct_command = '{exe} build --canonical -p {num_threads} \
+                --graph {repr} -k 20 -o {outfile} {input}'.format(
+            exe=METAGRAPH,
+            num_threads=NUM_THREADS,
+            repr=graph_repr,
+            outfile=self.tempdir.name + '/graph',
+            input=TEST_DATA_DIR + '/transcripts_100.fa'
+        )
+
+        res = subprocess.run([construct_command], shell=True)
+        self.assertEqual(res.returncode, 0)
+
+        stats_command = '{exe} stats {graph}'.format(
+            exe=METAGRAPH,
+            graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+        )
+        res = subprocess.run(stats_command.split(), stdout=PIPE, stderr=PIPE)
+        self.assertEqual(res.returncode, 0)
+        params_str = res.stdout.decode().split('\n')[2:]
+        self.assertEqual('k: 20', params_str[0])
+        self.assertEqual('nodes (k): 91584', params_str[1])
+        self.assertEqual('canonical mode: yes', params_str[2])
+
+        for anno_repr in ['row', 'column']:
+            # build annotation
+            annotate_command = '{exe} annotate --anno-header -i {graph} \
+                    --anno-type {anno_repr} -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                anno_repr=anno_repr,
+                outfile=self.tempdir.name + '/annotation',
+                input=TEST_DATA_DIR + '/transcripts_100.fa'
+            )
+            res = subprocess.run([annotate_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            # check annotation
+            anno_stats_command = '{exe} stats -a {annotation}'.format(
+                exe=METAGRAPH,
+                annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
+            )
+            res = subprocess.run(anno_stats_command.split(), stdout=PIPE, stderr=PIPE)
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('labels:  100', params_str[0])
+            self.assertEqual('objects: 91584', params_str[1])
+            self.assertEqual('representation: ' + anno_repr, params_str[3])
+
+            # query graph
+            query_command = '{exe} query --fast --batch-size 100 -i {graph} -a {annotation} {input}'.format(
+                exe=METAGRAPH,
+                graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
+                annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
+                input=TEST_DATA_DIR + '/transcripts_1000.fa'
+            )
+            res = subprocess.run(query_command.split(), stdout=PIPE, stderr=PIPE)
+            self.assertEqual(res.returncode, 0)
+            self.assertEqual(len(res.stdout), 137269)
+
+            query_command = '{exe} query --fast --batch-size 100 --count-labels -i {graph} -a {annotation} {input}'.format(
                 exe=METAGRAPH,
                 graph=self.tempdir.name + '/graph' + graph_file_extension[graph_repr],
                 annotation=self.tempdir.name + '/annotation' + anno_file_extension[anno_repr],
