f32e78ab34481a7a8e108a1162db084f1f9d5636
Daniel Danciu <dd@daedalean.ai>
2020-01-30 17:53:55 +0100

    A new client/server architecture that doesn't copy data around at the price of reduced parallelism and less efficient CPU usage

--- metagraph/scripts/cloud/server2.py
+++ metagraph/scripts/cloud/server2.py
@@ -4,7 +4,6 @@ import argparse
 import buckets
 import cgi
 import errno
-import fileinput
 import http.server
 import json
 import logging
@@ -16,23 +15,22 @@ import urllib
 
 args = None  # the parsed command line arguments
 
-# jobs done
-downloaded_sras = {}  # the downloaded sras
-created_sras = {}  # the sras for which a BOSS graph was generated
-cleaned_sras = {}  # the sras for which the BOSS graph was cleaned (so processing is completed)
-transferred_sras = {}  # the sras that were successfully transferred to permanent storage
-
-# jobs in the queue (waiting for a worker)
 sra_download_gen = None  # the generator function that returns the next SRA id to be downloaded
-to_create_sras = {}
-to_clean_sras = {}
-to_transfer_sras = {}
+
+# jobs done
+downloaded_sras = set()  # the downloaded sras
+created_sras = set()  # the sras for which a BOSS graph was generated
+cleaned_sras = set()  # the sras for which the BOSS graph was cleaned (so processing is completed)
+transferred_sras = set()  # the sras that were successfully transferred to permanent storage
 
 # jobs currently being processed by a worker
+pending_jobs = {}  # includes jobs that were pre-empted
 pending_downloads = set()
 pending_creates = set()
 pending_cleans = set()
 pending_transfers = set()
+
+preempted_ids = set()
 
 # set to true when all download ids were read
 download_done = False
@@ -49,16 +47,17 @@ status_str = f"""
 <p>Pending clean: %s</p>
 <p>Pending transfer: %s</p>
 
-<h3> Jobs waiting to be scheduled </h3>
-<p>Waiting create: %s</p>
-<p>Waiting clean: %s</p>
-<p>Waiting transfer: %s</p>
+<h3> Jobs preempted </h3>
+<p>%s</p>
 
 <h3> Jobs completed </h3>
 <p>Completed downloads: %s </p>
 <p>Completed create: %s</p>
 <p>Completed clean: %s</p>
 <p>Completed transfer: %s</p>
+
+<h3> Download status </h3>
+Download done: %s
 
 </body>
 </html>
@@ -72,66 +71,43 @@ def get_var(post_vars, var):
     return var_list[0].decode('utf-8')
 
 
-def should_download():
-    """ Returns true if it makes sense to download more data for processing, e.g. if we don't already have a
-    large number of downloads waiting to be processed"""
-
-    return len(to_create_sras) < 10 and len(to_create_sras) < 3 * args.worker_count
-
-
-def should_create():
-    """ Returns true if it makes sense to create rather than clean """
-
-    return len(to_create_sras) <= len(to_clean_sras) or len(to_clean_sras) < 4 * args.worker_count
-
-
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     """ Processes requests for new jobs or for acknowledging finished jobs """
 
-    def handle_get_job(self, params):
+    def handle_get_job(self):
         """ Handles a request for a new job """
 
-        download_jobs = params.get('download')
-        create_jobs = params.get('create')
-        clean_jobs = params.get('clean')
-        if None in (download_jobs, create_jobs, clean_jobs):
-            self.send_reply(400, "One of 'download', 'create' or 'clean' parameters is missing\n")
-            return
         global download_done
-        if download_done and not (
-                to_create_sras or to_clean_sras or to_transfer_sras or pending_downloads or pending_creates
-                or pending_cleans or pending_transfers):
-            self.send_reply(204, 'All done, feel free to exit and pat yourself on the back')
         response = {}
-        if download_jobs[0] == '0' and should_download():
-            try:
+        try:
+            if preempted_ids:
+                sra_id = preempted_ids.pop()
+                bucket = pending_jobs[sra_id]
+            else:
                 sra_id_line = next(sra_download_gen)
-                response['download'] = {'id': sra_id_line[0]}
+                sra_id = sra_id_line[0]
                 if len(sra_id_line) == 2:
-                    response['download']['bucket'] = sra_id_line[1]
-                pending_downloads.add(sra_id_line[0])
-            except StopIteration:
-                download_done = True  # nothing else to download
-        create_jobs_int = int(create_jobs[0])
-        clean_jobs_int = int(clean_jobs[0])
-        if create_jobs_int == 0:  # if we have a create job, we're done - the machine is full
-            if to_create_sras and clean_jobs_int == 0 and should_create():
-                sra_id, directory = to_create_sras.popitem()
-                response['create'] = {'id': sra_id, 'location': directory}
-                pending_creates.add(sra_id)
-            elif to_clean_sras and (0 <= clean_jobs_int < 4):  # there is room for more clean jobs
-                sra_id, dbg_file = to_clean_sras.popitem()
-                response['clean'] = {'id': sra_id, 'location': dbg_file}
-                pending_cleans.add(sra_id)
+                    bucket = sra_id_line[1]
+                else:
+                    bucket = None
+                pending_jobs[sra_id] = bucket
+            response['download'] = {'id': sra_id}
+            if bucket:
+                response['download']['bucket'] = bucket
+            pending_downloads.add(sra_id)
+
+        except StopIteration:
+            download_done = True  # nothing else to download
+
         self.send_reply(200, json.dumps(response), {'Content-type': 'application/json'})
 
     def handle_get_status(self):
         self.send_reply(200, status_str % (
-            pending_downloads, pending_creates, pending_cleans, pending_transfers, to_create_sras, to_clean_sras,
-            to_transfer_sras, downloaded_sras, created_sras, cleaned_sras, transferred_sras),
+            pending_downloads, pending_creates, pending_cleans, pending_transfers, preempted_ids, downloaded_sras,
+            created_sras, cleaned_sras, transferred_sras, download_done),
                         {'Content-type': 'text/html'})
 
-    def handle_ack(self, operation, post_vars, add_maps, pending_operations):
+    def handle_ack(self, operation, post_vars, add_sets, pending_operations):
         sra_id = get_var(post_vars, 'id')
         location = get_var(post_vars, 'location')
         if None in (sra_id, location):
@@ -139,9 +115,9 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             return False
         filename = os.path.join(args.output_dir, f'succeed_{operation}.id')
         with open(filename, 'a') as fp:
-            fp.write(f'{sra_id} {location}\n')
-        for m in add_maps:
-            m[sra_id] = location
+            fp.write(f'{sra_id}\n')
+        for s in add_sets:
+            s.add(sra_id)
         self.send_reply(200, "")
         try:
             pending_operations.remove(sra_id)
@@ -164,19 +140,20 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             logging.warning(f'Acknowledging nonexistent pending {operation} {sra_id}')
 
     def handle_ack_download(self, post_vars):
-        self.handle_ack('download', post_vars, [downloaded_sras, to_create_sras], pending_downloads)
+        self.handle_ack('download', post_vars, [downloaded_sras, pending_creates], pending_downloads)
 
     def handle_ack_create(self, post_vars):
-        self.handle_ack('create', post_vars, [created_sras, to_clean_sras], pending_creates)
+        self.handle_ack('create', post_vars, [created_sras, pending_cleans], pending_creates)
 
     def handle_ack_clean(self, post_vars):
-        if self.handle_ack('clean', post_vars, [cleaned_sras, to_transfer_sras], pending_cleans):
+        if self.handle_ack('clean', post_vars, [cleaned_sras, pending_transfers], pending_cleans):
             # a transfer is automatically started after a clean operation by the client
             pending_transfers.add(get_var(post_vars, 'id'))
 
     def handle_ack_transfer(self, post_vars):
         self.handle_ack('transfer', post_vars, [transferred_sras], pending_transfers)
-        del to_transfer_sras[get_var(post_vars, 'id')]
+        sra_id = get_var(post_vars, 'id')
+        del pending_jobs[sra_id]
 
     def handle_nack_download(self, post_vars):
         self.handle_nack('download', post_vars, pending_downloads)
@@ -189,6 +166,19 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def handle_nack_transfer(self, post_vars):
         self.handle_nack('transfer', post_vars, pending_transfers)
+
+    def handle_preempt(self, post_vars):
+        sra_ids = get_var(post_vars, 'ids')
+        if sra_ids is None:
+            self.send_reply(400, 'No ids specified. Need at least one')
+            return
+        for sra_id in sra_ids.split(','):
+            sra_id = sra_id.strip()
+            preempted_ids.add(sra_id)
+            pending_downloads.remove(sra_id)
+            pending_creates.remove(sra_id)
+            pending_cleans.remove(sra_id)
+            pending_transfers.remove(sra_id)
 
     def send_reply(self, code, message, headers={}):
         self.send_response(code)
@@ -209,9 +199,8 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         parsed_url = urllib.parse.urlparse(self.path)
-        params = urllib.parse.parse_qs(parsed_url.query)
         if parsed_url.path == '/jobs':
-            self.handle_get_job(params)
+            self.handle_get_job()
         elif parsed_url.path == '/status':
             self.handle_get_status()
         else:
@@ -238,18 +227,20 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.handle_nack_create(post_vars)
         elif parsed_url.path == '/jobs/nack/clean':
             self.handle_nack_clean(post_vars)
+        elif parsed_url.path == '/jobs/preempt':
+            self.handle_preempt(post_vars)
         else:
             self.send_reply(404, f'Invalid path: {self.path}\n')
 
 
-def load_file_dict(filename):
-    result = {}
+def load_file_set(filename):
+    result = set()
     try:
         with open(filename) as fp:
             for line in fp:
                 tokens = line.split()
-                assert len(tokens) == 2, f'Invalid line in downloads sras {line}'
-                result[tokens[0]] = tokens[1]
+                assert len(tokens) == 1, f'Invalid line in downloads sras {line}'
+                result.add(tokens[0])
     except FileNotFoundError:  # no downloaded sras yet, that's fine
         return result
     return result
@@ -279,23 +270,14 @@ def init_state():
         except OSError as exc:  # Guard against race condition
             if exc.errno != errno.EEXIST:
                 raise
-    global downloaded_sras, created_sras, cleaned_sras, transferred_sras, to_create_sras, to_clean_sras, to_transfer_sras
-    downloaded_sras = load_file_dict(filename)
-    created_sras = load_file_dict(os.path.join(args.output_dir, 'succeed_create.id'))
-    cleaned_sras = load_file_dict(os.path.join(args.output_dir, 'succeed_clean.id'))
-    transferred_sras = load_file_dict(os.path.join(args.output_dir, 'succeed_transfer.id'))
-
-    to_create_sras = {k: downloaded_sras[k] for k in set(downloaded_sras) - set(created_sras)}
-    to_clean_sras = {k: created_sras[k] for k in set(created_sras) - set(cleaned_sras)}
-    to_transfer_sras = {k: cleaned_sras[k] for k in set(cleaned_sras) - set(transferred_sras)}
-
-    # TODO: this incorrect because we need the create paths, not the clean path; need to implement transfer jobs
-    to_clean_sras.update(to_transfer_sras)  # because we don't support transfer only jobs
-    to_transfer_sras = {}
+    global downloaded_sras, created_sras, cleaned_sras, transferred_sras
+    downloaded_sras = load_file_set(filename)
+    created_sras = load_file_set(os.path.join(args.output_dir, 'succeed_create.id'))
+    cleaned_sras = load_file_set(os.path.join(args.output_dir, 'succeed_clean.id'))
+    transferred_sras = load_file_set(os.path.join(args.output_dir, 'succeed_transfer.id'))
 
     global sra_download_gen
     sra_download_gen = buckets.Sra(data_files, args.add_gcloud_bucket, args.data_dir, downloaded_sras).next_item()
-
 
 
 def init_logging():
