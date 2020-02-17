86f0a567cfeeb1aa53f9c9f55ffea5d1a7eef3c9
Mikhail Karasikov <mike-karas@ya.ru>
2020-02-17 20:24:19 +0100

    generalized merge heap, fixed bug with count overflow

--- metagraph/src/common/file_merger.hpp
+++ metagraph/src/common/file_merger.hpp
@@ -15,22 +15,27 @@ namespace common {
 
 /**
  * Heap implemented as a sorted vector.
+ *
+ * The heap uses a vector as the underlying data structure, thus making it efficient
+ * only for a small (<100) number of elements.
+ * @tparam T the actual heap element type; this is the type tested for equality
  */
 // Note: profiling shows that using a sorted vector instead of a std::priority queue is
 // faster up to ~1000 elements.  Using an unsorted vector (faster insert,
 // slower pop()) is ~40% slower. Preventing duplicates in the heap so that we don't
 // need to test for dupes at pop time is ~60% slower.
-template <typename T>
-class VectorHeap {
-    /** The heap stores pairs of the form <Element, SourceIndex>  in descending order */
+// Note: Profiling indicates that merging elements within the heap is ~30% slower than
+// inserting duplicates and merging them when popping from the heap, as we do now
+template <typename T, class Compare = std::greater<T>>
+class MergeHeap {
+    /** The heap stores triplets of the form <Element, Count, SourceIndex> */
     using value_type = std::pair<T, uint32_t>;
 
   public:
-    VectorHeap(size_t size) { els.reserve(size); }
     void emplace(T el, uint32_t idx) {
-        auto it = std::lower_bound(els.begin(), els.end(), value_type(el, idx),
-                                   [](const value_type &a, const value_type &b) {
-                                       return a.first > b.first;
+        auto it = std::lower_bound(els.begin(), els.end(), el,
+                                   [this](const value_type &p, const T &v) {
+                                       return compare_(p.first, v);
                                    });
         els.emplace(it, el, idx);
     }
@@ -44,8 +49,9 @@ class VectorHeap {
     bool empty() { return els.empty(); }
 
   private:
-    // elements stored in decreasing order of T
+    // elements stored in decreasing order of the first tuple member
     std::vector<value_type> els;
+    Compare compare_ = Compare();
 };
 
 
@@ -68,7 +74,7 @@ uint64_t merge_files(const std::vector<std::string> sources,
     std::vector<std::ifstream> chunk_files(sources.size());
     uint64_t num_elements_read = 0;
 
-    VectorHeap<T> merge_heap(sources.size());
+    MergeHeap<T> merge_heap;
     T data_item;
     // profiling indicates that setting a larger buffer slightly increases performance
     std::unique_ptr<char[]> buffer(new char[sources.size() * 1024 * 1024]);
@@ -110,41 +116,6 @@ uint64_t merge_files(const std::vector<std::string> sources,
     return num_elements_read;
 }
 
-/**
- * A heap that merges elements that are equal by adding their counts.
- * The heap uses a vector as the underlying data structure, thus making it efficient
- * only for a small (<100) number of elements.
- * @tparam T the actual heap element type; this is the type tested for equality
- * @tparam C the count type for elements in the heap (typically some unsigned integer)
- */
-// Note: Profiling indicates that merging elements within the heap is ~30% slower than
-// inserting duplicates and merging them when popping from the heap, as we do now
-template <typename T, typename C>
-class MergingHeap {
-    /** The heap stores triplets of the form <Element, Count, SourceIndex> */
-    using value_type = std::tuple<T, C, uint32_t>;
-
-  public:
-    void emplace(T el, C count, uint32_t idx) {
-        auto it = std::lower_bound(els.begin(), els.end(), value_type(el, count, idx),
-                                   [](const value_type &a, const value_type &b) {
-                                       return std::get<0>(a) > std::get<0>(b);
-                                   });
-        els.emplace(it, el, count, idx);
-    }
-
-    value_type pop() {
-        value_type result = els.back();
-        els.pop_back();
-        return result;
-    }
-
-    bool empty() { return els.empty(); }
-
-  private:
-    // elements stored in decreasing order of the first tuple member
-    std::vector<value_type> els;
-};
 
 /**
  * Given a list of n source files, containing ordered pairs of  <element, count>,
@@ -162,14 +133,12 @@ template <typename T, typename C>
 uint64_t merge_files(const std::vector<std::string> sources,
                      std::function<void(const std::pair<T, C> &)> on_new_item,
                      bool cleanup = true) {
-    // Convenience type for storing a pair and it's source file index
-    using CountedEl = std::tuple<T, C, uint32_t>;
     // start merging disk chunks by using a heap to store the current element
     // from each chunk
     std::vector<std::ifstream> chunk_files(sources.size());
     uint64_t num_elements = 0;
 
-    MergingHeap<T, C> merge_heap;
+    MergeHeap<std::pair<T, C>, utils::GreaterFirst> merge_heap;
     std::pair<T, C> data_item;
     std::unique_ptr<char[]> buffer(new char[sources.size() * 1024 * 1024]);
     for (uint32_t i = 0; i < sources.size(); ++i) {
@@ -181,7 +150,7 @@ uint64_t merge_files(const std::vector<std::string> sources,
         }
         if (chunk_files[i].read(reinterpret_cast<char *>(&data_item),
                                 sizeof(std::pair<T, C>))) {
-            merge_heap.emplace(data_item.first, data_item.second, i);
+            merge_heap.emplace(data_item, i);
             num_elements++;
         }
     }
@@ -194,34 +163,40 @@ uint64_t merge_files(const std::vector<std::string> sources,
         return num_elements;
     }
 
-    CountedEl current = merge_heap.pop();
-    uint32_t chunk_index = std::get<2>(current);
+    std::pair<T, C> current;
+    uint32_t chunk_index;
+    std::tie(current, chunk_index) = merge_heap.pop();
+
     if (chunk_files[chunk_index]
         && chunk_files[chunk_index].read(reinterpret_cast<char *>(&data_item),
                                          sizeof(std::pair<T, C>))) {
-        merge_heap.emplace(data_item.first, data_item.second, chunk_index);
+        merge_heap.emplace(data_item, chunk_index);
         num_elements++;
     }
 
     while (!merge_heap.empty()) {
-        CountedEl smallest = merge_heap.pop();
+        std::pair<T, C> smallest;
+        std::tie(smallest, chunk_index) = merge_heap.pop();
 
-        if (std::get<0>(smallest) != std::get<0>(current)) {
-            on_new_item({ std::get<0>(current), std::get<1>(current) });
+        if (smallest.first != current.first) {
+            on_new_item(current);
             current = smallest;
         } else {
-            std::get<1>(current) += std::get<1>(smallest);
+            if (current.second < std::numeric_limits<C>::max() - smallest.second) {
+                current.second += smallest.second;
+            } else {
+                current.second = std::numeric_limits<C>::max();
+            }
         }
 
-        chunk_index = std::get<2>(smallest);
         if (chunk_files[chunk_index]
             && chunk_files[chunk_index].read(reinterpret_cast<char *>(&data_item),
                                              sizeof(std::pair<T, C>))) {
-            merge_heap.emplace(data_item.first, data_item.second, chunk_index);
+            merge_heap.emplace(data_item, chunk_index);
             num_elements++;
         }
     }
-    on_new_item({ std::get<0>(current), std::get<1>(current) });
+    on_new_item(current);
 
     if (cleanup) {
         std::for_each(sources.begin(), sources.end(),
