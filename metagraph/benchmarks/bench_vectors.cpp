d9c7fdfc617165e1db7be1a94e27af38f8174211
Mikhail Karasikov <mike-karas@ya.ru>
2020-03-08 17:02:20 +0100

    templates for benchmarking bit vectors

--- metagraph/benchmarks/bench_vectors.cpp
+++ metagraph/benchmarks/bench_vectors.cpp
@@ -7,175 +7,97 @@
 namespace mg {
 namespace bm {
 
-template <class bit_vector_type, uint64_t size, uint8_t density_percent>
-static void BM_bit_vector_query_rank(benchmark::State& state) {
-    DataGenerator generator;
-    generator.set_seed(42);
-
-    bit_vector_type bv(
-        generator.generate_random_column(size, density_percent / 100.)
-    );
-
-    uint64_t i = 0;
-    for (auto _ : state) {
-        bv.rank1((i++ * 87'178'291'199) % size);
-    }
+#define DEFINE_BV_QUERY_BENCHMARK(NAME, OPERATION, MIN_INDEX, RANGE) \
+template <class bit_vector_type, uint64_t size, uint8_t density_percent> \
+static void BM_bv_query_##NAME(benchmark::State& state) { \
+    DataGenerator gen; \
+    gen.set_seed(42); \
+ \
+    bit_vector_type bv(gen.generate_random_column(size, density_percent / 100.)); \
+    uint64_t max_index = bv.RANGE(); \
+    if (max_index <= MIN_INDEX) \
+        return; \
+ \
+    uint64_t i = 0; \
+    for (auto _ : state) { \
+        bv.OPERATION(MIN_INDEX + (i++ * 87'178'291'199) % (max_index - MIN_INDEX)); \
+    } \
+    state.counters["Density"] \
+        = static_cast<double>(bv.num_set_bits()) / bv.size(); \
+} \
+ \
+template <class bit_vector_type, uint64_t size, uint8_t density_percent> \
+static void BM_bv_query_sequential_##NAME(benchmark::State& state) { \
+    DataGenerator gen; \
+    gen.set_seed(42); \
+ \
+    bit_vector_type bv(gen.generate_random_column(size, density_percent / 100.)); \
+    uint64_t max_index = bv.RANGE(); \
+    if (max_index <= MIN_INDEX) \
+        return; \
+ \
+    uint64_t i = 0; \
+    for (auto _ : state) { \
+        bv.OPERATION(MIN_INDEX + (i++) % (max_index - MIN_INDEX)); \
+    } \
+    state.counters["Density"] \
+        = static_cast<double>(bv.num_set_bits()) / bv.size(); \
 }
 
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 1) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 2) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 5) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 9) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 10) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 15) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 20) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 30) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 40) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 50) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 60) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 70) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 80) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 90) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 95) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 98) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_rank, bit_vector_rrr<63>, 10000000, 99) -> Unit(benchmark::kMicrosecond);
+DEFINE_BV_QUERY_BENCHMARK(rank,           rank1,          0, size);
+DEFINE_BV_QUERY_BENCHMARK(access,         operator[],     0, size);
+DEFINE_BV_QUERY_BENCHMARK(inverse_select, inverse_select, 0, size);
+DEFINE_BV_QUERY_BENCHMARK(select,         select1,        1, num_set_bits);
+DEFINE_BV_QUERY_BENCHMARK(next,           next1,          0, size);
+DEFINE_BV_QUERY_BENCHMARK(prev,           prev1,          0, size);
 
+#define INST_BV_QUERY_BENCHMARK(BV_TYPE, NAME) \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 0) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 1) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 2) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 5) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 9) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 10) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 15) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 20) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 30) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 40) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 50) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 60) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 70) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 80) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 90) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 95) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 98) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 99) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_##NAME, BV_TYPE, 10000000, 100) -> Unit(benchmark::kMicrosecond); \
+\
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 0) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 1) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 2) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 5) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 9) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 10) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 15) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 20) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 30) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 40) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 50) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 60) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 70) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 80) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 90) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 95) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 98) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 99) -> Unit(benchmark::kMicrosecond); \
+BENCHMARK_TEMPLATE(BM_bv_query_sequential_##NAME, BV_TYPE, 10000000, 100) -> Unit(benchmark::kMicrosecond);
 
-template <class bit_vector_type, uint64_t size, uint8_t density_percent>
-static void BM_bit_vector_sequential_query_rank(benchmark::State& state) {
-    DataGenerator generator;
-    generator.set_seed(42);
-
-    bit_vector_type bv(
-        generator.generate_random_column(size, density_percent / 100.)
-    );
-
-    uint64_t i = 0;
-    for (auto _ : state) {
-        bv.rank1((i++) % size);
-    }
-}
-
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 1) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 2) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 5) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 9) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 10) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 15) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 20) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 30) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 40) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 50) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 60) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 70) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 80) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 90) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 95) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 98) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_sequential_query_rank, bit_vector_rrr<63>, 10000000, 99) -> Unit(benchmark::kMicrosecond);
-
-
-template <class bit_vector_type, uint64_t size, uint8_t density_percent>
-static void BM_bit_vector_query_access(benchmark::State& state) {
-    DataGenerator generator;
-    generator.set_seed(42);
-
-    bit_vector_type bv(
-        generator.generate_random_column(size, density_percent / 100.)
-    );
-
-    uint64_t i = 0;
-    for (auto _ : state) {
-        bv[(i++ * 87'178'291'199) % size];
-    }
-}
-
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 1) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 2) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 5) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 9) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 10) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 15) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 20) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 30) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 40) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 50) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 60) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 70) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 80) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 90) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 95) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 98) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_access, bit_vector_rrr<63>, 10000000, 99) -> Unit(benchmark::kMicrosecond);
-
-
-template <class bit_vector_type, uint64_t size, uint8_t density_percent>
-static void BM_bit_vector_query_inverse_select(benchmark::State& state) {
-    DataGenerator generator;
-    generator.set_seed(42);
-
-    bit_vector_type bv(
-        generator.generate_random_column(size, density_percent / 100.)
-    );
-
-    uint64_t i = 0;
-    for (auto _ : state) {
-        bv.inverse_select((i++ * 87'178'291'199) % size);
-    }
-}
-
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 1) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 2) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 5) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 9) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 10) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 15) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 20) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 30) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 40) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 50) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 60) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 70) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 80) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 90) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 95) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 98) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_inverse_select, bit_vector_rrr<63>, 10000000, 99) -> Unit(benchmark::kMicrosecond);
-
-
-template <class bit_vector_type, uint64_t size, uint8_t density_percent>
-static void BM_bit_vector_query_select(benchmark::State& state) {
-    DataGenerator generator;
-    generator.set_seed(42);
-
-    bit_vector_type bv(
-        generator.generate_random_column(size, density_percent / 100.)
-    );
-
-    uint64_t i = 0;
-    uint64_t num_set_bits = bv.num_set_bits();
-    for (auto _ : state) {
-        bv.select1((i++ * 87'178'291'199) % num_set_bits);
-    }
-}
-
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 1) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 2) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 5) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 9) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 10) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 15) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 20) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 30) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 40) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 50) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 60) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 70) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 80) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 90) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 95) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 98) -> Unit(benchmark::kMicrosecond);
-BENCHMARK_TEMPLATE(BM_bit_vector_query_select, bit_vector_rrr<63>, 10000000, 99) -> Unit(benchmark::kMicrosecond);
+INST_BV_QUERY_BENCHMARK(bit_vector_rrr<63>, rank);
+INST_BV_QUERY_BENCHMARK(bit_vector_rrr<63>, access);
+INST_BV_QUERY_BENCHMARK(bit_vector_rrr<63>, inverse_select);
+INST_BV_QUERY_BENCHMARK(bit_vector_rrr<63>, select);
+INST_BV_QUERY_BENCHMARK(bit_vector_rrr<63>, next);
+INST_BV_QUERY_BENCHMARK(bit_vector_rrr<63>, prev);
 
 } // namespace bm
 } // namespace mg
