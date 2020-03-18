9c4d1e93ccafa0b02feb0c7b70f3e13a2bebb190
Daniel Danciu <dd@daedalean.ai>
2020-03-18 23:54:25 +0100

    Use a lookup table

--- metagraph/src/common/elias_fano.cpp
+++ metagraph/src/common/elias_fano.cpp
@@ -9,6 +9,18 @@
 
 namespace mg{
 namespace common {
+// clang-format off
+/** Lookup table for the values of 2^n-1 for n in 0..56 */
+static uint64_t two_n_1[56] = {
+            0, 1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191, 16383, 32767,
+            65535, 131071, 262143, 524287, 1048575, 2097151, 4194303, 8388607, 16777215,
+            33554431, 67108863, 134217727, 268435455, 536870911, 1073741823, 2147483647,
+            4294967295, 8589934591, 17179869183, 34359738367, 68719476735, 137438953471,
+            274877906943, 549755813887, 1099511627775, 2199023255551, 4398046511103,
+            8796093022207, 17592186044415, 35184372088831, 70368744177663, 140737488355327,
+            281474976710655, 562949953421311, 1125899906842623, 2251799813685247,
+            4503599627370495, 9007199254740991, 18014398509481983, 36028797018963967 };
+// clang-format on
 template <class T, class Enable = void>
 struct Unaligned;
 
@@ -106,12 +118,12 @@ EliasFanoEncoder<T>::EliasFanoEncoder(size_t size,
     sink_.write(reinterpret_cast<char *>(&num_lower_bytes_), sizeof(size_t));
     sink_.write(reinterpret_cast<char *>(&num_upper_bytes_), sizeof(size_t));
 }
+
 template <typename T>
 void EliasFanoEncoder<T>::add(T value) {
 #ifndef NDEBUG
     assert(value >= last_value_);
 #endif
-
     const T upper_bits = value >> num_lower_bits_;
 
     // We are adding the size_-th element, so we have a 1 followed by upper_bits
@@ -122,7 +134,7 @@ void EliasFanoEncoder<T>::add(T value) {
 
     // Append the #num_lower_bits_ bits of #value to #lower_
     if (num_lower_bits_ != 0) {
-        const T lowerBits = value & ((T(1) << num_lower_bits_) - 1);
+        const T lowerBits = value & two_n_1[num_lower_bits_];
         size_t pos_bits = size_ * num_lower_bits_;
         if (pos_bits - cur_pos_lbits_ >= 64) { // first 64 bits are ready to be written
             cur_pos_lbits_ += 64;
