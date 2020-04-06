11143f63a854c565f0baafd183113df03f1a6d91
Harun Mustafa <bilgisayaradami@gmail.com>
2020-04-06 23:29:54 +0200

    Added FastaWriter benchmarks

---
+++ metagraph/benchmarks/common/benchmark_sequence_io.cpp
@@ -0,0 +1,47 @@
+#include <cmath>
+
+#include <benchmark/benchmark.h>
+
+#include "graph/annotated_dbg.hpp"
+#include "seq_io/sequence_io.hpp"
+
+#include "../benchmark_graph_helpers.hpp"
+
+const std::string file_prefix = "/tmp/bm_mg_outfile.fasta.gz";
+
+
+static void BM_UnitigsWrite(benchmark::State& state) {
+    std::string graph_file = "../tests/data/transcripts_1000.fa";
+    auto anno_graph = mg::bm::build_anno_graph<>(graph_file);
+    std::vector<std::string> unitigs;
+    anno_graph->get_graph().call_unitigs([&](const auto &unitig, auto&&) {
+        unitigs.push_back(unitig);
+    });
+
+    for (auto _ : state) {
+        FastaWriter writer(file_prefix, "", false, std::pow(10, state.range(0)) - 1);
+        for (const auto &unitig : unitigs) {
+            writer.write(unitig);
+        }
+    }
+}
+
+BENCHMARK(BM_UnitigsWrite)
+    ->Unit(benchmark::kMillisecond)
+    ->DenseRange(0, 10, 2);
+
+static void BM_UnitigsExtractAndWrite(benchmark::State& state) {
+    std::string graph_file = "../tests/data/transcripts_1000.fa";
+    auto anno_graph = mg::bm::build_anno_graph<>(graph_file);
+
+    for (auto _ : state) {
+        FastaWriter writer(file_prefix, "", false, std::pow(10, state.range(0)) - 1);
+        anno_graph->get_graph().call_unitigs([&](const auto &unitig, auto&&) {
+            writer.write(unitig);
+        });
+    }
+}
+
+BENCHMARK(BM_UnitigsExtractAndWrite)
+    ->Unit(benchmark::kMillisecond)
+    ->DenseRange(0, 10, 2);
