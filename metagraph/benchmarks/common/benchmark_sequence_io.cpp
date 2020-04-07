3340fef1c7de43d6765a2ac986908b456c00ec5a
Harun Mustafa <bilgisayaradami@gmail.com>
2020-04-07 19:45:53 +0200

    Added benchmark with random sequence generator

--- metagraph/benchmarks/common/benchmark_sequence_io.cpp
+++ metagraph/benchmarks/common/benchmark_sequence_io.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <random>
 
 #include <benchmark/benchmark.h>
 
@@ -11,7 +12,39 @@
 const std::string file_prefix = "/tmp/bm_mg_outfile.fasta.gz";
 
 
-static void BM_ContigsWrite(benchmark::State& state) {
+static void BM_WriteRandomSequences(benchmark::State& state) {
+    const std::string alphabet = "ATGCN";
+    std::mt19937 rng(123457);
+    std::uniform_int_distribution<std::mt19937::result_type> dist4(0, 4);
+    std::uniform_int_distribution<std::mt19937::result_type> dist1000(10, 1000);
+
+    const size_t num_seqs = 10'000;
+
+    std::vector<std::string> sequences;
+    sequences.reserve(num_seqs);
+    for (size_t i = 0; i < num_seqs; ++i) {
+        sequences.emplace_back(dist1000(rng), 'A');
+        std::for_each(sequences.back().begin(),
+                      sequences.back().end(),
+                      [&](char &c) { c = alphabet[dist4(rng)]; });
+    }
+
+    set_num_threads(state.range(0));
+    for (auto _ : state) {
+        FastaWriter writer(file_prefix, "", false);
+        for (const auto &sequence : sequences) {
+            writer.write(sequence);
+        }
+    }
+    set_num_threads(1);
+}
+
+BENCHMARK(BM_WriteRandomSequences)
+    ->Unit(benchmark::kMillisecond)
+    ->DenseRange(1, 2, 1);
+
+
+static void BM_WriteContigs(benchmark::State& state) {
     std::string graph_file = "../tests/data/transcripts_1000.fa";
     auto graph = mg::bm::build_graph(graph_file);
     std::vector<std::string> contigs;
@@ -27,6 +60,6 @@ static void BM_ContigsWrite(benchmark::State& state) {
     set_num_threads(1);
 }
 
-BENCHMARK(BM_ContigsWrite)
+BENCHMARK(BM_WriteContigs)
     ->Unit(benchmark::kMillisecond)
     ->DenseRange(1, 2, 1);
