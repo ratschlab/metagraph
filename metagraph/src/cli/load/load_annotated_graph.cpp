3a3f3a7d702700657df5efb8ddca1e22cd50a4f1
Mikhail Karasikov <mike-karas@ya.ru>
2020-05-19 00:59:25 +0200

    added matrix get_row benchmarks, cleanups

--- metagraph/src/cli/load/load_annotated_graph.cpp
+++ metagraph/src/cli/load/load_annotated_graph.cpp
@@ -15,7 +15,7 @@ using mtg::common::logger;
 std::unique_ptr<AnnotatedDBG> initialize_annotated_dbg(std::shared_ptr<DeBruijnGraph> graph,
                                                        const Config &config) {
     auto annotation_temp = config.infbase_annotators.size()
-            ? initialize_annotation(parse_annotation_type(config.infbase_annotators.at(0)), config, 0)
+            ? initialize_annotation(config.infbase_annotators.at(0), config, 0)
             : initialize_annotation(config.anno_type, config, graph->max_index());
 
     if (config.infbase_annotators.size()
