0c165f8930d9e7af4400d62f9c7626d975e68ee2
Daniel Danciu <dd@daedalean.ai>
2020-06-15 12:40:28 +0200

    Revert "Fix unsigned computation in merge"
    This reverts commit ca7fa33d1a33843fe59439ceb34a01f17dfd1df4.

--- metagraph/src/graph/representation/succinct/merge.hpp
+++ metagraph/src/graph/representation/succinct/merge.hpp
@@ -3,33 +3,25 @@
 #include <cstddef>
 #include <limits>
 #include <random>
-#include <iostream>
-
 namespace mtg {
 namespace common {
 template <typename T>
-size_t find_next_X_block(T *arr,
-                         size_t beg,
-                         size_t z,
-                         size_t y,
-                         size_t k,
-                         size_t f,
-                         size_t buf1,
-                         size_t buf2) {
+size_t
+find_next_X_block(T *A, size_t x0, size_t z, size_t y, size_t k, size_t f, size_t b1, size_t b2) {
     T min1 = std::numeric_limits<T>::max();
     T min2 = std::numeric_limits<T>::max();
-    size_t m = (static_cast<int64_t>(z - beg - f) / static_cast<int64_t>(k)) * k + f + beg;
+    size_t m = ((z - x0 - f) / k) * k + f + x0;
     if (m <= z)
         m += k;
     size_t i = m; // find from m; the start of the block adjacent to the right of z
     size_t result = 0;
     while (i + k <= y) {
-        if (i != buf1 && i != buf2) {
-            size_t j = (i < buf1 && buf1 < i + k) ? m - 1 : i + k - 1;
-            if (arr[i] <= min1 && arr[j] <= min2) {
+        if (i != b1 && i != b2) {
+            size_t j = (i < b1 && b1 < i + k) ? m - 1 : i + k - 1;
+            if (A[i] <= min1 && A[j] <= min2) {
                 result = i;
-                min1 = arr[i];
-                min2 = arr[j];
+                min1 = A[i];
+                min2 = A[j];
             }
         }
         i += k;
@@ -37,79 +29,61 @@ size_t find_next_X_block(T *arr,
     return result;
 }
 
-/**
- * Merges the  remaining Y-elements A[y..end) with the buffer elementsA[z..y−1]. Note that
- * A[y..end) is  already  sorted/
- */
 template <typename T>
-void merge_b_and_y(T *arr, size_t z, size_t y, size_t end) {
-    while (z < y && y < end) {
-        T *Aj = std::min_element(arr + z, arr + y);
-        if (*Aj <= arr[y]) {
-            std::swap(arr[z], *Aj);
+void mergeBandY(T *A, size_t z, size_t y, size_t yn) {
+    while (z < y && y < yn) {
+        T *Aj = std::min_element(A + z, A + y);
+        if (*Aj <= A[y]) {
+            std::swap(A[z], *Aj);
         } else {
-            std::swap(arr[z], arr[y]);
+            std::swap(A[z], A[y]);
             y++;
         }
         z++;
     }
     if (z < y)
-        std::sort(arr + z, arr + end);
+        std::sort(A + z, A + yn);
 }
 
-/**
- * In place merge-sort implementation, based on "A simple algorithm for in-place merging"
- * by Jing-Chao Chen,
- * @param T the elements to be merged in-place. The two sorted arrays are beg..mid-1 and
- * mid..end-1.
- * @param beg index of the first element of the first array
- * @param mid index the first element of the second array
- * @param end total number of elements
- * @param k the merge block size
- */
 template <typename T>
-void merge(T *arr, size_t beg, size_t mid, size_t end, size_t k) {
-    assert(beg <= mid && mid < end);
-    static constexpr size_t MAX = std::numeric_limits<size_t>::max();
+void merge(T *A, size_t beg, size_t mid, size_t end, size_t k) {
     size_t f = (mid - beg) % k;
-    size_t x = (f == 0) ? mid - 2 * k : mid - k - f; // current element in 1st half
-    if (mid - beg < 2 * k)
-        x = beg;
-    T tmp = arr[x];
-    arr[x] = arr[beg];
-    size_t z = beg; // z is the "whole" position; everything to its left is sorted
-    size_t y = mid; // current element in 2nd half
-    size_t buf1 = x + 1; // buf1 and buf2 are the 2 buffers for temporary data
+    size_t x = (f == 0) ? mid - 2 * k : mid - k - f;
+    T tmp = A[x];
+    A[x] = A[beg];
+    size_t z = beg;
+    size_t y = mid;
+    size_t buf1 = x + 1;
     size_t buf2 = mid - k;
     while (y - z > 2 * k) { // test if X-elements (excluding the buffer elements) are exhausted
-        if (y >= end || arr[x] <= arr[y]) {
-            arr[z] = arr[x];
-            arr[x] = arr[buf1];
+        if (A[x] <= A[y] || y >= end) {
+            A[z] = A[x];
+            A[x] = A[buf1];
             x++;
             if ((x - beg) % k == f) { // x reached the start position of the new block
                 if (z < x - k)
                     buf2 = x - k;
-                x = find_next_X_block(arr, beg, z, y, k, f, buf1, buf2);
+                x = find_next_X_block(A, beg, z, y, k, f, buf1, buf2);
             }
         } else {
-            arr[z] = arr[y];
-            arr[y] = arr[buf1];
+            A[z] = A[y];
+            A[y] = A[buf1];
             y++;
             if ((y - mid) % k == 0)
                 buf2 = y - k;
         }
         z++;
-        arr[buf1] = arr[z];
+        A[buf1] = A[z];
         if (z == x)
             x = buf1;
         if (z == buf2)
-            buf2 = MAX;
-        buf1++;
+            buf2 = -1;
+        buf1 = buf1 + 1;
         if ((buf1 - beg) % k == f)
-            buf1 = buf2 == MAX ? buf1 - k : buf2;
+            buf1 = buf2 == -1 ? buf1 - k : buf2;
     }
-    arr[z] = tmp;
-    merge_b_and_y(arr, z, y, end);
+    A[z] = tmp;
+    mergeBandY(A, z, y, end);
 }
 } // namespace common
 } // namespace mtg
@@ -117,41 +91,19 @@ void merge(T *arr, size_t beg, size_t mid, size_t end, size_t k) {
 int main() {
     std::mt19937 rng(123456);
     std::uniform_int_distribution<int> uniform_dist(1, 10);
-    for (uint32_t size = 10; size < 1000; ++size) {
-        for (uint32_t k = sqrt(size); k < 2 * sqrt(size); ++k) {
-            for (int mid_diff = -10; mid_diff < 10; ++mid_diff) {
-                std::vector<int> a(size);
-                size_t mid = std::max(std::min(1, static_cast<int>(a.size()) / 2 + mid_diff),
-                                      static_cast<int>(size - 2));
-                a[0] = uniform_dist(rng);
-                for (uint32_t i = 1; i < mid; ++i) {
-                    a[i] = a[i - 1] + uniform_dist(rng);
-                }
-                a[mid] = uniform_dist(rng);
-                for (uint32_t i = mid + 1; i < a.size(); ++i) {
-                    a[i] = a[i - 1] + uniform_dist(rng);
-                }
-                mtg::common::merge(a.data(), 0, mid, a.size(), k);
-                bool broken = false;
-                for (uint32_t i = 1; i < a.size(); ++i) {
-                    if (a[i] < a[i - 1]) {
-                        broken = true;
-                        break;
-                    }
-                }
-                if (broken) {
-                    std::cout << a[0] << " ";
-                    for (uint32_t i = 1; i < a.size(); ++i) {
-                        std::cout << a[i] << " ";
-                        if (a[i] < a[i - 1]) {
-                            std::cout << "\nBroken at " << i << " " << size << " " << k
-                                      << std::endl;
-                        }
-                    }
-                    std::exit(0);
-                }
-            }
-        }
+    std::vector<int> a(100);
+    size_t mid = a.size()/2;
+    a[0] = uniform_dist(rng);
+    for (uint32_t i = 1; i < mid; ++i) {
+        a[i] = a[i - 1] + uniform_dist(rng);
+    }
+    a[mid] = uniform_dist(rng);
+    for (uint32_t i = mid+1; i < a.size(); ++i) {
+        a[i] = a[i - 1] + uniform_dist(rng);
+    }
+    merge(a.data(), 0, mid, a.size(), 5);
+    for (const auto v : a) {
+        std::cout << v << " ";
     }
 }
 */
