97bfc667c1d2e15df78a2ad2e2f3e3c9d3b577f9
Daniel Danciu <dd@daedalean.ai>
2020-07-06 11:56:03 +0200

    Shrink vector to fit, as resize does nothing, duh.

--- metagraph/src/graph/representation/succinct/boss_chunk_construct.cpp
+++ metagraph/src/graph/representation/succinct/boss_chunk_construct.cpp
@@ -237,7 +237,7 @@ void recover_dummy_nodes(const KmerCollector &kmer_collector,
 
     logger->trace("Total number of real k-mers: {}", kmers.size());
 
-    kmers.resize(kmers.size());
+    kmers.shrink_to_fit();
     Vector<KMER> dummy_kmers;
     dummy_kmers.reserve(0.1 * kmers.size()); // assume at most 10% dummy sink k-mers
     add_dummy_sink_kmers(k, kmers, &dummy_kmers);
