e9428609a2233d4bd8dbdabd982cd47af1d03553
Harun Mustafa <bilgisayaradami@gmail.com>
2020-08-04 17:59:49 +0200

    Report extension stats

--- metagraph/src/graph/alignment/aligner_extender_methods.cpp
+++ metagraph/src/graph/alignment/aligner_extender_methods.cpp
@@ -553,6 +553,9 @@ void DefaultColumnExtender<NodeType>
         update_columns(node, out_columns, min_path_score);
     }
 
+    logger->trace("Extension completed:\tquery size:\t{}\tseed size:\t{}\texplored nodes:\t{}",
+                  query.size(), path_->size(), dp_table.size());
+
     assert(start_score > config_.min_cell_score);
 
     // no good path found
