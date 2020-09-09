fa07b12a12efe28542aaf6cce8948e314c958675
Daniel Danciu <dd@daedalean.ai>
2020-09-09 08:45:43 +0200

    Add integration tests for parallel building

--- metagraph/integration_tests/test_build.py
+++ metagraph/integration_tests/test_build.py
@@ -371,5 +371,42 @@ class TestBuild(unittest.TestCase):
         self.assertEqual('nodes (k): 1159851', params_str[1])
         self.assertEqual('canonical mode: yes', params_str[2])
 
+    # tests that we can build and resume 2 separate graphs on the same machine
+    @parameterized.expand(['succinct_disk'])
+    def test_build_phase_parallel(self, build):
+        representation, tmp_dir = build_params[build]
+
+        for name in ('graph1', 'graph2'):
+            construct_command = '{exe} build --phase 1 --mask-dummy --graph {repr} --canonical -k 20 ' \
+                                '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=representation,
+                tmp_dir=tmp_dir,
+                outfile=self.tempdir.name + '/' + name,
+                input=TEST_DATA_DIR + ('/transcripts_1000.fa' if name == 'graph1' else '/transcripts_100.fa')
+            )
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+            self.assertTrue(os.path.isfile(self.tempdir.name + '/' + name + '.checkpoint'))
+
+        for name in ('graph', 'graph2'):
+            construct_command = '{exe} build --mask-dummy --graph {repr} --canonical -k 20 ' \
+                                '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
+                exe=METAGRAPH,
+                repr=representation,
+                tmp_dir=tmp_dir,
+                outfile=self.tempdir.name + '/' + name,
+                input=TEST_DATA_DIR + ('/transcripts_1000.fa' if name == 'graph1' else '/transcripts_100.fa')
+            )
+            res = subprocess.run([construct_command], shell=True)
+            self.assertEqual(res.returncode, 0)
+
+            res = self.__get_stats(self.tempdir.name + '/graph' + graph_file_extension[representation])
+            self.assertEqual(res.returncode, 0)
+            params_str = res.stdout.decode().split('\n')[2:]
+            self.assertEqual('k: 20', params_str[0])
+            self.assertEqual('nodes (k): ' + ('1159851' if name == 'graph1' else '91584'), params_str[1])
+            self.assertEqual('canonical mode: yes', params_str[2])
+
 if __name__ == '__main__':
     unittest.main()
