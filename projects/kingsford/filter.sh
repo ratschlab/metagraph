498f1b15e0df325ebe206291c52b3a24fac7c65b
Mikhail Karasikov <mike-karas@ya.ru>
2020-09-17 15:19:42 +0200

    construct kingsford with new pipeline, cleanup

--- projects/kingsford/filter.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-DATA_DIR="/cluster/work/grlab/projects/metagenome/benchmark_kingsford/data_fasta"
-
-ls -1S "$DATA_DIR"/*.fasta.gz > kingsford_list.txt
-num_files=$(cat kingsford_list.txt | wc -l)
-
-bsub -J "filter[1-$num_files]%500" -W 72:00 -n 10 -R "rusage[mem=3400]" "./filtering.sh \"\$(sed -n \${LSB_JOBINDEX}p kingsford_list.txt)\""
