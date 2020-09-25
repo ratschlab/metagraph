41fc10c34557cc4bef2c67f82b69af11e55a9d02
Daniel Danciu <dd@daedalean.ai>
2020-09-25 10:24:53 +0200

    Dealt with clang error on templates

--- metagraph/src/annotation/binary_matrix/row_diff/column_diff.cpp
+++ metagraph/src/annotation/binary_matrix/row_diff/column_diff.cpp
@@ -7,25 +7,6 @@ namespace annot {
 namespace binmat {
 
 template <class BaseMatrix>
-ColumnDiff<BaseMatrix>::ColumnDiff(const graph::DBGSuccinct *graph,
-                                   BaseMatrix &&diffs,
-                                   const std::string& terminal_file)
-    : graph_(graph), diffs_(std::move(diffs)), terminal_file_(terminal_file) {
-    load_terminal();
-}
-
-template <class BaseMatrix>
-void ColumnDiff<BaseMatrix>::load_terminal() {
-    std::ifstream f(terminal_file_, ios::binary);
-    if (!f.good()) {
-        common::logger->error("Could not open terminal file {}", terminal_file_);
-        std::exit(1);
-    }
-    terminal_.load(f);
-    f.close();
-}
-
-template <class BaseMatrix>
 void ColumnDiff<BaseMatrix>::serialize(const std::string &name) const {
     std::ofstream f(name, ios::binary);
     serialize(f);
@@ -38,80 +19,9 @@ bool ColumnDiff<BaseMatrix>::load(const std::string &name) {
     bool result = load(f);
     f.close();
 
-    load_terminal();
+    load_terminal(terminal_file_, &terminal_);
 
     return result;
-}
-
-template <class BaseMatrix>
-bool ColumnDiff<BaseMatrix>::get(Row row, Column column) const {
-    SetBitPositions set_bits = get_row(row);
-    SetBitPositions::iterator v = std::lower_bound(set_bits.begin(), set_bits.end(), column);
-    return v != set_bits.end() && *v == column;
-}
-
-/**
- * Returns the given column.
- */
-template <class BaseMatrix>
-std::vector<BinaryMatrix::Row> ColumnDiff<BaseMatrix>::get_column(Column column) const {
-    std::vector<Row> result;
-    for (Row row = 0; row < num_rows(); ++row) {
-        if (get(row, column))
-            result.push_back(row);
-    }
-    return result;
-}
-
-template <class BaseMatrix>
-BinaryMatrix::SetBitPositions ColumnDiff<BaseMatrix>::get_row(Row row) const {
-    Vector<uint64_t> result = get_diff(row);
-
-    uint64_t boss_edge = graph_->kmer_to_boss_index(
-            graph::AnnotatedSequenceGraph::anno_to_graph_index(row));
-    const graph::boss::BOSS &boss = graph_->get_boss();
-
-    while (!terminal_[row]) {
-        graph::boss::BOSS::TAlphabet w = boss.get_W(boss_edge);
-        assert(boss_edge > 1 && w != 0);
-
-        // fwd always selects the last outgoing edge for a given node
-        boss_edge = boss.fwd(boss_edge, w % boss.alph_size);
-        row = graph::AnnotatedSequenceGraph::graph_to_anno_index(
-                graph_->boss_to_kmer_index(boss_edge));
-        merge(&result, get_diff(row));
-    };
-    return result;
-}
-
-void merge(Vector<uint64_t> *result, const Vector<uint64_t> &diff2) {
-    if (diff2.empty()) {
-        return;
-    }
-    Vector<uint64_t> diff1;
-    std::swap(*result, diff1);
-    uint64_t idx1 = 0;
-    uint64_t idx2 = 0;
-    while (idx1 < diff1.size() && idx2 < diff2.size()) {
-        if (diff1[idx1] == diff2[idx2]) {
-            idx1++;
-            idx2++;
-        } else if (diff1[idx1] < diff2[idx2]) {
-            result->push_back(diff1[idx1]);
-            idx1++;
-        } else {
-            result->push_back(diff2[idx2]);
-            idx2++;
-        }
-    }
-    while (idx1 < diff1.size()) {
-        result->push_back(diff1[idx1]);
-        idx1++;
-    }
-    while (idx2 < diff2.size()) {
-        result->push_back(diff2[idx2]);
-        idx2++;
-    }
 }
 
 template
--- metagraph/src/annotation/binary_matrix/row_diff/column_diff.hpp
+++ metagraph/src/annotation/binary_matrix/row_diff/column_diff.hpp
@@ -16,8 +16,6 @@ namespace mtg {
 namespace annot {
 namespace binmat {
 
-void merge(Vector<uint64_t> *result, const Vector<uint64_t> &diff2);
-
 /**
  * Row-major representation of annotations where rows are stored as differences vs a
  * predecessor row. The predecessor of a row is determined by a path in an external graph
@@ -32,6 +30,10 @@ void merge(Vector<uint64_t> *result, const Vector<uint64_t> &diff2);
  * row, we start traversing the node corresponding to i in #graph_ and accumulate the
  * diffs until we hit a terminal node, which is stored in full.
  */
+//NOTE: Clang aggressively abuses the clause in the C++ standard (14.7.1/11) that allows
+// virtual methods in template classes to not be instantiated if unused and mistakenly
+// does not instantiate the virtual methods in this class, so I had to move definitions
+// to the header
  template <class BaseMatrix>
 class ColumnDiff : public BinaryMatrix {
   public:
@@ -39,7 +41,10 @@ class ColumnDiff : public BinaryMatrix {
 
     ColumnDiff(const graph::DBGSuccinct *graph,
                BaseMatrix &&diffs,
-               const std::string& terminal_file);
+               const std::string &terminal_file)
+        : graph_(graph), diffs_(std::move(diffs)), terminal_file_(terminal_file) {
+        load_terminal(terminal_file_, &terminal_);
+    }
 
     uint64_t num_columns() const override { return diffs_.num_columns(); }
 
@@ -51,15 +56,43 @@ class ColumnDiff : public BinaryMatrix {
     uint64_t num_rows() const override { return diffs_.num_rows(); }
     void set_graph(const graph::DBGSuccinct *graph) { graph_ = graph; }
 
-    bool get(Row row, Column column) const override;
+    bool get(Row row, Column column) const override {
+        SetBitPositions set_bits = get_row(row);
+        SetBitPositions::iterator v = std::lower_bound(set_bits.begin(), set_bits.end(), column);
+        return v != set_bits.end() && *v == column;
+    }
 
     /**
      * Returns the given column.
      */
-    std::vector<Row> get_column(Column column) const override;
+    std::vector<Row> get_column(Column column) const override  {
+        std::vector<Row> result;
+        for (Row row = 0; row < num_rows(); ++row) {
+            if (get(row, column))
+                result.push_back(row);
+        }
+        return result;
+    }
 
+    SetBitPositions get_row(Row row) const override  {
+        Vector<uint64_t> result = get_diff(row);
 
-    SetBitPositions get_row(Row row) const override;
+        uint64_t boss_edge = graph_->kmer_to_boss_index(
+                graph::AnnotatedSequenceGraph::anno_to_graph_index(row));
+        const graph::boss::BOSS &boss = graph_->get_boss();
+
+        while (!terminal_[row]) {
+            graph::boss::BOSS::TAlphabet w = boss.get_W(boss_edge);
+            assert(boss_edge > 1 && w != 0);
+
+            // fwd always selects the last outgoing edge for a given node
+            boss_edge = boss.fwd(boss_edge, w % boss.alph_size);
+            row = graph::AnnotatedSequenceGraph::graph_to_anno_index(
+                    graph_->boss_to_kmer_index(boss_edge));
+            merge(&result, get_diff(row));
+        };
+        return result;
+    }
 
     bool load(std::istream &f) override { return diffs_.load(f); }
     void serialize(std::ostream &f) const override { diffs_.serialize(f); };
@@ -74,7 +107,45 @@ class ColumnDiff : public BinaryMatrix {
     Vector<uint64_t> get_diff(uint64_t node_id) const { return diffs_.get_row(node_id); }
 
   private:
-    void load_terminal();
+    static void load_terminal(const std::string &filename, sdsl::rrr_vector<> *terminal) {
+        std::ifstream f(filename, ios::binary);
+        if (!f.good()) {
+            common::logger->error("Could not open terminal file {}", filename);
+            std::exit(1);
+        }
+        terminal->load(f);
+        f.close();
+    }
+
+    static void merge(Vector<uint64_t> *result, const Vector<uint64_t> &diff2) {
+        if (diff2.empty()) {
+            return;
+        }
+        Vector<uint64_t> diff1;
+        std::swap(*result, diff1);
+        uint64_t idx1 = 0;
+        uint64_t idx2 = 0;
+        while (idx1 < diff1.size() && idx2 < diff2.size()) {
+            if (diff1[idx1] == diff2[idx2]) {
+                idx1++;
+                idx2++;
+            } else if (diff1[idx1] < diff2[idx2]) {
+                result->push_back(diff1[idx1]);
+                idx1++;
+            } else {
+                result->push_back(diff2[idx2]);
+                idx2++;
+            }
+        }
+        while (idx1 < diff1.size()) {
+            result->push_back(diff1[idx1]);
+            idx1++;
+        }
+        while (idx2 < diff2.size()) {
+            result->push_back(diff2[idx2]);
+            idx2++;
+        }
+    }
 
     const graph::DBGSuccinct *graph_;
 
