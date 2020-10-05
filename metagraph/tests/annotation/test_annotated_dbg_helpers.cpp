7c6c9f14b86e1b0bcfbcbff4fc8f4bdaf1f996df
Harun Mustafa <bilgisayaradami@gmail.com>
2020-10-05 23:07:48 +0200

    fix build_anno_graph

--- metagraph/tests/annotation/test_annotated_dbg_helpers.cpp
+++ metagraph/tests/annotation/test_annotated_dbg_helpers.cpp
@@ -4,6 +4,7 @@
 
 #include "annotation/annotation_converters.hpp"
 #include "graph/annotated_graph_algorithm.hpp"
+#include "graph/representation/canonical_dbg.hpp"
 
 
 namespace mtg {
@@ -18,8 +19,10 @@ std::unique_ptr<AnnotatedDBG> build_anno_graph(uint64_t k,
                                                BuildMode mode) {
     assert(sequences.size() == labels.size());
     auto graph = build_graph_batch<Graph>(k, sequences, mode);
+    auto canonical = std::dynamic_pointer_cast<CanonicalDBG>(graph);
+    assert(!canonical || mode == BuildMode::WRAPPER);
 
-    uint64_t max_index = graph->max_index();
+    uint64_t max_index = canonical ? canonical->get_graph().max_index() : graph->max_index();
 
     auto anno_graph = std::make_unique<AnnotatedDBG>(
         graph,
