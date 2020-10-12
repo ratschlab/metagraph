b6d9d642f7f18d88eb82e7fcb2c53f14d56c3208
Daniel Danciu <dd@daedalean.ai>
2020-10-12 22:00:47 +0200

    Parallelize succ/pred construction

--- metagraph/src/annotation/annotation_converters.cpp
+++ metagraph/src/annotation/annotation_converters.cpp
@@ -1102,18 +1102,8 @@ void build_successor(const graph::DBGSuccinct &graph,
     const BOSS &boss = graph.get_boss();
     sdsl::bit_vector terminal;
     sdsl::bit_vector dummy;
-    boss.call_sequences_row_diff(
-            [&](const vector<uint64_t> &, std::optional<uint64_t>) {
-                /*std::string chars(boss.get_node_str(path.front()));
-                for (uint32_t i = 0; i< path.size();++i) {
-                    chars += boss.decode(boss.get_W(path[i]) % boss.alph_size);
-                }
-                std::cout << chars << std::endl;*/
-            },
-            num_threads, max_length, &terminal, &dummy);
-
-    ProgressBar progress_bar(graph.num_nodes(), "Compute successors", std::cerr,
-                             !common::get_verbose());
+    boss.call_sequences_row_diff([&](const vector<uint64_t> &, std::optional<uint64_t>) {},
+                                 num_threads, max_length, &terminal, &dummy);
 
     // create the succ file, indexed using annotation indices
     uint32_t width = sdsl::bits::hi(graph.num_nodes()) + 1;
@@ -1121,31 +1111,62 @@ void build_successor(const graph::DBGSuccinct &graph,
     std::vector<bool> pred_boundary;
     sdsl::int_vector_buffer pred(outfbase + ".pred", std::ios::out, 1024 * 1024, width);
 
-    // TODO: parallelize this
-    for(uint64_t i = 1; i <= graph.num_nodes(); ++i) {
-        uint64_t boss_idx = graph.kmer_to_boss_index(i);
-        if (dummy[boss_idx] || terminal[boss_idx]) {
-            succ.push_back(0);
-        } else {
-            const graph::boss::BOSS::TAlphabet w = boss.get_W(boss_idx) % boss.alph_size;
-            uint64_t next = w ? graph.boss_to_kmer_index(boss.fwd(boss_idx, w)) : 0;
-            succ.push_back(next);
-        }
-        uint64_t back_idx = boss.bwd(boss_idx);
-        boss.call_incoming_to_target(
-                back_idx, boss.get_W(back_idx), [&](BOSS::edge_index incoming_edge) {
-                    // terminal and dummy predecessors are ignored; also ignore
-                    // predecessors for which boss_idx is not the last outgoing
-                    // edge (bc. we only traverse the last outgoing at a bifurcation)
-                    if (terminal[incoming_edge] || dummy[incoming_edge]
-                        || boss.fwd(incoming_edge, boss.get_W(incoming_edge) % boss.alph_size)
-                                != boss_idx)
-                        return;
+    // traverse BOSS table in parallel processing num_threads chunks of size 1'000'000
+    constexpr uint64_t chunk_size = 1'000'000;
+    const uint64_t batch_size = chunk_size * num_threads;
+    const uint64_t batch_count = (graph.num_nodes() - 1) / batch_size + 1;
 
-                    pred.push_back(graph.boss_to_kmer_index(incoming_edge));
-                    pred_boundary.push_back(0);
-                });
-        pred_boundary.push_back(1);
+    ProgressBar progress_bar(batch_count, "Compute successors", std::cerr,
+                             !common::get_verbose());
+
+    for(uint64_t batch = 0; batch < batch_count; ++batch) {
+        std::vector<std::vector<uint64_t>> pred_buf(num_threads);
+        std::vector<std::vector<uint64_t>> succ_buf(num_threads);
+        std::vector<std::vector<bool>> pred_boundary_buf(num_threads);
+
+        #pragma omp parallel for num_threads(num_threads) schedule(static, 1)
+        for (uint32_t chunk = 0; chunk < std::max((uint32_t)1, num_threads); ++ chunk) {
+            uint64_t start = batch * batch_size + chunk * chunk_size + 1;
+            for (uint64_t i = start;
+                 i < std::min(graph.num_nodes() + 1, start + chunk_size); ++i) {
+                uint64_t boss_idx = graph.kmer_to_boss_index(i);
+                if (dummy[boss_idx] || terminal[boss_idx]) {
+                    succ_buf[chunk].push_back(0);
+                } else {
+                    const graph::boss::BOSS::TAlphabet w
+                            = boss.get_W(boss_idx) % boss.alph_size;
+                    uint64_t next = w ? graph.boss_to_kmer_index(boss.fwd(boss_idx, w)) : 0;
+                    succ_buf[chunk].push_back(next);
+                }
+                uint64_t back_idx = boss.bwd(boss_idx);
+                boss.call_incoming_to_target(
+                        back_idx, boss.get_W(back_idx), [&](BOSS::edge_index incoming_edge) {
+                            // terminal and dummy predecessors are ignored; also ignore
+                            // predecessors for which boss_idx is not the last outgoing
+                            // edge (bc. we only traverse the last outgoing at a bifurcation)
+                            if (terminal[incoming_edge] || dummy[incoming_edge]
+                                || boss.fwd(incoming_edge,
+                                            boss.get_W(incoming_edge) % boss.alph_size)
+                                        != boss_idx)
+                                return;
+
+                            pred_buf[chunk].push_back(graph.boss_to_kmer_index(incoming_edge));
+                            pred_boundary_buf[chunk].push_back(0);
+                        });
+                pred_boundary_buf[chunk].push_back(1);
+            }
+        }
+        for (uint32_t i = 0; i < num_threads; ++i) {
+            for (uint32_t j = 0; j < succ_buf[i].size(); ++j) {
+                succ.push_back(succ_buf[i][j]);
+            }
+            for (uint32_t j = 0; j < pred_buf[i].size(); ++j) {
+                pred.push_back(pred_buf[i][j]);
+            }
+            for (uint32_t j = 0; j < pred_boundary_buf[i].size(); ++j) {
+                pred_boundary.push_back(pred_boundary_buf[i][j]);
+            }
+        }
         ++progress_bar;
     }
     succ.close();
@@ -1183,7 +1204,8 @@ void convert_batch_to_row_diff(const graph::DBGSuccinct &graph,
     if (source_files.empty())
         return;
 
-    build_successor(graph, graph_fname, max_depth, get_num_threads());
+    uint32_t num_threads = get_num_threads() == 0 ? 1 : get_num_threads();
+    build_successor(graph, graph_fname, max_depth, num_threads);
 
     std::vector<std::unique_ptr<annot::ColumnCompressed<>>> sources;
     for(const auto& fname : source_files) {
@@ -1211,7 +1233,7 @@ void convert_batch_to_row_diff(const graph::DBGSuccinct &graph,
             std::filesystem::path tmp_dir
                     = tmp_path/("col_" + std::to_string(i) + "_" + std::to_string(j));
             std::filesystem::create_directories(tmp_dir);
-            auto sorted_set = std::make_unique<SSD>(get_num_threads(), num_elements, tmp_dir,
+            auto sorted_set = std::make_unique<SSD>(num_threads, num_elements, tmp_dir,
                                                     std::numeric_limits<uint64_t>::max());
             targets[i].push_back(std::move(sorted_set));
         }
@@ -1262,7 +1284,7 @@ void convert_batch_to_row_diff(const graph::DBGSuccinct &graph,
 
         assert(pred_chunk.size() == pred_chunk_idx.back());
 
-        #pragma omp parallel for num_threads(get_num_threads()) schedule(dynamic)
+        #pragma omp parallel for num_threads(num_threads) schedule(dynamic)
         for (uint64_t l_idx = 0; l_idx < sources.size(); ++l_idx) {
             if (sources[l_idx]->num_labels()
                 && sources[l_idx]->num_objects() != graph.num_nodes()) {
@@ -1313,7 +1335,7 @@ void convert_batch_to_row_diff(const graph::DBGSuccinct &graph,
     std::vector<std::unique_ptr<ColumnCompressed<>>>().swap(sources);
 
     logger->trace("Generating row_diff columns...");
-    #pragma omp parallel for num_threads(get_num_threads()) schedule(dynamic)
+    #pragma omp parallel for num_threads(num_threads) schedule(dynamic)
     for (uint32_t l_idx = 0; l_idx < targets.size(); ++l_idx) {
         std::vector<std::unique_ptr<bit_vector>> columns(targets[l_idx].size());
         for (uint64_t l_idx2 = 0; l_idx2 < targets[l_idx].size(); ++l_idx2) {
