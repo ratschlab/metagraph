3828de352c5dde65e66ed95d4382823c80e56385
Mikhail Karasikov <mike-karas@ya.ru>
2020-11-20 18:35:24 +0100

    precompute the number of bits mergeed

--- metagraph/src/annotation/row_diff_builder.cpp
+++ metagraph/src/annotation/row_diff_builder.cpp
@@ -457,14 +457,20 @@ void convert_batch_to_row_diff(const std::string &pred_succ_fprefix,
                 //TODO: benchmark using Elias-Fano encoders + merger
                 common::merge_files<uint64_t>(filenames, call);
             };
-            try {
-                columns[j] = std::make_unique<bit_vector_sd>(call_ones, anchor.size(),
-                                                             row_diff_bits[l_idx][j]);
-            } catch (const std::exception &e) {
-                logger->error("Error when building column {}. Exception: {}",
-                              source_files[l_idx], e.what());
+
+            uint64_t num_rd_bits = 0;
+            call_ones([&](uint64_t) { num_rd_bits++; });
+
+            if (num_rd_bits != row_diff_bits[l_idx][j]) {
+                logger->error("Error when building column {}."
+                              " Num bits: {}, excepted {}.",
+                              source_files[l_idx],
+                              num_rd_bits, row_diff_bits[l_idx][j]);
                 columns[j] = std::make_unique<bit_vector_sd>([](auto) {}, anchor.size(), 0);
             }
+
+            columns[j] = std::make_unique<bit_vector_sd>(call_ones, anchor.size(),
+                                                         num_rd_bits);
         }
         row_diff[l_idx] = std::make_unique<RowDiffColumnAnnotator>(
                 std::make_unique<RowDiff<ColumnMajor>>(nullptr, ColumnMajor(std::move(columns))),
