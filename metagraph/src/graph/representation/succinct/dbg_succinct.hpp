7bf5522127ca4e8f334b61c816acd2596348d09c
Mikhail Karasikov <mike-karas@ya.ru>
2020-11-26 18:37:35 +0100

    added comment

--- metagraph/src/graph/representation/succinct/dbg_succinct.hpp
+++ metagraph/src/graph/representation/succinct/dbg_succinct.hpp
@@ -123,6 +123,32 @@ class DBGSuccinct : public DeBruijnGraph {
     virtual std::string file_extension() const override final { return kExtension; }
     std::string bloom_filter_file_extension() const { return kBloomFilterExtension; }
 
+    /*
+     * Available representations:
+     *    STAT: provides the best space/time trade-off
+     *         Representation:
+     *             last -- bit_vector_stat
+     *                W -- wavelet_tree_stat
+     *     valid_edges_ -- bit_vector_small
+     *
+     *    SMALL: is the smallest, useful for storage or when RAM is limited
+     *         Representation:
+     *             last -- bit_vector_small
+     *                W -- wavelet_tree_small
+     *     valid_edges_ -- bit_vector_small
+     *
+     *    FAST: is the fastest but large
+     *         Representation:
+     *             last -- bit_vector_stat
+     *                W -- wavelet_tree_fast
+     *     valid_edges_ -- bit_vector_stat
+     *
+     *    DYN: is a dynamic representation supporting insert and delete
+     *         Representation:
+     *             last -- bit_vector_dyn
+     *                W -- wavelet_tree_dyn
+     *     valid_edges_ -- bit_vector_dyn
+     */
     virtual void switch_state(boss::BOSS::State new_state) final;
     virtual boss::BOSS::State get_state() const final;
 
