03d5b2d55b889e691842a513f5bac5b63123fa85
Daniel Danciu <dd@daedalean.ai>
2020-12-14 18:26:35 +0100

    Simplify test_build_phase

--- metagraph/integration_tests/test_build.py
+++ metagraph/integration_tests/test_build.py
@@ -339,39 +339,26 @@ class TestBuild(unittest.TestCase):
     @parameterized.expand(['succinct_disk'])
     def test_build_phase(self, build):
         representation, tmp_dir = build_params[build]
-
         construct_command = '{exe} build --phase 1 --mask-dummy --graph {repr} --canonical -k 20 ' \
                             '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
             exe=METAGRAPH,
             repr=representation,
-            tmp_dir='""' if tmp_dir == '""' else self.tempdir.name,
+            tmp_dir=self.tempdir.name,
             outfile=self.tempdir.name + '/graph',
             input=TEST_DATA_DIR + '/transcripts_1000.fa'
         )
+        print(f'Executing phase1: {construct_command}')
         res = subprocess.run([construct_command], shell=True)
         self.assertEqual(res.returncode, 0)
         self.assertTrue(os.path.isfile(self.tempdir.name + '/graph.checkpoint'))
 
-        construct_command = '{exe} build --phase 2 --mask-dummy --graph {repr} --canonical -k 20 ' \
-                            '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
-            exe=METAGRAPH,
-            repr=representation,
-            tmp_dir='""' if tmp_dir == '""' else self.tempdir.name,
-            outfile=self.tempdir.name + '/graph',
-            input=TEST_DATA_DIR + '/transcripts_1000.fa'
-        )
+        construct_command = construct_command.replace('--phase 1', '--phase 2')
+        print(f'Executing phase2: {construct_command}')
         res = subprocess.run([construct_command], shell=True)
         self.assertEqual(res.returncode, 0)
         self.assertTrue(os.path.isfile(self.tempdir.name + '/graph.checkpoint'))
 
-        construct_command = '{exe} build --mask-dummy --graph {repr} --canonical -k 20 ' \
-                            '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
-            exe=METAGRAPH,
-            repr=representation,
-            tmp_dir='""' if tmp_dir == '""' else self.tempdir.name,
-            outfile=self.tempdir.name + '/graph',
-            input=TEST_DATA_DIR + '/transcripts_1000.fa'
-        )
+        construct_command = construct_command.replace('--phase 2', '')
         res = subprocess.run([construct_command], shell=True)
         self.assertEqual(res.returncode, 0)
 
@@ -387,13 +374,12 @@ class TestBuild(unittest.TestCase):
     @parameterized.expand(['succinct_disk'])
     def test_build_phase_parallel(self, build):
         representation, tmp_dir = build_params[build]
-
         for name in ('graph1', 'graph2'):
             construct_command = '{exe} build --phase 2 --mask-dummy --graph {repr} --canonical -k 20 ' \
                                 '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
                 exe=METAGRAPH,
                 repr=representation,
-                tmp_dir='""' if tmp_dir == '""' else self.tempdir.name,
+                tmp_dir=self.tempdir.name,
                 outfile=self.tempdir.name + '/' + name,
                 input=TEST_DATA_DIR + ('/transcripts_1000.fa' if name == 'graph1' else '/transcripts_100.fa')
             )
@@ -406,7 +392,7 @@ class TestBuild(unittest.TestCase):
                                 '--disk-swap {tmp_dir} -o {outfile} {input}'.format(
                 exe=METAGRAPH,
                 repr=representation,
-                tmp_dir='""' if tmp_dir == '""' else self.tempdir.name,
+                tmp_dir=self.tempdir.name,
                 outfile=self.tempdir.name + '/' + name,
                 input=TEST_DATA_DIR + ('/transcripts_1000.fa' if name == 'graph1' else '/transcripts_100.fa')
             )
