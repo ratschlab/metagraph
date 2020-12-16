e76956f3b0f72eabc4546cfc2673ac31fe507fd2
Harun Mustafa <bilgisayaradami@gmail.com>
2020-12-16 19:36:20 +0100

    mac fixes

--- metagraph/src/common/serialization.cpp
+++ metagraph/src/common/serialization.cpp
@@ -7,9 +7,19 @@
 #include <unordered_map>
 
 #ifndef __APPLE__
+
 #include <endian.h>
+
 #else
-#include <machine/endian.h>
+
+#include <libkern/OSByteOrder.h>
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+
 #endif
 
 #include <tsl/hopscotch_map.h>
