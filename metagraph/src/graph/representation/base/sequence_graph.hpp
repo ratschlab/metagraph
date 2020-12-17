18cb47cd3fa3f1003754c6148400ea1832b1faf9
Harun Mustafa <bilgisayaradami@gmail.com>
2020-12-17 16:26:02 +0100

    fix npos case

--- metagraph/src/graph/representation/base/sequence_graph.hpp
+++ metagraph/src/graph/representation/base/sequence_graph.hpp
@@ -230,7 +230,9 @@ class DeBruijnGraph : public SequenceGraph {
     // Call all nodes that have no incoming edges
     virtual void call_source_nodes(const std::function<void(node_index)> &callback) const;
 
-    virtual size_t get_node_length(node_index) const { return get_k(); }
+    virtual size_t get_node_length(node_index node) const {
+        return node ? get_k() : 0;
+    }
 };
 
 
