70ec920ab765ddf26ca77715549b94752cc53ed4
Radu Muntean <radu.muntean.2@gmail.com>
2021-02-01 16:09:40 +0100

    (draft) return only the taxid in taxonomic classification

--- metagraph/src/annotation/taxonomy/taxo_classifier.cpp
+++ metagraph/src/annotation/taxonomy/taxo_classifier.cpp
@@ -68,6 +68,7 @@ TaxoClassifier::TaxoClassifier(const std::string &filepath) {
 std::string TaxoClassifier::assign_class(const mtg::graph::DeBruijnGraph &graph,
                                          const std::string &sequence,
                                          const double &lca_coverage_threshold) {
+    std::cout << "\n\n\n";
     tsl::hopscotch_map<NormalizedTaxId, uint64_t> raw_node_matches;
     uint64_t total_kmers = 0;
 
@@ -78,13 +79,22 @@ std::string TaxoClassifier::assign_class(const mtg::graph::DeBruijnGraph &graph,
             total_kmers++;
         }
     });
+
+    std::cout << "raw_node_matches\n";
+    for (auto &it: raw_node_matches) {
+        std::cout << "fst=" << it.first << " scd=" << it.second << "\n";
+    }
+
     tsl::hopscotch_set<NormalizedTaxId> nodes_already_propagated;
+    tsl::hopscotch_map<NormalizedTaxId, uint64_t> node_scores;
 
     uint64_t desired_number_kmers = total_kmers * lca_coverage_threshold;
     NormalizedTaxId best_lca = root_node;
     uint64_t best_lca_dist_to_root = 1;
+    std::cout << "iterate raw nodes \n";
     for (const auto &node_pair: raw_node_matches) {
         uint64_t start_node = node_pair.first;
+        std::cout << "\tstart_node=" << start_node << "\n";
         if (nodes_already_propagated.count(start_node)) {
             continue;
         }
@@ -103,16 +113,17 @@ std::string TaxoClassifier::assign_class(const mtg::graph::DeBruijnGraph &graph,
                 if (raw_node_matches.count(act_node)) {
                     score_from_unprocessed_parents += raw_node_matches[act_node];
                 }
+                std::cout << "\tadd unprocessed parent=" << act_node << "\n";
                 unprocessed_parents.push_back(act_node);
             } else {
                 if (raw_node_matches.count(act_node)) {
                     score_from_processed_parents += raw_node_matches[act_node];
                 }
+                std::cout << "\tadd processed parent=" << act_node << "\n";
                 processed_parents.push_back(act_node);
             }
         }
-
-        tsl::hopscotch_map<NormalizedTaxId, uint64_t> node_scores;
+        std::cout << "\n";
         for (uint64_t i = 0; i < unprocessed_parents.size(); ++i) {
             NormalizedTaxId &act_node = unprocessed_parents[i];
             node_scores[act_node] = score_from_processed_parents +
@@ -126,6 +137,7 @@ std::string TaxoClassifier::assign_class(const mtg::graph::DeBruijnGraph &graph,
                 best_lca = act_node;
                 best_lca_dist_to_root = act_dist_to_root;
             }
+            std::cout << "\tnode_scores[" << act_node << "]=" << node_scores[act_node] / (double)total_kmers << " act_dist_to_root=" << act_dist_to_root << "\n";
         }
         for (uint64_t i = 0; i < processed_parents.size(); ++i) {
             NormalizedTaxId &act_node = processed_parents[i];
@@ -137,8 +149,10 @@ std::string TaxoClassifier::assign_class(const mtg::graph::DeBruijnGraph &graph,
                 best_lca = act_node;
                 best_lca_dist_to_root = act_dist_to_root;
             }
+            std::cout << "\tnode_scores[" << act_node << "]=" << node_scores[act_node] / (double)total_kmers << " act_dist_to_root=" << act_dist_to_root << "\n";
         }
     }
+    std::cout << "best_lca=" << best_lca << std::endl;
     return node_to_acc_version[best_lca];
 }
 
