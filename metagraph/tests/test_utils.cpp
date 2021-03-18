115021c44a2430ccfbe4c4a67df259a7e5827eb6
Mikhail Karasikov <mike-karas@ya.ru>
2021-03-19 00:21:32 +0100

    more tests

--- metagraph/tests/test_utils.cpp
+++ metagraph/tests/test_utils.cpp
@@ -1013,6 +1013,26 @@ TEST(int_vector_buffer, small_buffer) {
     }
 }
 
+TEST(int_vector_buffer, seek_to_front_65) {
+    const size_t width = 1;
+    const size_t buffer_size = 8; // buffer size 8 bytes
+
+    // initialize buffer
+    {
+        sdsl::int_vector_buffer<> buf(test_dump_basename + "ivb",
+                                      std::ios::out, buffer_size, width);
+    }
+
+    sdsl::int_vector_buffer<> buf(test_dump_basename + "ivb",
+                                  std::ios::in | std::ios::out, buffer_size, width);
+    for (size_t i = 0; i < 65; ++i) {
+        buf.push_back(1);
+    }
+
+    ASSERT_EQ(1, buf[0]);
+    ASSERT_EQ(1, buf[64]);
+}
+
 TEST(int_vector_buffer, seek_to_front) {
     const size_t size = 1'000'000;
     const size_t width = 1;
@@ -1030,6 +1050,33 @@ TEST(int_vector_buffer, seek_to_front) {
                                   std::ios::in | std::ios::out, buffer_size, width);
     for (size_t i = 0; i < size; ++i) {
         buf.push_back(1);
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+        ASSERT_EQ(1, buf[i]);
+    }
+}
+
+TEST(int_vector_buffer, change_and_seek_to_front) {
+    const size_t size = 1'000'000;
+    const size_t width = 1;
+    const size_t buffer_size = 10'000;
+
+    sdsl::int_vector<> vector(size, 1, width);
+
+    // initialize buffer
+    {
+        sdsl::int_vector_buffer<> buf(test_dump_basename + "ivb",
+                                      std::ios::out, buffer_size, width);
+        for (size_t i = 0; i < size; ++i) {
+            buf.push_back(0);
+        }
+    }
+
+    sdsl::int_vector_buffer<> buf(test_dump_basename + "ivb",
+                                  std::ios::in | std::ios::out, buffer_size, width);
+    for (size_t i = 0; i < size; ++i) {
+        buf[i] = 1;
     }
 
     for (size_t i = 0; i < size; ++i) {
