3500fbd02b2072cf20a5df1e051ae524636df58e
Radu Muntean <radu.muntean.2@gmail.com>
2021-04-09 14:45:18 +0200

    delete the sequentially processing of transform_anno_tax
    Signed-off-by: Radu Muntean <radu.muntean.2@gmail.com>

--- metagraph/src/cli/transform_anno_tax.cpp
+++ metagraph/src/cli/transform_anno_tax.cpp
@@ -39,18 +39,16 @@ std::vector<std::string> get_anno_filenames(const std::vector<std::string> &path
     return filenames;
 }
 
-void call_taxonomy_map_updates(const std::vector<std::string> &filenames,
+void call_taxonomy_map_updates(const std::string &filename,
                                cli::Config *config,
                                annot::TaxonomyDB *taxonomy) {
-    for (uint64_t i = 0; i < filenames.size(); ++i) {
-        std::unique_ptr<annot::MultiLabelEncoded<std::string>> annot =
-                cli::initialize_annotation(filenames[i], *config);
-        if (!annot->load(filenames[i])) {
-            logger->error("Filed to load annotations from file {}.", filenames[i]);
-            exit(1);
-        }
-        taxonomy->kmer_to_taxid_map_update(*annot);
+    std::unique_ptr<annot::MultiLabelEncoded<std::string>> annot =
+            cli::initialize_annotation(filename, *config);
+    if (!annot->load(filename)) {
+        logger->error("Filed to load annotations from file {}.", filename);
+        exit(1);
     }
+    taxonomy->kmer_to_taxid_map_update(*annot);
 }
 
 int transform_anno_tax(Config *config) {
@@ -75,27 +73,8 @@ int transform_anno_tax(Config *config) {
     annot::TaxonomyDB taxonomy(config->taxonomic_tree, config->label_taxid_map, all_labels);
 
     // Load as many annotation matrix files as we can fit in memory.
-    for (uint32_t i = 0; i < filenames.size(); ) {
-        logger->trace("Loading a new batch of files...");
-        size_t mem_bytes_left = config->memory_available * 1e9;
-        std::vector<std::string> file_batch;
-        for ( ; i < filenames.size(); ++i) {
-            uint64_t file_size = std::filesystem::file_size(filenames[i]);
-
-            // If one file only exceeds the memory available, evaluate it, but print a warning log.
-            if (file_size > mem_bytes_left && file_batch.size() > 0) {
-                break;
-            }
-            if (file_size > config->memory_available * 1e9) {
-                logger->warn(
-                        "File {} requires {} MB, while available are only {} MB. Can't optimize the processing of this file.",
-                        filenames[i], file_size / 1e6, config->memory_available / 1e3);
-            }
-
-            mem_bytes_left -= file_size;
-            file_batch.push_back(filenames[i]);
-        }
-        call_taxonomy_map_updates(file_batch, config, &taxonomy);
+    for (uint32_t i = 0; i < filenames.size(); ++i) {
+        call_taxonomy_map_updates(filenames[i], config, &taxonomy);
     }
 
     taxonomy.export_to_file(config->outfbase + ".taxdb");
