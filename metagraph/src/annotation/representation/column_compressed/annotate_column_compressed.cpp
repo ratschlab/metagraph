2fbf99fa41d017f8888145a76259aedb4704dcf6
Mikhail Karasikov <mike-karas@ya.ru>
2021-04-09 19:23:55 +0200

    create outdir for annotation with --separately, also improved error messages

--- metagraph/src/annotation/representation/column_compressed/annotate_column_compressed.cpp
+++ metagraph/src/annotation/representation/column_compressed/annotate_column_compressed.cpp
@@ -151,7 +151,8 @@ void ColumnCompressed<Label>::serialize(const std::string &filename) const {
     std::ofstream outstream(remove_suffix(filename, kExtension) + kExtension,
                             std::ios::binary);
     if (!outstream.good()) {
-        logger->trace("Could not open {}", remove_suffix(filename, kExtension) + kExtension);
+        logger->error("Could not open file for writing: {}",
+                      remove_suffix(filename, kExtension) + kExtension);
         throw std::ofstream::failure("Bad stream");
     }
 
@@ -172,7 +173,7 @@ void ColumnCompressed<Label>::serialize(const std::string &filename) const {
     outstream.open(remove_suffix(filename, kExtension) + kExtension + ".counts",
                    std::ios::binary);
     if (!outstream.good()) {
-        logger->trace("Could not open {}",
+        logger->error("Could not open file for writing: {}",
                       remove_suffix(filename, kExtension) + kExtension + ".counts");
         throw std::ofstream::failure("Bad stream");
     }
