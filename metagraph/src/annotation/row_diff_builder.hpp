5a1a20fe2322c44325e178b929692634b333d2b1
Mikhail Karasikov <mike-karas@ya.ru>
2021-06-22 12:44:46 +0100

    adapted query algorithm for multiple fork successors

--- metagraph/src/annotation/row_diff_builder.hpp
+++ metagraph/src/annotation/row_diff_builder.hpp
@@ -28,7 +28,7 @@ void assign_anchors(const std::string &graph_filename,
                     uint32_t max_length,
                     const std::string &row_reduction_extension,
                     uint32_t num_threads,
-                    bool multiple_fork_successors = false);
+                    bool multiple_fork_successors);
 
 void convert_batch_to_row_diff(const std::string &pred_succ_fprefix,
                                const std::vector<std::string> &source_files,
