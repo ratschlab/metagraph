01888393c06cde41e62ccd27e77b0462891b94ea
Harun Mustafa <bilgisayaradami@gmail.com>
2021-07-23 14:39:06 +0200

    make everything final

--- metagraph/src/graph/representation/canonical_dbg.hpp
+++ metagraph/src/graph/representation/canonical_dbg.hpp
@@ -32,75 +32,75 @@ class CanonicalDBG : public DBGWrapper {
     virtual ~CanonicalDBG() {}
 
     virtual void add_sequence(std::string_view sequence,
-                              const std::function<void(node_index)> &on_insertion = [](node_index) {}) override;
+                              const std::function<void(node_index)> &on_insertion = [](node_index) {}) override final;
 
     // Traverse graph mapping sequence to the graph nodes
     // and run callback for each node until the termination condition is satisfied
     virtual void map_to_nodes(std::string_view sequence,
                               const std::function<void(node_index)> &callback,
-                              const std::function<bool()> &terminate = [](){ return false; }) const override;
+                              const std::function<bool()> &terminate = [](){ return false; }) const override final;
 
     // Traverse graph mapping sequence to the graph nodes
     // and run callback for each node until the termination condition is satisfied.
     // Guarantees that nodes are called in the same order as the input sequence
     virtual void map_to_nodes_sequentially(std::string_view sequence,
                                            const std::function<void(node_index)> &callback,
-                                           const std::function<bool()> &terminate = [](){ return false; }) const override;
+                                           const std::function<bool()> &terminate = [](){ return false; }) const override final;
 
     // Given a node index, call the target nodes of all edges outgoing from it.
     virtual void adjacent_outgoing_nodes(node_index node,
-                                         const std::function<void(node_index)> &callback) const override;
+                                         const std::function<void(node_index)> &callback) const override final;
 
     virtual void call_outgoing_kmers(node_index kmer,
-                                     const OutgoingEdgeCallback &callback) const override;
+                                     const OutgoingEdgeCallback &callback) const override final;
 
     virtual void call_incoming_kmers(node_index kmer,
-                                     const IncomingEdgeCallback &callback) const override;
+                                     const IncomingEdgeCallback &callback) const override final;
 
     // Given a node index, call the source nodes of all edges incoming to it.
     virtual void adjacent_incoming_nodes(node_index node,
-                                         const std::function<void(node_index)> &callback) const override;
+                                         const std::function<void(node_index)> &callback) const override final;
 
     virtual void call_sequences(const CallPath &callback,
                                 size_t num_threads = 1,
-                                bool kmers_in_single_form = false) const override;
+                                bool kmers_in_single_form = false) const override final;
 
     virtual void call_unitigs(const CallPath &callback,
                               size_t num_threads = 1,
                               size_t min_tip_size = 1,
-                              bool kmers_in_single_form = false) const override;
+                              bool kmers_in_single_form = false) const override final;
 
-    virtual uint64_t num_nodes() const override { return graph_.num_nodes() * 2; }
-    virtual uint64_t max_index() const override { return graph_.max_index() * 2; }
+    virtual uint64_t num_nodes() const override final { return graph_.num_nodes() * 2; }
+    virtual uint64_t max_index() const override final { return graph_.max_index() * 2; }
 
     // Get string corresponding to |node_index|.
     // Note: Not efficient if sequences in nodes overlap. Use sparingly.
-    virtual std::string get_node_sequence(node_index index) const override;
+    virtual std::string get_node_sequence(node_index index) const override final;
 
     virtual Mode get_mode() const override { return CANONICAL; }
 
     // Traverse the outgoing edge
-    virtual node_index traverse(node_index node, char next_char) const override;
+    virtual node_index traverse(node_index node, char next_char) const override final;
     // Traverse the incoming edge
-    virtual node_index traverse_back(node_index node, char prev_char) const override;
+    virtual node_index traverse_back(node_index node, char prev_char) const override final;
 
-    virtual size_t outdegree(node_index) const override;
-    virtual size_t indegree(node_index) const override;
+    virtual size_t outdegree(node_index) const override final;
+    virtual size_t indegree(node_index) const override final;
 
     virtual void call_nodes(const std::function<void(node_index)> &callback,
-                            const std::function<bool()> &stop_early = [](){ return false; }) const override;
+                            const std::function<bool()> &stop_early = [](){ return false; }) const override final;
 
-    virtual bool operator==(const CanonicalDBG &other) const {
+    bool operator==(const CanonicalDBG &other) const {
         return graph_ == other.graph_;
     }
 
-    virtual bool operator==(const DeBruijnGraph &other) const override;
+    virtual bool operator==(const DeBruijnGraph &other) const override final;
 
-    virtual bool load(const std::string & /* filename */) override {
+    virtual bool load(const std::string & /* filename */) override final {
         throw std::runtime_error("Not implemented");
     }
 
-    virtual void serialize(const std::string & /* filename */) const override {
+    virtual void serialize(const std::string & /* filename */) const override final {
         throw std::runtime_error("Not implemented");
     }
 
