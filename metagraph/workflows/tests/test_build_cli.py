aa8a2f8ccf54369d5cd81d45719abca942ea6a0c
Marc Zimmermann <marc.zimmermann@inf.ethz.ch>
2021-10-19 09:51:48 +0200

    moving 'build' subcommand related stuff to cli.py

--- metagraph/workflows/tests/test_build_cli.py
+++ metagraph/workflows/tests/test_build_cli.py
@@ -6,8 +6,7 @@ from pathlib import Path
 import pytest
 
 import metagraph_workflows
-from metagraph_workflows import utils
-from metagraph_workflows import workflows
+from metagraph_workflows import cli, utils
 from metagraph_workflows.workflow_configs import AnnotationLabelsSource, \
     AnnotationFormats
 
@@ -61,7 +60,7 @@ def test_build_workflow(primary, annotation_format, annotation_label_src, sample
 
 
 def test_workflow_invocation_via_python(sample_list_path, output_dir):
-    assert workflows.run_build_workflow(output_dir, seqs_file_list_path=sample_list_path)
+    assert cli.run_build_workflow(output_dir, seqs_file_list_path=sample_list_path)
 
 
 def test_workflow_invocation_additional_args(sample_list_path, output_dir):
--- metagraph/workflows/tests/test_workflows.py
+++ metagraph/workflows/tests/test_workflows.py
@@ -1,12 +1,12 @@
 
-from metagraph_workflows import workflows
+from metagraph_workflows import cli
 
 
 def test_parse_additional_snakemake_args():
-    assert workflows._parse_additional_snakemake_args('') == {}
-    assert workflows._parse_additional_snakemake_args('dryrun=1') == {
+    assert cli._parse_additional_snakemake_args('') == {}
+    assert cli._parse_additional_snakemake_args('dryrun=1') == {
         'dryrun': True}
 
-    assert workflows._parse_additional_snakemake_args(
+    assert cli._parse_additional_snakemake_args(
         'some_param="hello world" another=123') == {
                'some_param': 'hello world', 'another': 123}
