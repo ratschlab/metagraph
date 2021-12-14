49d2b5b0d63fabffe8342c58e72bf878b23eb8c8
Thomas Zhou <thomas@tzhou.me>
2021-12-14 13:08:01 +0100

    Ensure regular API integration tests do not use parallel search

--- metagraph/integration_tests/test_api.py
+++ metagraph/integration_tests/test_api.py
@@ -216,24 +216,28 @@ class TestAPIClient(TestAPIBase):
 
     def test_api_multiple_query_df(self):
         repetitions = 5
-        ret = self.graph_client.search([self.sample_query] * repetitions, discovery_threshold=0.01)
+        ret = self.graph_client.search([self.sample_query] * repetitions, parallel=False,
+                                       discovery_threshold=0.01)
         df = ret[self.graph_name]
         self.assertEqual((self.sample_query_expected_rows * repetitions, 3), df.shape)
 
     def test_api_simple_query_df(self):
-        ret = self.graph_client.search(self.sample_query, discovery_threshold=0.01)
+        ret = self.graph_client.search(self.sample_query, parallel=False,
+                                       discovery_threshold=0.01)
         df = ret[self.graph_name]
 
         self.assertEqual((self.sample_query_expected_rows, 3), df.shape)
 
     def test_api_simple_query_with_signature_df(self):
-        ret = self.graph_client.search(self.sample_query, discovery_threshold=0.01, with_signature=True)
+        ret = self.graph_client.search(self.sample_query, parallel=False,
+                                       discovery_threshold=0.01, with_signature=True)
         df = ret[self.graph_name]
 
         self.assertEqual((self.sample_query_expected_rows, 4), df.shape)
 
     def test_api_simple_query_align_df(self):
-        ret = self.graph_client.search(self.sample_query, discovery_threshold=0.01, align=True, min_exact_match=0.01)
+        ret = self.graph_client.search(self.sample_query, parallel=False,
+                                       discovery_threshold=0.01, align=True, min_exact_match=0.01)
         df = ret[self.graph_name]
 
         self.assertEqual((self.sample_query_expected_rows, 3), df.shape)
@@ -252,7 +256,8 @@ class TestAPIClient(TestAPIBase):
         repetitions = 4
         alignment_cnt = 3
         seq = ["TCGATCGATCGATCGATCGATCGACGATCGATCGATCGATCGATCGACGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGA"]
-        ret = self.graph_client.align(seq * repetitions, max_alternative_alignments=alignment_cnt)
+        ret = self.graph_client.align(seq * repetitions, parallel=False,
+                                      max_alternative_alignments=alignment_cnt)
 
         align_res = ret[self.graph_name]
         self.assertIn('cigar', align_res.columns)
@@ -264,7 +269,8 @@ class TestAPIClient(TestAPIBase):
         repetitions = 4
         alignment_cnt = 3
         seq = ["TCGATCGATCGATCGATCGATCGACGATCGATCGATCGATCGATCGACGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGA"]
-        ret = self.graph_client.align(seq * repetitions, max_alternative_alignments=alignment_cnt, min_exact_match=1.0)
+        ret = self.graph_client.align(seq * repetitions, parallel=False,
+                                      max_alternative_alignments=alignment_cnt, min_exact_match=1.0)
 
         align_res = ret[self.graph_name]
         self.assertIn('cigar', align_res.columns)
@@ -344,7 +350,7 @@ class TestAPIClientWithProperties(TestAPIBase):
         cls.graph_client.add_graph(cls.host, cls.port, cls.graph_name)
 
     def test_api_search_property_df(self):
-        df = self.graph_client.search(self.sample_query)[self.graph_name]
+        df = self.graph_client.search(self.sample_query, parallel=False)[self.graph_name]
 
         self.assertIn('kmer_count', df.columns)
         self.assertEqual(df['kmer_count'].dtype, int)
@@ -353,5 +359,5 @@ class TestAPIClientWithProperties(TestAPIBase):
         self.assertEqual(df.shape, (3, 10))
 
     def test_api_search_property_df_empty(self):
-        df = self.graph_client.search("THISSEQUENCEDOESNOTEXIST")[self.graph_name]
+        df = self.graph_client.search("THISSEQUENCEDOESNOTEXIST", parallel=False)[self.graph_name]
         self.assertTrue(df.empty)
