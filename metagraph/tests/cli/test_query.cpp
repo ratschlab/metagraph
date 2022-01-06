e7a45b192123c6adf994385cdeb10be0d8d7f0fb
Thomas Zhou <thomas@tzhou.me>
2022-01-06 22:32:37 +1000

    collapse_coords tests and brief docs

--- metagraph/tests/cli/test_query.cpp
+++ metagraph/tests/cli/test_query.cpp
@@ -10,11 +10,51 @@ namespace {
 
 using namespace mtg;
 
-TEST(collapse_coord_ranges, sample) {
+TEST(collapse_coord_ranges, standard) {
     std::vector<SmallVector<uint64_t>> tuples = { { 1, 2, 3 },
                                                   { 2, 3, 4 } };
 
     EXPECT_EQ(std::vector<std::string>({ "0-1-2", "0-2-3", "0-3-4" }),
+              cli::collapse_coord_ranges(tuples));
+
+    // test longer
+    std::vector<SmallVector<uint64_t>> longer = { { 1, 2, 3 },
+                                                  { 2, 3, 4 },
+                                                  { 3, 4, 5 },
+                                                  { 4, 5, 6 } };
+
+    EXPECT_EQ(std::vector<std::string>({ "0-1-4", "0-2-5", "0-3-6" }),
+              cli::collapse_coord_ranges(longer));
+}
+
+TEST(collapse_coord_ranges, staggered) {
+    std::vector<SmallVector<uint64_t>> tuples = { { 1, 2, 3 },
+                                                  { 3, 4, 5 } };
+
+    EXPECT_EQ(std::vector<std::string>({ "0-1", "0-2-3", "0-3-4", "1-5" }),
+              cli::collapse_coord_ranges(tuples));
+}
+
+TEST(collapse_coord_ranges, disjoint) {
+    std::vector<SmallVector<uint64_t>> tuples = { { 1, 2, 3 },
+                                                  { 5, 6, 7 } };
+
+    EXPECT_EQ(std::vector<std::string>({ "0-1", "0-2", "0-3", "1-5", "1-6", "1-7" }),
+              cli::collapse_coord_ranges(tuples));
+
+    std::vector<SmallVector<uint64_t>> longer = { { 1, 2, 3 },
+                                                  { 5, 6, 7 },
+                                                  { 6, 7, 8 },
+                                                  { 7, 8, 9 } };
+
+    EXPECT_EQ(std::vector<std::string>({ "0-1", "0-2", "0-3", "1-5-7", "1-6-8", "1-7-9" }),
+              cli::collapse_coord_ranges(longer));
+}
+
+TEST(collapse_coord_ranges, empty) {
+    std::vector<SmallVector<uint64_t>> tuples = {};
+
+    EXPECT_EQ(std::vector<std::string>(),
               cli::collapse_coord_ranges(tuples));
 }
 
