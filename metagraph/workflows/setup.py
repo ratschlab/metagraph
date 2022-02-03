aa206bfc9cc137e4fca3796ffa2e2fd5cd836132
Mikhail Karasikov <mike-karas@ya.ru>
2022-02-03 18:46:04 +0100

    test docker build

--- metagraph/workflows/setup.py
+++ metagraph/workflows/setup.py
@@ -4,6 +4,7 @@
 """The setup script."""
 
 import json
+import os
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -16,7 +17,7 @@ with open('requirements.txt') as f:
 
 test_requirements = ['pytest']
 
-with open('../../package.json') as f:
+with open(os.path.abspath(os.path.dirname(__file__)) + '/../../package.json') as f:
     version = json.load(f)['version']
 
 setup(
