2f15636f9e7808643ffac0e2e0ae7ef20d58b7f5
Harun Mustafa <bilgisayaradami@gmail.com>
2022-07-18 18:58:43 +0200

    fix

--- metagraph/src/graph/alignment/aligner_chainer.cpp
+++ metagraph/src/graph/alignment/aligner_chainer.cpp
@@ -686,20 +686,19 @@ std::vector<Alignment> chain_alignments(const IDBGAligner &aligner,
             continue;
 
         for (const auto &[a_suffix_trim, tab] : chain_table[i]) {
-            Alignment a_base = alignments[i];
-            a_base.trim_query_suffix(a_suffix_trim, config);
-            assert(a_base.size());
+            Alignment a = alignments[i];
+            a.trim_query_suffix(a_suffix_trim, config);
+            assert(a.size());
+            assert(a.is_valid(graph, &config));
+#ifndef NDEBUG
+            const char *query_start = a.get_query_view().data() - a.get_clipping();
+#endif
 
             for (const auto &[cur_columns, vals] : tab) {
                 const auto &[a_score, a_extra_score, a_gap, a_prefix_trim,
                              a_last_table_i, a_last_labels, a_last_suffix_trim] = vals;
-                Alignment a = a_base;
-                a.trim_query_prefix(a_prefix_trim, node_overlap, config);
-                assert(a.size());
-                assert(a.is_valid(graph, &config));
-
-                const char *chain_begin = a.get_query_view().data();
-                const char *chain_end = chain_begin + a.get_query_view().size();
+                const char *chain_begin = a.get_query_view().data() + a_prefix_trim;
+                const char *chain_end = a.get_query_view().data() + a.get_query_view().size();
 
                 for (size_t j = i + 1; j < alignments.size(); ++j) {
                     if (a.get_orientation() != alignments[j].get_orientation())
@@ -731,7 +730,7 @@ std::vector<Alignment> chain_alignments(const IDBGAligner &aligner,
                         }
 
                         assert(b_base.size());
-                        assert(chain_begin - a.get_clipping() == next_begin - b_base.get_clipping());
+                        assert(query_start == next_begin - b_base.get_clipping());
                         assert(next_begin + b_base.get_query_view().size() == next_end);
                         assert(b_base.is_valid(graph, &config));
 
@@ -744,13 +743,17 @@ std::vector<Alignment> chain_alignments(const IDBGAligner &aligner,
                             if (b.empty())
                                 continue;
 
-                            assert(chain_begin - a.get_clipping() == next_begin - b.get_clipping());
+                            assert(query_start == next_begin - b.get_clipping());
                             assert(b.is_valid(graph, &config));
                         }
 
                         auto label_change_scores = get_label_change_scores(cur_columns, b_col);
 
                         ssize_t gap = next_begin - chain_end;
+                        if (0 >= node_overlap + gap) {
+                            // DO THINGS
+                            continue;
+                        }
 
                         auto last_is_insertion = [&](const Cigar &a) {
                             auto it = a.data().rbegin();
@@ -793,7 +796,7 @@ std::vector<Alignment> chain_alignments(const IDBGAligner &aligner,
                             gap = std::max(gap, -node_overlap);
 
                             ssize_t gapoffset = gap + static_cast<ssize_t>(b.get_offset());
-                            if (gap + node_overlap <= 2 || gapoffset < 0 || static_cast<ssize_t>(b.size()) <= gapoffset)
+                            if (gapoffset < 0 || static_cast<ssize_t>(b.size()) <= gapoffset)
                                 continue;
 
                             if (is_insertion_junction(a.get_cigar(), b.get_cigar()))
@@ -803,6 +806,7 @@ std::vector<Alignment> chain_alignments(const IDBGAligner &aligner,
 #ifndef NDEBUG
                         DEBUG_LOG("Chain: {}\t{}\t{}", gap, a, b);
                         Alignment a_test = a;
+                        a_test.trim_query_prefix(a_prefix_trim, node_overlap, config);
                         a_test.trim_end_clipping();
                         Alignment b_test = b;
                         b_test.insert_gap_prefix(gap, node_overlap, config);
