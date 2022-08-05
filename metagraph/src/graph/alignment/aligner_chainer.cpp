d82ddd4ecc79cef39979365888a881785d0a23a4
Harun Mustafa <bilgisayaradami@gmail.com>
2022-08-05 16:29:23 +0200

    test

--- metagraph/src/graph/alignment/aligner_chainer.cpp
+++ metagraph/src/graph/alignment/aligner_chainer.cpp
@@ -8,7 +8,7 @@
 #include "aligner_labeled.hpp"
 
 #include "common/aligned_vector.hpp"
-#include "common/algorithms.hpp"
+#include "graph/representation/rc_dbg.hpp"
 #include "graph/graph_extensions/unitigs.hpp"
 
 namespace mtg {
