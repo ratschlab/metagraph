108ed4005fac9ca31b17735d5960c08677362c39
Mikhail Karasikov <mike-karas@ya.ru>
2022-08-08 12:37:44 +0200

    fix

--- metagraph/src/annotation/row_diff_builder.cpp
+++ metagraph/src/annotation/row_diff_builder.cpp
@@ -33,14 +33,11 @@ using rd_succ_bv_type = RowDiff<ColumnMajor>::fork_succ_bv_type;
 template <typename T>
 using Encoder = mtg::elias_fano::EliasFanoEncoderBuffered<T>;
 
-#define __OMP_TWO_NONRECT_LOOP
 #if _OpenMP_5
-#pragma omp parallel for num_threads(get_num_threads()) collapse(2)
+#define _OMP_NONRECT_LOOP collapse(2)
 #else
-#pragma omp parallel for num_threads(get_num_threads()) schedule(dynamic)
+#define _OMP_NONRECT_LOOP schedule(dynamic)
 #endif
-#endif
-
 
 std::vector<annot::ColumnCompressed<>>
 load_columns(const std::vector<std::string> &source_files, uint64_t *num_rows) {
@@ -163,7 +160,7 @@ void count_labels_per_row(const std::vector<std::string> &source_files,
         row_count_block.assign(block_size, 0);
 
         // process the current block
-        __OMP_TWO_NONRECT_LOOP
+        #pragma omp parallel for num_threads(get_num_threads()) _OMP_NONRECT_LOOP
         for (size_t l_idx = 0; l_idx < sources.size(); ++l_idx) {
             for (size_t j = 0; j < sources[l_idx].num_labels(); ++j) {
                 const bit_vector &source_col
@@ -656,7 +653,7 @@ void traverse_anno_chunked(
         assert(succ_chunk.size() == succ_chunk_idx.back());
         assert(pred_chunk.size() == pred_chunk_idx.back());
         // process the current block
-        __OMP_TWO_NONRECT_LOOP
+        #pragma omp parallel for num_threads(num_threads) _OMP_NONRECT_LOOP
         for (size_t l_idx = 0; l_idx < col_annotations.size(); ++l_idx) {
             for (size_t j = 0; j < col_annotations[l_idx].num_labels(); ++j) {
                 const bit_vector &source_col
@@ -980,7 +977,7 @@ void convert_batch_to_row_diff(const std::string &pred_succ_fprefix,
                 });
             });
 
-    __OMP_TWO_NONRECT_LOOP
+    #pragma omp parallel for num_threads(num_threads) _OMP_NONRECT_LOOP
     for (size_t s = 0; s < sources.size(); ++s) {
         for (size_t j = 0; j < sources[s].num_labels(); ++j) {
             // flush and release the buffer
@@ -1079,7 +1076,7 @@ void convert_batch_to_row_diff(const std::string &pred_succ_fprefix,
     for (uint64_t chunk = 0; chunk < row_reduction.size(); chunk += BLOCK_SIZE) {
         row_nbits_block.assign(std::min(BLOCK_SIZE, row_reduction.size() - chunk), 0);
 
-        __OMP_TWO_NONRECT_LOOP
+        #pragma omp parallel for num_threads(num_threads) _OMP_NONRECT_LOOP
         for (size_t l_idx = 0; l_idx < diff_columns.size(); ++l_idx) {
             for (const auto &col_ptr : diff_columns[l_idx]) {
                 col_ptr->call_ones_in_range(chunk, chunk + row_nbits_block.size(),
@@ -1375,7 +1372,7 @@ void convert_batch_to_row_diff_coord(const std::string &pred_succ_fprefix,
             [&](uint64_t) {}
     );
 
-    __OMP_TWO_NONRECT_LOOP
+    #pragma omp parallel for num_threads(num_threads) _OMP_NONRECT_LOOP
     for (size_t s = 0; s < sources.size(); ++s) {
         for (size_t j = 0; j < sources[s].num_labels(); ++j) {
             // flush and release the buffer
