d343e96fcb16d17f9982f281971806b5c647ac43
Oleksandr Kulkov <oleksandr.kulkov@grlab.org>
2024-03-27 21:54:38 +0100

    apply clang-format to dbg_sshash

--- metagraph/src/graph/representation/hash/dbg_sshash.hpp
+++ metagraph/src/graph/representation/hash/dbg_sshash.hpp
@@ -14,59 +14,60 @@ namespace mtg::graph {
 
 class DBGSSHash : public DeBruijnGraph {
     using kmer_t = sshash::dna_uint_kmer_t<uint64_t>;
+
   public:
     explicit DBGSSHash(size_t k);
     DBGSSHash(std::string const& input_filename, size_t k, Mode mode = BASIC);
 
-// SequenceGraph overrides
+    // SequenceGraph overrides
     void add_sequence(
             std::string_view sequence,
-            const std::function<void(node_index)> &on_insertion = [](node_index) {}) override;
+            const std::function<void(node_index)>& on_insertion = [](node_index) {}) override;
 
     void map_to_nodes(
             std::string_view sequence,
-            const std::function<void(node_index)> &callback,
-            const std::function<bool()> &terminate = []() { return false; }) const override;
+            const std::function<void(node_index)>& callback,
+            const std::function<bool()>& terminate = []() { return false; }) const override;
 
     void map_to_nodes_sequentially(
             std::string_view sequence,
-            const std::function<void(node_index)> &callback,
-            const std::function<bool()> &terminate = []() { return false; }) const override;
+            const std::function<void(node_index)>& callback,
+            const std::function<bool()>& terminate = []() { return false; }) const override;
 
     void map_to_nodes_with_rc(
             std::string_view sequence,
-            const std::function<void(node_index, bool)> &callback,
-            const std::function<bool()> &terminate = []() { return false; }) const;
+            const std::function<void(node_index, bool)>& callback,
+            const std::function<bool()>& terminate = []() { return false; }) const;
 
     void adjacent_outgoing_nodes(node_index node,
-                                 const std::function<void(node_index)> &callback) const override;
+                                 const std::function<void(node_index)>& callback) const override;
 
     void adjacent_incoming_nodes(node_index node,
-                                 const std::function<void(node_index)> &callback) const override;
+                                 const std::function<void(node_index)>& callback) const override;
 
     uint64_t num_nodes() const override;
 
-    bool load(std::istream &in);
-    bool load(const std::string &filename) override;
+    bool load(std::istream& in);
+    bool load(const std::string& filename) override;
 
-    void serialize(std::ostream &out) const;
-    void serialize(const std::string &filename) const override;
+    void serialize(std::ostream& out) const;
+    void serialize(const std::string& filename) const override;
 
     static constexpr auto kExtension = ".sshashdbg";
     std::string file_extension() const override;
 
     std::string get_node_sequence(node_index node) const override;
 
-// DeBruijnGraph overrides
-    size_t get_k() const override ;
+    // DeBruijnGraph overrides
+    size_t get_k() const override;
 
     // TODO: add the support for the canonical mode
     Mode get_mode() const override;
-    
+
     node_index traverse(node_index node, char next_char) const override;
     node_index traverse_back(node_index node, char prev_char) const override;
 
-    void call_kmers(const std::function<void(node_index, const std::string &)> &callback) const override;
+    void call_kmers(const std::function<void(node_index, const std::string&)>& callback) const override;
 
     size_t outdegree(node_index) const override;
 
@@ -76,21 +77,23 @@ class DBGSSHash : public DeBruijnGraph {
     std::pair<node_index, bool> kmer_to_node_with_rc(std::string_view kmer) const;
 
     void call_outgoing_kmers(node_index node,
-                             const OutgoingEdgeCallback &callback) const override;
+                             const OutgoingEdgeCallback& callback) const override;
 
-    void call_outgoing_kmers_with_rc(node_index node,
-                             const std::function<void(node_index, char, bool)> &callback) const;
+    void call_outgoing_kmers_with_rc(
+            node_index node,
+            const std::function<void(node_index, char, bool)>& callback) const;
 
     void call_incoming_kmers(node_index node,
-                             const IncomingEdgeCallback &callback) const override;
+                             const IncomingEdgeCallback& callback) const override;
 
-    void call_incoming_kmers_with_rc(node_index node,
-                             const std::function<void(node_index, char, bool)> &callback) const;
+    void call_incoming_kmers_with_rc(
+            node_index node,
+            const std::function<void(node_index, char, bool)>& callback) const;
 
 
-    bool operator==(const DeBruijnGraph &other) const override;
+    bool operator==(const DeBruijnGraph& other) const override;
 
-    const std::string &alphabet() const override;
+    const std::string& alphabet() const override;
 
     const sshash::dictionary<kmer_t>& data() const { return dict_; }
 
