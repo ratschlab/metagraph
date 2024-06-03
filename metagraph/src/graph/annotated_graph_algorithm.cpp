9a749f3636c97fe1ca72784437fbef7d549772d5
Harun Mustafa <bilgisayaradami@gmail.com>
2024-06-03 18:48:34 +0200

    minor

--- metagraph/src/graph/annotated_graph_algorithm.cpp
+++ metagraph/src/graph/annotated_graph_algorithm.cpp
@@ -403,12 +403,6 @@ mask_nodes_by_label_dual(std::shared_ptr<const DeBruijnGraph> graph_ptr,
             return std::make_pair(r, r / (r + mu));
         };
 
-        double target_size = 0.0;
-        for (size_t j = 0; j < groups.size(); ++j) {
-            target_size += log(static_cast<double>(sums[j]));
-        }
-        target_size = exp(target_size / groups.size());
-
         double target_p_num = 0.0;
         double target_p_denom = 0.0;
         std::vector<std::pair<double, double>> nb_params;
@@ -436,9 +430,9 @@ mask_nodes_by_label_dual(std::shared_ptr<const DeBruijnGraph> graph_ptr,
             nb_params.emplace_back(r, p);
 
             target_p_num += p * p;
-            target_p_denom += p / s;
+            target_p_denom += p;
         }
-        double target_p = target_p_num / target_size / target_p_denom;
+        double target_p = target_p_num / target_p_denom;
 
         std::vector<VectorMap<uint64_t, std::pair<size_t, uint64_t>>> count_maps(groups.size());
         double r_in = 0;
