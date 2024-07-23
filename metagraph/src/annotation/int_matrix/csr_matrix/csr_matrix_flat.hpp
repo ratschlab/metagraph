fd1c8f8d1ae4bc08a855f99fdde57d2fcbb2d282
Harun Mustafa <bilgisayaradami@gmail.com>
2024-07-23 22:08:08 +0200

    test

--- metagraph/src/annotation/int_matrix/csr_matrix/csr_matrix_flat.hpp
+++ metagraph/src/annotation/int_matrix/csr_matrix/csr_matrix_flat.hpp
@@ -33,8 +33,8 @@ class CSRMatrixFlat : public RowMajor, public IntMatrix {
 
     std::vector<RowValues> get_row_values(const std::vector<BinaryMatrix::Row> &rows) const;
 
-    void call_row_values(const std::function<void(uint64_t, RowValues&&, size_t)> &callback,
-                         bool ordered = true) const override {
+    inline void call_row_values(const std::function<void(uint64_t, RowValues&&, size_t)> &callback,
+                                bool ordered = true) const override final {
         constexpr size_t kNumRowsInBlock = 50'000;
         std::visit([&](const auto &binmat) {
             using T = std::decay_t<decltype(binmat)>;
@@ -94,12 +94,35 @@ class CSRMatrixFlat : public RowMajor, public IntMatrix {
                         }
                     }
                 } else {
+                    uint64_t num_set_bits = flat.num_set_bits();
+                    uint64_t block_size = num_set_bits / std::max(1u, get_num_threads() - 1);
+                    size_t row_block_size = num_columns_ * 64;
+                    std::vector<uint64_t> boundaries;
+                    boundaries.emplace_back(0);
+                    for (size_t r = 1; r < num_set_bits; r += block_size) {
+                        uint64_t b = flat.select1(r);
+                        uint64_t b_round_ncols_64 = (b + row_block_size - 1) / row_block_size * row_block_size;
+                        if (b_round_ncols_64 > boundaries.back()) {
+                            boundaries.emplace_back(b_round_ncols_64);
+                            if (boundaries.back() >= flat.size()) {
+                                boundaries.back() = flat.size();
+                                break;
+                            }
+                        }
+                    }
+
+                    if (boundaries.back() != flat.size())
+                        boundaries.emplace_back(flat.size());
+
+                    common::logger->trace("Streaming rows");
                     ProgressBar progress_bar(n, "Constructed rows", std::cerr, !common::get_verbose());
-                    uint64_t block_size = n / std::max(1u, get_num_threads() - 1);
-                    block_size -= block_size % 64;
                     #pragma omp parallel for num_threads(get_num_threads()) schedule(dynamic)
-                    for (uint64_t begin = 0; begin < n; begin += block_size) {
-                        size_t thread_id = begin / block_size;
+                    for (size_t thread_id = 0; thread_id < boundaries.size() - 1; ++thread_id) {
+                        uint64_t flat_begin = boundaries[thread_id];
+                        uint64_t flat_end = boundaries[thread_id + 1];
+                        uint64_t begin = flat_begin / num_columns_;
+                        uint64_t end = flat_end / num_columns_;
+
                         RowValues row;
 
                         const S *v_use = &v;
@@ -110,10 +133,6 @@ class CSRMatrixFlat : public RowMajor, public IntMatrix {
                             v_use = v_copy.get();
                         }
 
-                        uint64_t end = std::min(begin + block_size, n);
-                        assert(begin <= end);
-                        uint64_t flat_begin = begin * num_columns_;
-                        uint64_t flat_end = end * num_columns_;
                         uint64_t r = flat.rank1(flat_begin) - flat[flat_begin];
                         auto it = const_cast<S*>(v_use)->begin() + r;
 
@@ -154,10 +173,10 @@ class CSRMatrixFlat : public RowMajor, public IntMatrix {
         }, matrix_);
     }
 
-    std::vector<VectorMap<uint64_t, size_t>> get_histograms(const std::vector<size_t> &min_counts = {},
-                                                            sdsl::bit_vector *unmark_discarded = nullptr) const override {
+    inline std::vector<VectorMap<uint64_t, size_t>>
+    get_histograms(const std::vector<size_t> &min_counts = {},
+                   sdsl::bit_vector *unmark_discarded = nullptr) const override final {
         constexpr uint8_t HIST_CUTOFF = 64;
-        bool parallel = get_num_threads() > 1;
         uint64_t n = num_rows();
         size_t num_tasks = get_num_threads() + 1;
         std::vector<std::vector<size_t>> num_nonzeros;
@@ -180,39 +199,129 @@ class CSRMatrixFlat : public RowMajor, public IntMatrix {
 
         std::atomic_thread_fence(std::memory_order_release);
 
-        auto row_callback = [&](uint64_t row_i, auto&& row, size_t bucket_id) __attribute__((always_inline)) {
-            bool keep = min_counts.empty();
+        std::visit([&](const auto &binmat) {
+            using T = std::decay_t<decltype(binmat)>;
+            static_assert(std::is_same_v<T, RowFlat<>>);
+            const auto &flat = binmat.data();
 
-            if (min_counts.size()) {
-                for (const auto &[j, c] : row) {
-                    if (c >= min_counts[j]) {
-                        keep = true;
-                        break;
+            std::visit([&](const auto &v) {
+                using S = std::decay_t<decltype(v)>;
+                uint64_t n = num_rows();
+
+                uint64_t num_set_bits = flat.num_set_bits();
+                uint64_t block_size = num_set_bits / std::max(1u, get_num_threads() - 1);
+                size_t row_block_size = num_columns_ * 64;
+                std::vector<uint64_t> boundaries;
+                boundaries.emplace_back(0);
+                for (size_t r = 1; r < num_set_bits; r += block_size) {
+                    uint64_t b = flat.select1(r);
+                    uint64_t b_round_ncols_64 = (b + row_block_size - 1) / row_block_size * row_block_size;
+                    if (b_round_ncols_64 > boundaries.back()) {
+                        boundaries.emplace_back(b_round_ncols_64);
+                        if (boundaries.back() >= flat.size()) {
+                            boundaries.back() = flat.size();
+                            break;
+                        }
                     }
                 }
-            }
 
-            if (!keep) {
-                if (unmark_discarded)
-                    // unset_bit(unmark_discarded->data(), row_i, false);
-                    unset_bit(unmark_discarded->data(), row_i, parallel, std::memory_order_relaxed);
+                if (boundaries.back() != flat.size())
+                    boundaries.emplace_back(flat.size());
 
-                num_empty_rows.fetch_add(1, std::memory_order_relaxed);
-            } else {
-                auto &hists_map = hists_maps[bucket_id];
-                auto &hists_map_base = hists_maps_base[bucket_id];
-                for (const auto &[j, c] : row) {
-                    if (c < HIST_CUTOFF) {
-                        ++hists_map_base[j][c];
+                common::logger->trace("Streaming rows");
+                ProgressBar progress_bar(n, "Constructed rows", std::cerr, !common::get_verbose());
+                #pragma omp parallel for num_threads(get_num_threads()) schedule(dynamic)
+                for (size_t thread_id = 0; thread_id < boundaries.size() - 1; ++thread_id) {
+                    RowValues row;
+                    auto row_callback = [&](uint64_t row_i) __attribute__((always_inline)) {
+                        bool keep = min_counts.empty();
+
+                        if (min_counts.size()) {
+                            for (const auto &[j, c] : row) {
+                                if (c >= min_counts[j]) {
+                                    keep = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!keep) {
+                            if (unmark_discarded)
+                                unset_bit(unmark_discarded->data(), row_i, false, std::memory_order_relaxed);
+
+                            num_empty_rows.fetch_add(1, std::memory_order_relaxed);
+                        } else {
+                            auto &hists_map = hists_maps[thread_id];
+                            auto &hists_map_base = hists_maps_base[thread_id];
+                            for (const auto &[j, c] : row) {
+                                if (c < HIST_CUTOFF) {
+                                    ++hists_map_base[j][c];
+                                } else {
+                                    ++hists_map[j][c];
+                                }
+                                ++num_nonzeros[thread_id][j];
+                            }
+                        }
+                    };
+                    uint64_t flat_begin = boundaries[thread_id];
+                    uint64_t flat_end = boundaries[thread_id + 1];
+                    uint64_t begin = flat_begin / num_columns_;
+                    uint64_t end = flat_end / num_columns_;
+
+
+                    const S *v_use = &v;
+
+                    std::unique_ptr<sdsl::int_vector_buffer<>> v_copy;
+                    if constexpr(std::is_same_v<S, sdsl::int_vector_buffer<>>) {
+                        v_copy = std::make_unique<sdsl::int_vector_buffer<>>(v.filename(), std::ios::in, v.buffersize(), 0, false, offset_);
+                        v_use = v_copy.get();
+                    }
+
+                    uint64_t r = flat.rank1(flat_begin) - flat[flat_begin];
+                    auto it = const_cast<S*>(v_use)->begin() + r;
+
+                    uint64_t last_i = begin;
+                    flat.call_ones_in_range(flat_begin, flat_end, [&](uint64_t b) __attribute__((always_inline)) {
+                        uint64_t i = b / num_columns_;
+                        if (i > last_i) {
+                            row_callback(last_i);
+
+                            ++last_i;
+                            row.clear();
+
+                            num_empty_rows.fetch_add(i - last_i, std::memory_order_relaxed);
+                            if (unmark_discarded) {
+                                for ( ; last_i < i; ++last_i) {
+                                    unset_bit(unmark_discarded->data(), last_i, false, std::memory_order_relaxed);
+                                }
+                            } else {
+                                last_i = i;
+                            }
+                        }
+
+                        uint64_t j = b % num_columns_;
+                        uint64_t val = *it;
+                        ++it;
+
+                        row.emplace_back(j, val);
+                    });
+
+                    row_callback(last_i);
+                    ++last_i;
+
+                    num_empty_rows.fetch_add(end - last_i, std::memory_order_relaxed);
+                    if (unmark_discarded) {
+                        for ( ; last_i < end; ++last_i) {
+                            unset_bit(unmark_discarded->data(), last_i, false, std::memory_order_relaxed);
+                        }
                     } else {
-                        ++hists_map[j][c];
+                        last_i = end;
                     }
-                    ++num_nonzeros[bucket_id][j];
-                }
-            }
-        };
 
-        call_row_values(row_callback, false);
+                    progress_bar += end - begin;
+                }
+            }, values_);
+        }, matrix_);
 
         std::atomic_thread_fence(std::memory_order_acquire);
 
