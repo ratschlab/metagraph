3dd6bd7361b5f554afdd36e48d4a458c2bc9df00
Harun Mustafa <bilgisayaradami@gmail.com>
2024-12-09 14:19:10 +0100

    fix

--- metagraph/src/graph/representation/hash/dbg_sshash.hpp
+++ metagraph/src/graph/representation/hash/dbg_sshash.hpp
@@ -123,7 +123,7 @@ class DBGSSHash : public DeBruijnGraph {
                         sshash::streaming_query_regular_parsing<kmer_t<KmerInt64>>,
                         sshash::streaming_query_regular_parsing<kmer_t<KmerInt128>>,
                         sshash::streaming_query_regular_parsing<kmer_t<KmerInt256>>>;
-    static mutable parser_t parser_;
+    static parser_t parser_;
 
     size_t dict_size() const;
 };
