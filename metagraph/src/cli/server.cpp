cbb389185eeaefc5a55da1d7e24f7a1df5e9895a
Mikhail Karasikov <karasikov@users.noreply.github.com>
2025-10-14 09:59:03 +0200

    multi-graph server: support hosting multiple graphs in one process (#540)
    * Support hosting multiple graphs
    * Filter which ones to query with field graphs in the request json
    * All query requests in the multi-graph mode share the same thread pool for querying individual graphs

--- metagraph/src/cli/server.cpp
+++ metagraph/src/cli/server.cpp
@@ -1,4 +1,5 @@
 #include <json/json.h>
+#include <tsl/hopscotch_map.h>
 #include <server_http.hpp>
 
 #include "common/logger.hpp"
@@ -16,6 +17,7 @@
 #include "query.hpp"
 #include "align.hpp"
 #include "server_utils.hpp"
+#include "cli/load/load_annotation.hpp"
 
 
 namespace mtg {
@@ -27,11 +29,9 @@ using namespace mtg::graph;
 using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 
-std::string process_search_request(const std::string &received_message,
+Json::Value process_search_request(const Json::Value &json,
                                    const graph::AnnotatedDBG &anno_graph,
                                    const Config &config_orig) {
-    Json::Value json = parse_json_string(received_message);
-
     const auto &fasta = json["FASTA"];
     if (fasta.isNull())
         throw std::domain_error("No input sequences received from client");
@@ -132,13 +132,11 @@ std::string process_search_request(const std::string &received_message,
         search_response.append(seq_result.to_json(config.verbose_output, anno_graph));
     }
 
-    // Return JSON string
-    Json::StreamWriterBuilder builder;
-    return Json::writeString(builder, search_response);
+    return search_response;
 }
 
 // TODO: implement alignment_result.to_json as in process_search_request
-std::string process_align_request(const std::string &received_message,
+Json::Value process_align_request(const std::string &received_message,
                                   const graph::DeBruijnGraph &graph,
                                   const Config &config_orig) {
     Json::Value json = parse_json_string(received_message);
@@ -205,48 +203,7 @@ std::string process_align_request(const std::string &received_message,
         root.append(align_entry);
     });
 
-    Json::StreamWriterBuilder builder;
-    return Json::writeString(builder, root);
-}
-
-std::string process_column_label_request(const graph::AnnotatedDBG &anno_graph) {
-    auto labels = anno_graph.get_annotator().get_label_encoder().get_labels();
-
-    Json::Value root = Json::Value(Json::arrayValue);
-
-    for (const std::string &label : labels) {
-        Json::Value entry = label;
-        root.append(entry);
-    }
-
-    Json::StreamWriterBuilder builder;
-    return Json::writeString(builder, root);
-}
-
-std::string process_stats_request(const graph::AnnotatedDBG &anno_graph,
-                                  const std::string &graph_filename,
-                                  const std::string &annotation_filename) {
-    Json::Value root;
-
-    Json::Value graph_stats;
-    graph_stats["filename"] = std::filesystem::path(graph_filename).filename().string();
-    graph_stats["k"] = static_cast<uint64_t>(anno_graph.get_graph().get_k());
-    graph_stats["nodes"] = anno_graph.get_graph().num_nodes();
-    graph_stats["is_canonical_mode"] = (anno_graph.get_graph().get_mode()
-                                            == graph::DeBruijnGraph::CANONICAL);
-    root["graph"] = graph_stats;
-
-    Json::Value annotation_stats;
-    const auto &annotation = anno_graph.get_annotator();
-    annotation_stats["filename"] = std::filesystem::path(annotation_filename).filename().string();
-    annotation_stats["labels"] = static_cast<uint64_t>(annotation.num_labels());
-    annotation_stats["objects"] = static_cast<uint64_t>(annotation.num_objects());
-    annotation_stats["relations"] = static_cast<uint64_t>(annotation.num_relations());
-
-    root["annotation"] = annotation_stats;
-
-    Json::StreamWriterBuilder builder;
-    return Json::writeString(builder, root);
+    return root;
 }
 
 std::thread start_server(HttpServer &server_startup, Config &config) {
@@ -257,8 +214,9 @@ std::thread start_server(HttpServer &server_startup, Config &config) {
     }
     server_startup.config.port = config.port;
 
-    logger->info("[Server] Will listen on {} port {}", config.host_address,
-                 server_startup.config.port);
+    logger->info("[Server] Will listen on {} port {}",
+                 server_startup.config.address, server_startup.config.port);
+    logger->info("[Server] Maximum connections: {}", get_num_threads());
     return std::thread([&server_startup]() { server_startup.start(); });
 }
 
@@ -274,64 +232,256 @@ bool check_data_ready(std::shared_future<T> &data, shared_ptr<HttpServer::Respon
     return true;
 }
 
+std::vector<std::string> filter_graphs_from_list(
+        const tsl::hopscotch_map<std::string, std::vector<std::pair<std::string, std::string>>> &indexes,
+        const Json::Value &content_json,
+        size_t request_id,
+        size_t max_names_without_filtering = 10) {
+    std::vector<std::string> graphs_to_query;
+
+    if (content_json.isMember("graphs") && content_json["graphs"].isArray()) {
+        for (const auto &item : content_json["graphs"]) {
+            graphs_to_query.push_back(item.asString());
+            if (!indexes.count(graphs_to_query.back()))
+                throw std::invalid_argument("Request with an uninitialized graph " + graphs_to_query.back());
+        }
+        // deduplicate
+        std::sort(graphs_to_query.begin(), graphs_to_query.end());
+        graphs_to_query.erase(std::unique(graphs_to_query.begin(), graphs_to_query.end()), graphs_to_query.end());
+    } else {
+        if (indexes.size() > max_names_without_filtering) {
+            throw std::invalid_argument(
+                fmt::format("Bad request: requests without names (no \"graphs\" field) are "
+                            "only supported for small indexes (<={} names)",
+                            max_names_without_filtering));
+        }
+        // query all graphs from list `config->fnames`
+        for (const auto &[name, _] : indexes) {
+            graphs_to_query.push_back(name);
+        }
+    }
+    return graphs_to_query;
+}
+
+
 int run_server(Config *config) {
     assert(config);
-
-    assert(config->infbase_annotators.size() == 1);
+    std::atomic<size_t> num_requests = 0;
 
     ThreadPool graph_loader(1, 1);
+    std::shared_future<std::unique_ptr<AnnotatedDBG>> anno_graph;
 
-    logger->info("[Server] Loading graph...");
+    tsl::hopscotch_map<std::string, std::vector<std::pair<std::string, std::string>>> indexes;
 
-    auto anno_graph = graph_loader.enqueue([&]() {
-        auto graph = load_critical_dbg(config->infbase);
-        logger->info("[Server] Graph loaded. Current mem usage: {} MiB", get_curr_RSS() >> 20);
+    if (config->infbase_annotators.size() == 1) {
+        assert(config->fnames.empty());
+        anno_graph = graph_loader.enqueue([&]() {
+            logger->info("[Server] Loading graph...");
+            auto graph = load_critical_dbg(config->infbase);
+            logger->info("[Server] Graph loaded. Current mem usage: {} MiB", get_curr_RSS() >> 20);
 
-        auto anno_graph = initialize_annotated_dbg(graph, *config);
-        logger->info("[Server] Annotated graph loaded too. Current mem usage: {} MiB", get_curr_RSS() >> 20);
-        return anno_graph;
-    });
+            auto anno_graph = initialize_annotated_dbg(graph, *config);
+            logger->info("[Server] Annotated graph loaded too. Current mem usage: {} MiB", get_curr_RSS() >> 20);
+            return anno_graph;
+        });
+    } else {
+        assert(config->fnames.size() == 1);
 
-    // defaults for the server
-    config->num_top_labels = 10000;
+        std::ifstream file(config->fnames[0]);
+
+        if (!file.is_open()) {
+            logger->error("[Server] Could not open file {} for reading", config->fnames[0]);
+            std::exit(1);
+        }
+
+        size_t num_indexes = 0;
+        std::string line;
+        while (std::getline(file, line)) {
+            if (line.empty())
+                continue; // skip empty lines
+
+            std::stringstream ss(line);
+
+            std::string name;
+            std::string graph_path;
+            std::string annotation_path;
+
+            std::getline(ss, name, ',');
+            std::getline(ss, graph_path, ',');
+            if (ss.eof()) {
+                logger->error("[Server] Invalid line in the csv file: `{}`", line);
+                std::exit(1);
+            }
+            std::getline(ss, annotation_path, ',');
+
+            indexes[name].emplace_back(std::move(graph_path), std::move(annotation_path));
+            num_indexes++;
+        }
+        std::vector<std::string> names;
+        for (const auto &[name, _] : indexes) {
+            names.push_back(name);
+        }
+        logger->info("[Server] Loaded paths for {} graphs for {} names: {}",
+                     num_indexes, indexes.size(), fmt::join(names, ", "));
+        if (!utils::with_mmap()) {
+            logger->warn("[Server] --mmap wasn't passed but all indexes will be loaded with mmap."
+                         " Make sure they're on a fast disk.");
+            utils::with_mmap(true);
+        }
+    }
+
+    ThreadPool graphs_pool(get_num_threads());
+
+    logger->info("Collecting graph stats...");
+    tsl::hopscotch_map<std::string, std::vector<std::string>> name_labels;
+    for (const auto &[name, graphs] : indexes) {
+        for (const auto &[graph_fname, anno_fname] : graphs) {
+            auto &out = name_labels[name];
+            const auto &labels = read_labels(anno_fname);
+            out.insert(out.end(), labels.begin(), labels.end());
+        }
+    }
+
+    logger->info("All graphs were loaded and stats collected. Ready to serve queries.");
 
     // the actual server
     HttpServer server;
     server.resource["^/search"]["POST"] = [&](shared_ptr<HttpServer::Response> response,
                                               shared_ptr<HttpServer::Request> request) {
-        if (check_data_ready(anno_graph, response)) {
-            process_request(response, request, [&](const std::string &content) {
-                return process_search_request(content, *anno_graph.get(), *config);
-            });
-        }
+        size_t request_id = num_requests++;
+        logger->info("[Server] {} request {} from {}", request->path, request_id,
+                     request->remote_endpoint().address().to_string());
+
+        if (!config->fnames.size() && !check_data_ready(anno_graph, response))
+            return;  // the index is not loaded yet, so we can't process the request
+
+        process_request(response, request, [&](const std::string &content) {
+            Json::Value content_json = parse_json_string(content);
+            logger->info("Request {}: {}", request_id, content_json.toStyledString());
+            Json::Value result;
+
+            // simple case with a single graph pair
+            if (!config->fnames.size()) {
+                if (content_json.isMember("graphs"))
+                    throw std::invalid_argument("Bad request: no support for filtering graphs on this server");
+                result = process_search_request(content_json, *anno_graph.get(), *config);
+            } else {
+                std::vector<std::string> graphs_to_query
+                        = filter_graphs_from_list(indexes, content_json, request_id);
+                std::mutex mu;
+                std::vector<std::shared_future<void>> futures;
+                for (const auto &name : graphs_to_query) {
+                    for (const auto &[graph_fname, anno_fname] : indexes[name]) {
+                        Config config_copy = *config;
+                        config_copy.infbase = graph_fname;
+                        config_copy.infbase_annotators = { anno_fname };
+                        futures.push_back(graphs_pool.enqueue([config_copy{std::move(config_copy)},&content_json,&result,&mu]() {
+                            auto index = initialize_annotated_dbg(config_copy);
+                            auto json = process_search_request(content_json, *index, config_copy);
+                            std::lock_guard<std::mutex> lock(mu);
+                            if (result.empty()) {
+                                result = std::move(json);
+                            } else {
+                                assert(json.size() == result.size());
+                                for (Json::ArrayIndex i = 0; i < result.size(); ++i) {
+                                    if (result[i][SeqSearchResult::SEQ_DESCRIPTION_JSON_FIELD]
+                                            != json[i][SeqSearchResult::SEQ_DESCRIPTION_JSON_FIELD]) {
+                                        throw std::logic_error("ERROR: Results for different sequences can't be merged");
+                                    }
+                                    for (auto&& value : json[i]["results"]) {
+                                        result[i]["results"].append(std::move(value));
+                                    }
+                                }
+                            }
+                        }));
+                    }
+                }
+                for (auto &future : futures) {
+                    future.wait();
+                }
+            }
+            logger->info("Request {} finished", request_id);
+            return result;
+        });
     };
 
     server.resource["^/align"]["POST"] = [&](shared_ptr<HttpServer::Response> response,
                                              shared_ptr<HttpServer::Request> request) {
-        if (check_data_ready(anno_graph, response)) {
-            process_request(response, request, [&](const std::string &content) {
+        size_t request_id = num_requests++;
+        logger->info("[Server] {} request {} from {}", request->path, request_id,
+                     request->remote_endpoint().address().to_string());
+
+        if (!config->fnames.size() && !check_data_ready(anno_graph, response))
+            return;  // the index is not loaded yet, so we can't process the request
+
+        process_request(response, request, [&](const std::string &content) {
+            if (!config->fnames.size())
                 return process_align_request(content, anno_graph.get()->get_graph(), *config);
-            });
-        }
+
+            throw std::invalid_argument("Bad request: alignment requests are not yet supported for "
+                                        "servers with multiple graphs");
+        });
     };
 
     server.resource["^/column_labels"]["GET"] = [&](shared_ptr<HttpServer::Response> response,
                                                     shared_ptr<HttpServer::Request> request) {
-        if (check_data_ready(anno_graph, response)) {
-            process_request(response, request, [&](const std::string &) {
-                return process_column_label_request(*anno_graph.get());
-            });
-        }
+        size_t request_id = num_requests++;
+        logger->info("[Server] {} request {} from {}", request->path, request_id,
+                     request->remote_endpoint().address().to_string());
+
+        if (!config->fnames.size() && !check_data_ready(anno_graph, response))
+            return;  // the index is not loaded yet, so we can't process the request
+
+        process_request(response, request, [&](const std::string &) {
+            Json::Value root(Json::arrayValue);
+            if (!config->fnames.size()) {
+                auto labels = anno_graph.get()->get_annotator().get_label_encoder().get_labels();
+                for (const std::string &label : labels) {
+                    root.append(label);
+                }
+            } else {
+                for (const auto &[name, labels] : name_labels) {
+                    for (const std::string &label : labels) {
+                        root.append(label);
+                    }
+                }
+            }
+            return root;
+        });
     };
 
     server.resource["^/stats"]["GET"] = [&](shared_ptr<HttpServer::Response> response,
                                             shared_ptr<HttpServer::Request> request) {
-        if (check_data_ready(anno_graph, response)) {
-            process_request(response, request, [&](const std::string &) {
-                return process_stats_request(*anno_graph.get(), config->infbase,
-                                             config->infbase_annotators.front());
-            });
-        }
+        size_t request_id = num_requests++;
+        logger->info("[Server] {} request {} from {}", request->path, request_id,
+                     request->remote_endpoint().address().to_string());
+
+        if (!config->fnames.size() && !check_data_ready(anno_graph, response))
+            return;  // the index is not loaded yet, so we can't process the request
+
+        process_request(response, request, [&](const std::string &) {
+            Json::Value root;
+            if (config->fnames.size()) {
+                // for scenarios with multiple graphs
+                uint64_t num_labels = 0;
+                for (const auto &[name, labels] : name_labels) {
+                    num_labels += labels.size();
+                }
+                root["annotation"]["labels"] = num_labels;
+            } else {
+                root["graph"]["filename"] = std::filesystem::path(config->infbase).filename().string();
+                root["graph"]["k"] = static_cast<uint64_t>(anno_graph.get()->get_graph().get_k());
+                root["graph"]["nodes"] = anno_graph.get()->get_graph().num_nodes();
+                root["graph"]["is_canonical_mode"] = (anno_graph.get()->get_graph().get_mode()
+                                                        == graph::DeBruijnGraph::CANONICAL);
+                const auto &annotation = anno_graph.get()->get_annotator();
+                root["annotation"]["filename"] = std::filesystem::path(config->infbase_annotators.front()).filename().string();
+                root["annotation"]["labels"] = static_cast<uint64_t>(annotation.num_labels());
+                root["annotation"]["objects"] = static_cast<uint64_t>(annotation.num_objects());
+                root["annotation"]["relations"] = static_cast<uint64_t>(annotation.num_relations());
+            }
+            return root;
+        });
     };
 
     server.default_resource["GET"] = [](shared_ptr<HttpServer::Response> response,
--- metagraph/src/cli/server_utils.hpp
+++ metagraph/src/cli/server_utils.hpp
@@ -11,7 +11,7 @@ using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 void process_request(std::shared_ptr<HttpServer::Response> &response,
                      const std::shared_ptr<HttpServer::Request> &request,
-                     const std::function<std::string(const std::string &)> &process);
+                     const std::function<Json::Value(const std::string &)> &process);
 
 Json::Value parse_json_string(const std::string &msg);
 
